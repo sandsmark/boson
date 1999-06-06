f700c0ba1e4d421bbe478586836977d334ed2973
orzel <orzel@14664f6f-540d-0410-967e-c81f6ba7cd39>
1999-06-06 00:21:16 +0000

    slightly improve gameProperties_t

--- trunk/code/common/boconfig.h
+++ trunk/code/common/boconfig.h
@@ -30,6 +30,7 @@
 #define BOSON_MAGIC_LENGHT	(sizeof(BOSON_MAGIC)/sizeof(char))
 
 #define BOSON_MAX_CONNECTION	(5)
+#define BOSON_MAX_PLAYERS	BOSON_MAX_CONNECTION ///orzel : to clean up
 #define BOSON_DEFAULT_PORT	(5454)
 #define BOSON_BUFFER_SIZE	(5*1024)
 
