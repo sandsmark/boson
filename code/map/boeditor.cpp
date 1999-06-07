06e64651b906f51db3a5bcc6d889fba37dfd9171
orzel <orzel@14664f6f-540d-0410-967e-c81f6ba7cd39>
1999-06-07 00:26:13 +0000

    Cleaning of boson/map, boreader is dead. 	playField is moved to common 	serverCell is back in boson/server.

--- code/map/boeditor.cpp
+++ code/map/boeditor.cpp
@@ -18,19 +18,22 @@
  *                                                                         *
  ***************************************************************************/
 
-//#include <qdatastream.h>
 #include <stdio.h>
-#include "playField.h"
+
 #include "map.h"
 
+#include "../common/playField.h"
 #include "../common/groundType.h"
-//#include "../common/cell.h"
-#include "serverCell.h"
-
+#include "../common/cell.h"
 
 
 #define MAP_WIDTH	200
 #define MAP_HEIGHT	200
+#define PF_NAME		"basic.bpf"
+
+void createSeaMap(int width, int height, playField &field);
+void createIsland(int x, int y, int trans, playField &field, int width=1, int height=1, bool inverted=false);
+
 
 
 origFacility facility[] = {
@@ -61,51 +64,55 @@ origPeople people = {
 
 
 
-void createSeaMap(int width, int height, playField &field);
-void createIsland(int x, int y, int trans, playField &field, int width=1, int height=1, bool inverted=false);
 
 int main(void)
 {
 
-playField field("basic.bpf"); /// basic BosonPlayField...
-createSeaMap( MAP_WIDTH, MAP_HEIGHT, field);
-createIsland(12, 15, TRANS_GW, field, 8, 15);
-createIsland(15, 23, TRANS_GD, field, 2, 3, true);
-createIsland(22, 15, TRANS_GW, field);
+playField *field = new playField(PF_NAME); /// basic BosonPlayField...
+createSeaMap( MAP_WIDTH, MAP_HEIGHT, *field);
+createIsland(12, 15, TRANS_GW, *field, 8, 15);
+createIsland(15, 23, TRANS_GD, *field, 2, 3, true);
+createIsland(22, 15, TRANS_GW, *field);
 
 /* fill misc */
-field.nbPlayer = 2;
+field->nbPlayer = 2;
 
 /* fill people  */
 
-field.people = people;
+field->people = people;
 
-if (!field.write()) {
+if (!field->write()) {
 	puts("Error : impossible to create file");
 	exit(1);
 	}
 
 puts("Ok, file created");
 
-if (!field.load()) {
+delete field;
+
+/* re-read, check coherency */
+field = new playField(PF_NAME);
+
+if (!field->load()) {
 	puts("Error : impossible to re-read file");
 	exit(1);
 	}
 
 puts("Ok, file re-read, checking...");
 
-if (field.map.width != MAP_WIDTH)
+if (field->map.width != MAP_WIDTH)
 	puts("width NOK");
-if (field.map.height!= MAP_HEIGHT)
+if (field->map.height!= MAP_HEIGHT)
 	puts("height NOK");
-if (field.people.nbMobiles != sizeof(mobile)/sizeof(mobile[0]))
+if (field->people.nbMobiles != sizeof(mobile)/sizeof(mobile[0]))
 	puts("nbMobiles NOK");
-if (field.people.nbFacilities != sizeof(facility)/sizeof(facility[0]))
+if (field->people.nbFacilities != sizeof(facility)/sizeof(facility[0]))
 	puts("nbFacilities NOK");
-
 
 return 0;
 }
+
+
 
 void createSeaMap(int width, int height, playField &field)
 {
@@ -115,15 +122,17 @@ printf("Filling the area with water\n");
 /* fill map struct */
 field.map.width = width;
 field.map.height= height;
-	field.map.cells = new (serverCell *)[field.map.width];
+	field.map.cells = new (groundType *)[field.map.width];
 	for (i=0; i< field.map.width; i++)
-		field.map.cells[i] = new (serverCell)[field.map.height];
+		field.map.cells[i] = new (groundType)[field.map.height];
 		
 	for (i=0; i< field.map.width; i++)
 		for (j=0; j< field.map.height; j++)
-			field.map.cells[i][j].setGroundType(GROUND_WATER);
+			field.map.cells[i][j] = GROUND_WATER;
 	
 }
+
+
 
 void createIsland(int x, int y, int trans, playField &field, int width, int height, bool inverted)
 {
@@ -135,24 +144,23 @@ printf("Drawing island in %d,%d, size %dx%d, %s %s\n",
 	inverted?"(inverted)":""
 	);
 
-
 /* corners */
-field.map.cells[x-1][y-1].setGroundType(GET_TRANS_NUMBER(trans, inverted?TRANS_ULI:TRANS_UL));
-field.map.cells[x+width][y-1].setGroundType(GET_TRANS_NUMBER(trans, inverted?TRANS_URI:TRANS_UR));
+field.map.cells[x-1][y-1] = GET_TRANS_NUMBER(trans, inverted?TRANS_ULI:TRANS_UL);
+field.map.cells[x+width][y-1] = GET_TRANS_NUMBER(trans, inverted?TRANS_URI:TRANS_UR);
 
-field.map.cells[x-1][y+height].setGroundType(GET_TRANS_NUMBER(trans, inverted?TRANS_DLI:TRANS_DL));
-field.map.cells[x+width][y+height].setGroundType(GET_TRANS_NUMBER(trans, inverted?TRANS_DRI:TRANS_DR));
+field.map.cells[x-1][y+height] = GET_TRANS_NUMBER(trans, inverted?TRANS_DLI:TRANS_DL);
+field.map.cells[x+width][y+height] = GET_TRANS_NUMBER(trans, inverted?TRANS_DRI:TRANS_DR);
 
 /* horizontal borders */
 for (i=0; i<width; i++) {
-	field.map.cells[x+i][y-1].setGroundType(GET_TRANS_NUMBER(trans, inverted?TRANS_DOWN:TRANS_UP));
-	field.map.cells[x+i][y+height].setGroundType(GET_TRANS_NUMBER(trans, inverted?TRANS_UP:TRANS_DOWN));
+	field.map.cells[x+i][y-1] = GET_TRANS_NUMBER(trans, inverted?TRANS_DOWN:TRANS_UP);
+	field.map.cells[x+i][y+height] = GET_TRANS_NUMBER(trans, inverted?TRANS_UP:TRANS_DOWN);
 	}
 
 /* vertical borders */
 for (j=0; j<height; j++) {
-	field.map.cells[x-1][y+j].setGroundType(GET_TRANS_NUMBER(trans, inverted?TRANS_RIGHT:TRANS_LEFT));
-	field.map.cells[x+width][y+j].setGroundType(GET_TRANS_NUMBER(trans, inverted?TRANS_LEFT:TRANS_RIGHT));
+	field.map.cells[x-1][y+j] = GET_TRANS_NUMBER(trans, inverted?TRANS_RIGHT:TRANS_LEFT);
+	field.map.cells[x+width][y+j] = GET_TRANS_NUMBER(trans, inverted?TRANS_LEFT:TRANS_RIGHT);
 	}
 
 /* filling inside */
@@ -160,5 +168,5 @@ groundType plain = inverted?groundTransProp[trans].to:groundTransProp[trans].fro
 //printf("filling with %d\n", plain);
 for (i=0; i<width; i++)
 	for (j=0; j<height; j++)
-		field.map.cells[x+i][y+j].setGroundType(plain);
+		field.map.cells[x+i][y+j] = plain;
 }
