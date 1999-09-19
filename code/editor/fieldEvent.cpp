3ca60e0f2fa34a958622bc002b832f530d5744d2
orzel <orzel@14664f6f-540d-0410-967e-c81f6ba7cd39>
1999-09-19 12:09:31 +0000

    boeditor, first shot, copy from ../boson, removed network stuff, adapted everything so that it compiles, link, and execute. presently does nothing else :-)

---
+++ code/editor/fieldEvent.cpp
@@ -0,0 +1,231 @@
+/***************************************************************************
+                          fieldEvent.cpp  -  description                              
+                             -------------------                                         
+
+    version              : $Id$
+    begin                : Sat Feb 17, 1999
+                                           
+    copyright            : (C) 1999 by Thomas Capricelli                         
+    email                : capricel@enst.fr                                     
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   * 
+ *                                                                         *
+ ***************************************************************************/
+
+#include <qpainter.h>
+
+#include "../common/log.h"
+#include "../map/map.h"
+
+#include "viewMap.h"
+#include "fieldMap.h"
+//#include "orderWin.h"
+#include "playerMap.h"
+
+
+#define mobileList (((playerMap*)(view->phys))->mobile)
+#define facilityList (((playerMap*)(view->phys))->facility)
+
+static int selectX, selectY;
+static int oldX, oldY;
+
+void fieldMap::mousePressEvent(QMouseEvent *e)
+{
+int x, y;
+
+bool found = FALSE;
+
+x = e->x();
+y = e->y();
+
+if (e->button() & RightButton) {
+	emit relativeReCenterView( x/BO_TILE_SIZE , y/BO_TILE_SIZE);
+	return;
+	}
+
+if (e->button() & LeftButton) {	
+	/* Here we transpose coo into the map referential */
+	x += view->X()*BO_TILE_SIZE; y += view->Y()*BO_TILE_SIZE;
+
+
+	if (SELECT_MOVE == view->getSelectionMode()) {
+		view->leftClicked( x, y);
+		return;
+		}
+
+	/* Control -> multiselection, else... */
+	if (! (e->state()&ControlButton)) {
+		unSelectAll();
+		}
+
+	QIntDictIterator<visualMobUnit> mobIt(mobileList);
+	QIntDictIterator<visualFacility> fixIt(facilityList);
+
+	visualMobUnit	*m;
+	visualFacility	*f;
+
+//printf("\n\nselection , x=%d,y=%d\n", x, y);
+	for (fixIt.toFirst(); fixIt; ++fixIt) {
+		f = fixIt.current();
+/*		printf("f->rect() : (x,y)=%d,%d, (w,h)=%d,%d\n", 
+			f->rect().x(),
+			f->rect().y(),
+			f->rect().width(),
+			f->rect().height()
+			); */
+		if (f->rect().contains( QPoint( x, y) )) {
+			unSelectAll();
+			view->selectFix(f);
+			found = TRUE;
+			break;
+			}
+		}
+
+	for (mobIt.toFirst(); mobIt; ++mobIt) {
+		m = mobIt.current();
+/*		printf("m->rect() : (x,y)=%d,%d, (w,h)=%d,%d\n", 
+			m->rect().x(),
+			m->rect().y(),
+			m->rect().width(),
+			m->rect().height()
+			); */
+		if (m->rect().contains( QPoint( x, y) )) {
+			unSelectFix();
+		
+			if ((e->state()&ControlButton) && view->mobSelected.find(mobIt.currentKey()))
+				unSelectMob(mobIt.currentKey());
+			else
+				view->selectMob(mobIt.currentKey(), m);
+			found = TRUE;
+			}
+		}
+
+	if (!found) {
+	// Here, we have to draw a "selection box"...
+		view->setSelectionMode( SELECT_RECT);
+		oldX = selectX = e->x();
+		oldY = selectY = e->y();
+		unSelectFix();
+		}
+	}
+}
+
+void fieldMap::mouseMoveEvent(QMouseEvent *e)
+{
+QPainter p;
+QPen pen(green, 2);
+
+if (SELECT_RECT != view->getSelectionMode()) return;
+
+p.begin(this);
+p.setPen(pen);
+p.setRasterOp(XorROP);
+/* erase previous rect */
+if (oldX != selectX && oldY != selectY)	
+	drawRectSelect(selectX, selectY, oldX, oldY, p);
+/* draw present rect */
+oldX = e->x();
+oldY = e->y();
+if (oldX != selectX && oldY != selectY)	
+	drawRectSelect(selectX, selectY, oldX, oldY, p);
+p.end();
+}
+
+void fieldMap::mouseReleaseEvent(QMouseEvent *e)
+{
+QPainter p;
+QPen pen(green, 2);
+QIntDictIterator<visualMobUnit> mobIt(mobileList);
+QIntDictIterator<visualFacility> fixIt(facilityList);
+visualMobUnit	*m;
+int		t;
+
+if (SELECT_RECT != view->getSelectionMode()) return;
+
+p.begin(this);
+p.setPen(pen);
+p.setRasterOp(XorROP);
+/* erase rect */
+if (oldX != selectX && oldY != selectY)	
+	drawRectSelect(selectX, selectY, oldX, oldY, p);
+p.end();
+view->setSelectionMode( SELECT_NONE);
+
+/* generate multiple selection */
+
+if (selectX > oldX) {
+	t = oldX; 
+	oldX = selectX;
+	selectX = t;
+	}
+
+if (selectY > oldY) {
+	t = oldY; 
+	oldY = selectY;
+	selectY = t;
+	}
+
+selectX += BO_TILE_SIZE * view->X();
+selectY += BO_TILE_SIZE * view->Y();
+oldX += BO_TILE_SIZE * view->X();
+oldY += BO_TILE_SIZE *view->Y();
+	
+for (mobIt.toFirst(); mobIt; ++mobIt) {
+	m = mobIt.current();
+	if (selectX<=m->_x() && oldX>m->_x() + m->getWidth() &&
+	    selectY<=m->_y() && oldY>m->_y() + m->getHeight() && !view->mobSelected.find(mobIt.currentKey()) ) {
+		view->selectMob(mobIt.currentKey(), m);
+		}
+	}
+
+selectX -= BO_TILE_SIZE * view->X();
+selectY -= BO_TILE_SIZE * view->Y();
+oldX -= BO_TILE_SIZE * view->X();
+oldY -= BO_TILE_SIZE * view->Y();
+repaint (selectX, selectY, oldX, oldY, FALSE);
+}
+
+void fieldMap::resizeEvent(QResizeEvent *e)
+{
+emit reSizeView((width()+BO_TILE_SIZE+1)/BO_TILE_SIZE, (height()+BO_TILE_SIZE+1)/BO_TILE_SIZE);
+}
+
+
+
+
+void fieldMap::unSelectAll(void)
+{
+QIntDictIterator<visualMobUnit> selIt(view->mobSelected);
+
+
+/* deal with fix */
+unSelectFix();
+
+/* deal with mobiles */
+for (selIt.toFirst(); selIt;) { // ++ not needed, selIt should be increased
+	selIt.current()->unSelect();
+	unSelectMob(selIt.currentKey()); // by the .remove() in unselect
+	}
+boAssert(view->mobSelected.isEmpty());
+if (!view->mobSelected.isEmpty()) view->mobSelected.clear();
+
+view->unSelectAll();
+//logf(LOG_INFO, "deselecting all");
+}
+
+///orzel : those two should become inlined in .h
+void fieldMap::unSelectFix(void)
+{
+	view->unSelectFix();
+}
+
+void fieldMap::unSelectMob(long key)
+{
+	view->unSelectMob(key);
+}
---
+++ code/editor/mainView.cpp
@@ -0,0 +1,55 @@
+/***************************************************************************
+                       boson/editor/mainView.cpp -  description 
+                             -------------------                                         
+
+    version              : $Id$
+    begin                : Mon Apr 19 23:56:00 CET 1999
+                                           
+    copyright            : (C) 1999 by Thomas Capricelli                         
+    email                : capricel@enst.fr                                     
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   * 
+ *                                                                         *
+ ***************************************************************************/
+
+#include <qlayout.h>
+
+#include "infoWin.h"
+#include "playerMap.h"
+#include "miniMap.h"
+#include "fieldMap.h"
+#include "viewMap.h"
+
+#include "mainView.h"		// myself
+
+mainView::mainView(playerMap *phys, QWidget *parent=0, const char *name=0)
+	:QWidget(parent, name)
+{ 
+	QHBoxLayout	*topLayout = new QHBoxLayout(this);
+	QVBoxLayout	*leftLayout = new QVBoxLayout();
+
+	topLayout->addLayout(leftLayout,0);
+
+		view = new viewMap(phys); // the view associated with this window
+		mini = new miniMap(view, this);
+		mini->setFixedSize(200,200);
+		leftLayout->addWidget(mini);
+
+		info = new infoWin(this, "infowin");
+		leftLayout->addWidget(info, 10);
+
+/* This is the main map, the game area */
+	field = new fieldMap(view, this);
+	topLayout->addWidget(field,10);
+
+/* finish the stuff */
+//	leftLayout->addStretch(10);
+	topLayout->activate();
+}
+
