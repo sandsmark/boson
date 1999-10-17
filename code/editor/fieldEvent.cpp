eeff2fd2db78d23e2f38391082038f2240c99dc9
orzel <orzel@14664f6f-540d-0410-967e-c81f6ba7cd39>
1999-10-17 04:47:37 +0000

    move most of it under visual/ Now use the new function findUnitAt() rectangle selection entirely moved into visual/

--- code/editor/fieldEvent.cpp
+++ code/editor/fieldEvent.cpp
@@ -34,9 +34,6 @@
 #define facilityList	(((editorField*)(view->field))->facility)
 #define eMap		(((editorField*)(view->field))->map)
 
-static int selectX, selectY;
-static int oldX, oldY;
-
 /* ugly orzel hack......*/
 void visualBigDisplay::mousePressEvent(QMouseEvent *e)
 {
@@ -45,202 +42,77 @@ void visualBigDisplay::mousePressEvent(QMouseEvent *e)
 
 void editorBigDisplay::mousePressEvent(QMouseEvent *e)
 {
-int x, y;
 
-bool found = FALSE;
-
-x = e->x();
-y = e->y();
-
-
-if (e->button() & MidButton) {
-	emit relativeReCenterView( x/BO_TILE_SIZE , y/BO_TILE_SIZE);
-	return;
-	}
-
-if (e->button() & LeftButton) {	
-	/* Here we transpose coo into the map referential */
-	x += view->X()*BO_TILE_SIZE; y += view->Y()*BO_TILE_SIZE;
-
-	/* Control -> multiselection, else... */
-	if (! (e->state()&ControlButton)) {
-		unSelectAll();
-	}
-
-	QIntDictIterator<visualMobUnit> mobIt(mobileList);
-	QIntDictIterator<visualFacility> fixIt(facilityList);
-
-	visualMobUnit	*m;
-	visualFacility	*f;
-
-//printf("\n\nselection , x=%d,y=%d\n", x, y);
-	for (fixIt.toFirst(); fixIt; ++fixIt) {
-		f = fixIt.current();
-/*		printf("f->rect() : (x,y)=%d,%d, (w,h)=%d,%d\n", 
-			f->rect().x(),
-			f->rect().y(),
-			f->rect().width(),
-			f->rect().height()
-			); */
-		if (f->rect().contains( QPoint( x, y) )) {
-			unSelectAll();
-			view->selectFix(f);
-			found = TRUE;
-			break;
-		}
-	}
-
-	for (mobIt.toFirst(); mobIt; ++mobIt) {
-		m = mobIt.current();
-/*		printf("m->rect() : (x,y)=%d,%d, (w,h)=%d,%d\n", 
-			m->rect().x(),
-			m->rect().y(),
-			m->rect().width(),
-			m->rect().height()
-			); */
-		if (m->rect().contains( QPoint( x, y) )) {
-			unSelectFix();
-		
-			if ((e->state()&ControlButton) && view->mobSelected.find(mobIt.currentKey()))
-				unSelectMob(mobIt.currentKey());
-			else
-				view->selectMob(mobIt.currentKey(), m);
-			found = TRUE;
-		}
-	}
-
-	if (!found) {
-	// Here, we have to draw a "selection box"...
-		view->setSelectionMode( SELECT_RECT);
-		oldX = selectX = e->x();
-		oldY = selectY = e->y();
-		unSelectFix();
-	}
-} /* left button */
-
-if (e->button() & MidButton) {
-	x = e->x() / BO_TILE_SIZE; y = e->y() / BO_TILE_SIZE;
-	x += view->X(); y += view->Y();
-	boAssert(x>0);
-	boAssert(x<eMap.width);
-	boAssert(y>0);
-	boAssert(y<eMap.height);
-	selectedCell = &(eMap.cells[x][y]);
-	popup->exec(QCursor::pos());
-	return;
-}
-}
-
-void visualBigDisplay::mouseMoveEvent(QMouseEvent *e)
-{
-QPainter p;
-QPen pen(green, 2);
-
-if (SELECT_RECT != view->getSelectionMode()) return;
-
-p.begin(this);
-p.setPen(pen);
-p.setRasterOp(XorROP);
-/* erase previous rect */
-if (oldX != selectX && oldY != selectY)	
-	drawRectSelect(selectX, selectY, oldX, oldY, p);
-/* draw present rect */
-oldX = e->x();
-oldY = e->y();
-if (oldX != selectX && oldY != selectY)	
-	drawRectSelect(selectX, selectY, oldX, oldY, p);
-p.end();
-}
-
-void visualBigDisplay::mouseReleaseEvent(QMouseEvent *e)
-{
-QPainter p;
-QPen pen(green, 2);
-QIntDictIterator<visualMobUnit> mobIt(mobileList);
-QIntDictIterator<visualFacility> fixIt(facilityList);
-visualMobUnit	*m;
-int		t;
-
-if (SELECT_RECT != view->getSelectionMode()) return;
-
-p.begin(this);
-p.setPen(pen);
-p.setRasterOp(XorROP);
-/* erase rect */
-if (oldX != selectX && oldY != selectY)	
-	drawRectSelect(selectX, selectY, oldX, oldY, p);
-p.end();
-view->setSelectionMode( SELECT_NONE);
-
-/* generate multiple selection */
-
-if (selectX > oldX) {
-	t = oldX; 
-	oldX = selectX;
-	selectX = t;
-	}
-
-if (selectY > oldY) {
-	t = oldY; 
-	oldY = selectY;
-	selectY = t;
-	}
-
-selectX += BO_TILE_SIZE * view->X();
-selectY += BO_TILE_SIZE * view->Y();
-oldX += BO_TILE_SIZE * view->X();
-oldY += BO_TILE_SIZE *view->Y();
+	int x, y;
 	
-for (mobIt.toFirst(); mobIt; ++mobIt) {
-	m = mobIt.current();
-	if (selectX<=m->_x() && oldX>m->_x() + m->getWidth() &&
-	    selectY<=m->_y() && oldY>m->_y() + m->getHeight() && !view->mobSelected.find(mobIt.currentKey()) ) {
-		view->selectMob(mobIt.currentKey(), m);
+	x = e->x();
+	y = e->y();
+	
+	if (e->button() & MidButton) {
+		emit relativeReCenterView( x/BO_TILE_SIZE , y/BO_TILE_SIZE);
+		return;
 		}
+
+	/* Now we transpose coo into the map referential */
+	x += view->X()*BO_TILE_SIZE; y += view->Y()*BO_TILE_SIZE;
+	
+	if (e->button() & LeftButton) {	
+		/* Control -> multiselection, else... */
+		if (! (e->state()&ControlButton)) {
+			unSelectAll();
+			}
+	
+		QwSpriteFieldGraphic *sfg = view->field->findUnitAt( x, y);
+
+		if (!sfg) {
+			// nothing has been found : it's a ground-click
+			// Here, we have to draw a "selection box"...
+			view->setSelectionMode( SELECT_RECT);
+			oldX = selectX = e->x();
+			oldY = selectY = e->y();
+			unSelectFix();
+			return;
+		}
+	
+	
+		if ( IS_MOBILE(sfg->rtti())) {
+			visualMobUnit *m = (visualMobUnit *) sfg;
+	
+			unSelectFix();
+			if ((e->state()&ControlButton) && view->mobSelected.find(m->key))
+				unSelectMob(m->key);
+			else
+				view->selectMob(m->key, m);
+
+			return;
+		}
+
+		if ( IS_FACILITY(sfg->rtti())) {
+			visualFacility *f = (visualFacility *) sfg;
+			unSelectAll();		// anyway 
+			view->selectFix(f);
+
+			return;
+		}
+
+		// should never be reached !
+		logf(LOG_ERROR, "boson/fieldEvent.c, unexpeted field->findUnitAt() result");
+	
+	} // LeftButton 
+
+
+	if (e->button() & RightButton) {
+		x = e->x() / BO_TILE_SIZE; y = e->y() / BO_TILE_SIZE;
+		x += view->X(); y += view->Y();
+		boAssert(x>0);
+		boAssert(x<eMap.width);
+		boAssert(y>0);
+		boAssert(y<eMap.height);
+		selectedCell = &(eMap.cells[x][y]);
+		popup->exec(QCursor::pos());
+		return;
 	}
-
-selectX -= BO_TILE_SIZE * view->X();
-selectY -= BO_TILE_SIZE * view->Y();
-oldX -= BO_TILE_SIZE * view->X();
-oldY -= BO_TILE_SIZE * view->Y();
-repaint (selectX, selectY, oldX, oldY, FALSE);
-}
-
-void visualBigDisplay::resizeEvent(QResizeEvent *e)
-{
-emit reSizeView((width()+BO_TILE_SIZE+1)/BO_TILE_SIZE, (height()+BO_TILE_SIZE+1)/BO_TILE_SIZE);
+	
 }
 
 
-
-
-void visualBigDisplay::unSelectAll(void)
-{
-QIntDictIterator<visualMobUnit> selIt(view->mobSelected);
-
-
-/* deal with fix */
-unSelectFix();
-
-/* deal with mobiles */
-for (selIt.toFirst(); selIt;) { // ++ not needed, selIt should be increased
-	selIt.current()->unSelect();
-	unSelectMob(selIt.currentKey()); // by the .remove() in unselect
-	}
-boAssert(view->mobSelected.isEmpty());
-if (!view->mobSelected.isEmpty()) view->mobSelected.clear();
-
-view->unSelectAll();
-//logf(LOG_INFO, "deselecting all");
-}
-
-///orzel : those two should become inlined in .h
-void visualBigDisplay::unSelectFix(void)
-{
-	view->unSelectFix();
-}
-
-void visualBigDisplay::unSelectMob(long key)
-{
-	view->unSelectMob(key);
-}
