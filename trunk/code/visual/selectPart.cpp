b032c7a108084094f4074ef2420728d54e8ed051
orzel <orzel@14664f6f-540d-0410-967e-c81f6ba7cd39>
2000-03-01 05:05:38 +0000

    displayed parts length is now dependant on frame #

--- trunk/code/visual/selectPart.cpp
+++ trunk/code/visual/selectPart.cpp
@@ -30,18 +30,19 @@
 #define SP_W		35
 #define SP_H		(SP_CORNER_LEN+SP_CORNER_POS)
 
-static void drawSelectBox(QPainter &painter, QColor c1, QColor c2);
+static void drawSelectBox(QPainter &painter, QColor c1, QColor c2, int power);
 
 QwSpritePixmapSequence * selectPart_up::qsps = 0l;
 QwSpritePixmapSequence * selectPart_down::qsps = 0l;
 
 
-void drawSelectBox(QPainter &painter, QColor c1, QColor c2)
+void drawSelectBox(QPainter &painter, QColor c1, QColor c2, int power)
 {
-	QPen pen(red);
+	QPen	pen(red);
+	int	len =  2 * ( PART_NB-power);
 
 	painter.setPen(pen);
-	painter.fillRect(0,0,SP_W,SP_THICK,c1);
+	painter.fillRect(len,0, SP_W - len ,SP_THICK,c1);
 	painter.fillRect(
 		SP_W - SP_CORNER_LEN	, SP_CORNER_POS,
 		SP_CORNER_LEN		, SP_THICK, c2);
@@ -77,22 +78,26 @@ void selectPart_up::initStatic()
 	QPainter	painter;
 
 	pix = new QPixmap(SP_W, SP_H);
-	pix->fill();
-
-	painter.begin(pix);
-	drawSelectBox(painter, red, green);
-	painter.end();
-
 	mask = new QBitmap(SP_W, SP_H);
-	mask->fill(black);
-
-	painter.begin(mask);
-	drawSelectBox(painter, white, white);
-	painter.end();
-
-	pix->setMask(*mask);
 
 	for(i=0; i<PART_NB; i++) {
+		
+		/* draw it */
+		pix->fill();
+	
+		painter.begin(pix);
+		drawSelectBox(painter, red, green, i);
+		painter.end();
+	
+		/* draw the mask */
+		mask->fill(black);
+	
+		painter.begin(mask);
+		drawSelectBox(painter, white, white, i);
+		painter.end();
+	
+		/* create entries in QList */
+		pix->setMask(*mask);
 		pix = new QPixmap(*pix);
 		pixmaps.append (pix);
 		point = new QPoint(SP_W-2, SP_CORNER_POS);
@@ -101,6 +106,9 @@ void selectPart_up::initStatic()
 
 //	delete mask; ///orzel : shoud I ?
 	qsps = new QwSpritePixmapSequence(pixmaps,points);
+
+	delete mask;
+	delete pix;
 }
 
 
@@ -129,35 +137,44 @@ void selectPart_down::initStatic()
 	QPoint		*point;
 	QBitmap		*mask;
 	QPainter	painter;
-
+	
 	pix = new QPixmap(SP_W, SP_H);
-	pix->fill();
-
-	painter.begin(pix);
-	painter.rotate(180);
-	painter.translate(-SP_W+1, -SP_H+1);
-	drawSelectBox(painter, red, green);
-	painter.end();
-
-
 	mask = new QBitmap(SP_W, SP_H);
-	mask->fill(black);
-
-	painter.begin(mask);
-	painter.rotate(180);
-	painter.translate(-SP_W+1, -SP_H+1);
-	drawSelectBox(painter, white, white);
-	painter.end();
-
-	pix->setMask(*mask);
 
 	for(i=0; i<PART_NB; i++) {
+		
+		/* draw it */
+		pix->fill();
+	
+		painter.begin(pix);
+		painter.rotate(180);
+		painter.translate(-SP_W+1, -SP_H+1);
+		drawSelectBox(painter, red, green, i);
+		painter.end();
+	
+		/* draw the mask */
+		mask->fill(black);
+	
+		painter.begin(mask);
+		painter.rotate(180);
+		painter.translate(-SP_W+1, -SP_H+1);
+		drawSelectBox(painter, white, white, i);
+		painter.end();
+	
+		/* create entries in QList */
+		pix->setMask(*mask);
 		pix = new QPixmap(*pix);
 		pixmaps.append (pix);
 		point = new QPoint(1, SP_H-2 - SP_CORNER_POS);
 		points.append (point);
 		}
 
+
+
 //	delete mask; ///orzel : shoud I ?
 	qsps = new QwSpritePixmapSequence(pixmaps,points);
+
+	delete mask;
+	delete pix;
 }
+
