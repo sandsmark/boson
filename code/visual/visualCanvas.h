d800958641f33086f18621a64e97bf934675017d
orzel <orzel@14664f6f-540d-0410-967e-c81f6ba7cd39>
2000-05-23 02:21:30 +0000

    warning removed

--- code/visual/visualCanvas.h
+++ code/visual/visualCanvas.h
@@ -53,7 +53,7 @@ public:
 	void setCell(int i, int j, cell_t c);
 	/** find the unit at this position */
 	QCanvasItem		*findUnitAt(int x, int y);
-	groundType		groundAt(int x, int y) {return tile( x/tileWidth(), y/tileHeight() );}
+	groundType		groundAt(int x, int y) {return (groundType)tile( x/tileWidth(), y/tileHeight() );}
 	
 	virtual void resize (int, int);
 protected:
