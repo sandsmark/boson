b2f31ffbf12f262cb77b3d698b4c9bb21132a8c3
orzel <orzel@14664f6f-540d-0410-967e-c81f6ba7cd39>
2000-10-19 06:07:21 +0000

    documentation

--- trunk/code/common/map.h
+++ trunk/code/common/map.h
@@ -25,13 +25,18 @@
 #include "common/groundType.h"
 
 #define BO_TILE_SIZE 48
+
 /*
- * BIG_W		should be 4*n, cause this way it's easier to watch the generated file
- * NB_GROUND_TILES*4	is 1244 (on may, 14th 200), so BIG_H should be 39
+ * BIG_W,BIG_H
+ * number of pixmap in widht/height of the big picture (earth.png)
+ * should be 4*n, cause this way it's easier to watch the generated file
+ * NB_GROUND_TILES*4	is 1244 (on may, 14th 2000), so BIG_H should be 39
  */
 #define	BIG_W	32
 #define	BIG_H	( (NB_GROUND_TILES*4 + BIG_W-1) / BIG_W )
 
+// to get (x,y) position of a given ground 'g' in the big
+// pixmap used to store all ground pixmap
 #define GET_BIG_X(g) (( (g) % BIG_W) * BO_TILE_SIZE)
 #define GET_BIG_Y(g) (( (g) / BIG_W) * BO_TILE_SIZE)
 
