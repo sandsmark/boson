74ca5db905e43db602b1d27933de91b5599d154b
orzel <orzel@14664f6f-540d-0410-967e-c81f6ba7cd39>
2000-10-19 21:00:47 +0000

    new ranges, according to boDist()

--- code/common/unitType.cpp
+++ code/common/unitType.cpp
@@ -35,11 +35,11 @@
 */
 
 mobileProperties_t mobileProp[] = {
-	{"ship",		96, 96,  2, 100,	200, 100, 3, BO_GO_SEA},	// MOB_SHIP
-	{"quad",		32, 32,  3, 70,		200, 100, 2, BO_GO_EARTH},	// MOB_QUAD
+	{"ship",		96, 96,  2, 5,	200, 100, 3, BO_GO_SEA},	// MOB_SHIP
+	{"quad",		32, 32,  3, 2,		200, 100, 2, BO_GO_EARTH},	// MOB_QUAD
 	{"oilharvester",	64, 64,  4, 0,		200, 100, 1, BO_GO_EARTH},	// MOB_OIL_HARVESTER
 	{"mineralharvester",	64, 64,  4, 0,		200, 100, 1, BO_GO_EARTH},	// MOB_MINERAL_HARVESTER
-	{"aircraft",		72, 72,  3, 100,	200, 100, 3, BO_GO_AIR},	// MOB_AIRCRAFT
+	{"aircraft",		72, 72,  3, 4,	200, 100, 3, BO_GO_AIR},	// MOB_AIRCRAFT
 	};
 //	                        width    visibility     mineral   speed
 //	                            height  range            oil     goFlag
