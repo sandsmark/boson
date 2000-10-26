73fb11a8d2d38e7ad843314272ec50fbf984798b
orzel <orzel@14664f6f-540d-0410-967e-c81f6ba7cd39>
2000-10-26 17:56:42 +0000

    moved miniDisplay internal ground to _ground (conflict with other function) created sync() to update the whole minimap, don't know if it will ever be used..

--- code/visual/visualMiniDisplay.cpp
+++ code/visual/visualMiniDisplay.cpp
@@ -31,17 +31,59 @@ visualMiniDisplay::visualMiniDisplay(visualTopLevel *v, QWidget*parent, const ch
 {
 
 /* create the (back)ground pixmap */
-	ground = new QPixmap(v->maxX(), v->maxY());
-	ground->fill(black);
+	_ground = new QPixmap(v->maxX(), v->maxY());
+	_ground->fill(black);
 
 /* make the connection */
 	connect(vcanvas, SIGNAL(newCell(int,int, groundType)), this, SLOT(newCell(int,int, groundType)));
 	connect(vcanvas, SIGNAL(updateMobile(visualMobUnit *)), this, SLOT(drawMobile(visualMobUnit *)));
 	connect(vcanvas, SIGNAL(updateFix(visualFacility *)), this, SLOT(drawFix(visualFacility *)));
+	connect(vcanvas, SIGNAL(syncMini(void)), this, SLOT(sync(void)));
 
 // connect(, SIGNAL(), this, SLOT());
 	connect(this, SIGNAL(reCenterView(int, int)), vtl, SLOT(reCenterView(int, int)));
 	connect(this, SIGNAL(reSizeView(int, int)), vtl, SLOT(reSizeView(int, int)));
+}
+
+
+void visualMiniDisplay::sync(void)
+{
+
+	int i,j;
+	groundType g;
+	QPainter p;
+
+	p.begin(_ground);
+
+	for (i=0; i< vtl->maxX(); i++)
+		for (j=0; j< vtl->maxY(); j++) {
+
+			g = ground(vcanvas->tile(i,j));
+
+			if (IS_TRANS(g))
+				g = groundTransProp[ GET_TRANS_REF(g) ].from;
+
+			switch(g) {
+				default:
+					logf(LOG_ERROR, "visualMiniDisplay::drawCell : unexpected groundType : %d", g);
+				case GROUND_WATER :
+				case GROUND_WATER_OIL:
+					setPoint( i, j, blue, &p);
+					break;
+				case GROUND_GRASS :
+				case GROUND_GRASS_OIL :
+					setPoint( i, j, green, &p);
+					break;
+				case GROUND_DESERT :
+					setPoint( i, j, darkYellow, &p);
+					break;
+			}
+		}
+
+	p.end();
+
+	// update 
+	repaint(FALSE);
 }
 
 
@@ -52,7 +94,7 @@ void visualMiniDisplay::paintEvent(QPaintEvent *evt)
 	p.begin(this);
 
 	/* map is buffered */
-	p.drawPixmap(0,0,*ground);
+	p.drawPixmap(0,0,*_ground);
 
 	/* the little rectangle */
 	p.setPen(white);
@@ -75,7 +117,7 @@ void visualMiniDisplay::newCell(int i, int j, groundType g) //, QPainter *p)
 	if (IS_TRANS(g))
 		g = groundTransProp[ GET_TRANS_REF(g) ].from;
 
-	p.begin(ground);
+	p.begin(_ground);
 	switch(g) {
 		default:
 			logf(LOG_ERROR, "visualMiniDisplay::drawCell : unexpected groundType : %d", g);
