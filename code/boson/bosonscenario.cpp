3010efcf9ebb897dcc2ad779245022f0c6f35913
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2001-11-15 18:47:06 +0000

    - emm... a lot of changes! let's keep it short: XML, compression

--- code/boson/bosonscenario.cpp
+++ code/boson/bosonscenario.cpp
@@ -5,22 +5,18 @@
 #include "player.h"
 #include "boson.h"
 
-#include <qfile.h>
-#include <qdatastream.h>
-
-#include <kgame/kgamepropertyhandler.h>
-#include <kgame/kgameproperty.h>
 #include <kdebug.h>
 #include <kstandarddirs.h>
+#include <kfilterdev.h>
+
+#include <qfile.h>
+#include <qdatastream.h>
+#include <qdom.h>
 
 #include "defines.h"
 
-#define TAG_FIELD "boeditor_magic_0_3" // obsolete
-#define TAG_FIELD_LEN 18 // len of above // obsolete
-
-#define TAG_FIX (0xbe) // TODO
-#define TAG_MOB (0xba) // TODO
-//#define TAG_UNIT (0xba) // TODO
+#define TAG_FIELD "bosonscenario_magic_0_6"
+#define TAG_UNIT (0xba)
 
 struct ScenarioUnit
 {
@@ -40,11 +36,9 @@ public:
 	
 	QString mFileName;
 
-	KGamePropertyHandler mProperties;
-
-	KGameProperty<int> mMaxPlayers; // -1 == unlimited
-	KGameProperty<unsigned int> mMinPlayers;
-//	KGameProperty<QString> mWorldName; // TODO: i18n - see TODO file
+	int mMaxPlayers; // -1 == unlimited
+	unsigned int mMinPlayers;
+//	QString mWorldName; // TODO: i18n - see TODO file
 
 	QValueList<ScenarioUnit>* mUnits;
 };
@@ -72,14 +66,6 @@ BosonScenario::~BosonScenario()
 void BosonScenario::init()
 {
  d = new BosonScenarioPrivate;
- // note we do not register d->mProperties anywhere as we don't use send() or
- // emitSignal from KGameProperty. They are just used for saving the file.
- d->mMaxPlayers.registerData(IdMaxPlayers, dataHandler(),
-		KGamePropertyBase::PolicyLocal, "MaxPlayers");
- d->mMinPlayers.registerData(IdMinPlayers, dataHandler(),
-		KGamePropertyBase::PolicyLocal, "MinPlayers");
-// d->mWorldName.registerData(IdWorldName, dataHandler(),
-//		KGamePropertyBase::PolicyLocal, "WorldName");
 }
 
 QString BosonScenario::defaultScenario()
@@ -87,125 +73,490 @@ QString BosonScenario::defaultScenario()
  return locate("data", "boson/scenario/basic.bsc");
 }
 
-KGamePropertyHandler* BosonScenario::dataHandler() const
-{
- return &d->mProperties;
-}
-
 bool BosonScenario::loadScenario(const QString& fileName)
 {
  kdDebug() << "BosonScenario::loadScenario " << fileName << endl;
 
  // open stream 
- QFile f(fileName);
- if (!f.open(IO_ReadOnly)){
-	kdError() << "BosonScenario: Can't open file " << fileName << endl;
+ QIODevice* dev = KFilterDev::deviceForFile(fileName);
+ if (!dev) {
+	kdError() << "BosonScenario: NULL device for " << fileName << endl;
 	return false;
  }
- QDataStream stream(&f);
- bool ret = loadScenario(stream);
- if (ret) {
-	d->mFileName = fileName;
+ if (!dev->open(IO_ReadOnly)) {
+	kdError() << "BosonScenario: Could not open " << fileName << endl;
+	delete dev;
+	return false;
  }
- f.close();
- return ret;
+ QByteArray buffer = dev->readAll();
+ dev->close();
+ delete dev;
+ d->mFileName = fileName;
+
+ QDataStream stream(buffer, IO_ReadOnly);
+ if (verifyScenario(stream)) { // binary file
+	if (!loadScenarioSettings(stream)) {
+		kdError() << "Could not load scenario settings" << endl;
+		return false;
+	}
+	if (!loadPlayers(stream)) {
+		kdError() << "Could not load scenario players" << endl;
+		return false;
+	}
+	kdDebug() << "loading done - save now..." << endl;
+	return true;
+ }
+ // XML file
+ QDomDocument doc("BosonScenario");
+ QString errorMsg;
+ int lineNo;
+ int columnNo;
+ if (!doc.setContent(buffer, false, &errorMsg, &lineNo, &columnNo)) {
+	kdError() << "Parse error in line " << lineNo << ",column " << columnNo
+			<< " error message: " << errorMsg << endl;
+	return false;
+ }
+ QDomNodeList list;
+ QDomElement root = doc.documentElement();
+ list = root.elementsByTagName("ScenarioSettings");
+ if (list.count() != 1) {
+	kdError() << "Cannot have ScenarioSettings " << list.count() 
+			<< " times"<< endl;
+	return false;
+ }
+ QDomElement settings = list.item(0).toElement();
+ if (settings.isNull()) {
+	kdError() << "settings is not a QDomElement" << endl;
+	return false;
+ }
+ if (!loadScenarioSettings(settings)) {
+	kdError() << "Could not load scenario settings" << endl;
+	return false;
+ }
+
+ list = root.elementsByTagName("ScenarioPlayers");
+ if (list.count() != 1) {
+	kdError() << "Cannot have ScenarioPlayers " << list.count() 
+			<< " times"<< endl;
+	return false;
+ }
+ QDomElement players = list.item(0).toElement();
+ if (players.isNull()) {
+	kdError() << "players is not a QDomElement" << endl;
+	return false;
+ }
+ if (!loadPlayers(players)) {
+	kdError() << "Could not load scenario players" << endl;
+	return false;
+ }
+
+ return false;
 }
 
-bool BosonScenario::loadScenario(QDataStream& stream)
+bool BosonScenario::saveScenario(const QString& fileName, bool binary)
 {
-/*
- if (!verifyScenario(stream)) {//TODO
-	kdError() << "Invalid map file" << endl;
-	return false;
- }*/
+ if (binary) {
+	QIODevice* dev = KFilterDev::deviceForFile(fileName);
+	if (!dev) {
+		kdError() << "BosonScenario: NULL device for " << fileName << endl;
+		return false;
+	}
+	if (!dev->open(IO_WriteOnly)) {
+		kdError() << "BosonScenario: Could not open " << fileName << endl;
+		delete dev;
+		return false;
+	}
+	QDataStream stream(dev);
+	bool ret = saveScenario(stream);
+	dev->close();
+	delete dev;
+	return ret;
+ }
 
+ // now save the file
+ QIODevice* dev = KFilterDev::deviceForFile(fileName, "application/x-gzip");
+ if (!dev) {
+	kdError() << "BosonScenario: NULL device for " << fileName << endl;
+	return false;
+ }
+ if (!dev->open(IO_WriteOnly)) {
+	kdError() << "BosonScenario: Could not open " << fileName << endl;
+	delete dev;
+	return false;
+ }
+ saveXMLScenario(dev);
+ dev->close();
+ delete dev;
+ return true;
+}
+
+bool BosonScenario::saveScenario(QDataStream& stream)
+{
+ if (!saveValidityHeader(stream)) {
+	kdError() << "Could not write header" << endl;
+	return false;
+ }
+ if (!saveScenarioSettings(stream)) {
+	kdError() << "Could not write settings" << endl;
+	return false;
+ }
+ if (!savePlayers(stream)) {
+	kdError() << "Could not write players" << endl;
+	return false;
+ }
+ return true;
+}
+
+bool BosonScenario::saveXMLScenario(QIODevice* dev)
+{
+ if (!dev) {
+	kdError() << "NULL device" << endl;
+	return false;
+ }
+ QDomDocument doc("BosonScenario");
+ QDomElement root = doc.createElement("BosonScenario");
+ doc.appendChild(root);
+
+ QDomElement settings = doc.createElement("ScenarioSettings");
+ root.appendChild(settings);
+ 
+ if (!saveScenarioSettings(settings)) {
+	kdError() << "Could not save scenario settings to XML" << endl;
+	return false;
+ }
+
+ QDomElement players = doc.createElement("ScenarioPlayers");
+ root.appendChild(players);
+ if (!savePlayers(players)) {
+	kdError() << "Could not save players to XML" << endl;
+	return false;
+ }
+
+ QString xml = doc.toString();
+ dev->writeBlock(xml.data(), xml.length()); // FIXME: or xml.latin1() // or something else?
+ return true;
+}
+
+bool BosonScenario::saveScenarioSettings(QDomElement& node)
+{
+ QDomDocument doc = node.ownerDocument();
+ node.setAttribute("MinPlayers", minPlayers());
+ node.setAttribute("MaxPlayers", maxPlayers());
+ return true;
+}
+
+bool BosonScenario::loadScenarioSettings(QDomElement& node)
+{
+ if (!node.hasAttribute("MinPlayers")) {
+	kdError() << "Missing MinPlayers" << endl;
+	return false;
+ }
+ if (!node.hasAttribute("MaxPlayers")) {
+	kdError() << "Missing MaxPlayers" << endl;
+	return false;
+ }
+
+ QByteArray buffer;
+ QDataStream stream(buffer, IO_WriteOnly);
+
+ // TODO: check if attrtibute actually was int
+ Q_UINT32 min = node.attribute("MinPlayers").toUInt();
+ Q_INT32 max = node.attribute("MaxPlayers").toInt();
+ 
+ stream << min;
+ stream << max;
+ 
+ QDataStream readStream(buffer, IO_ReadOnly);
+ loadScenarioSettings(readStream);
+ return true;
+}
+
+bool BosonScenario::loadScenarioSettings(QDataStream& stream)
+{
  Q_UINT32 minPlayers;
  Q_INT32 maxPlayers;
  stream >> minPlayers;
  stream >> maxPlayers;
-
+ 
  if (minPlayers < 1) {
-	kdError() << "BosonScenario::loadCompleteScenario(): broken scenario file!" << endl;
+	kdError() << "BosonScenario::loadScenarioSettings(): broken scenario file!" << endl;
 	kdError() << "minPlayers < 1" << endl;
 	return false;
  }
  if (maxPlayers > BOSON_MAX_PLAYERS) {
-	kdError() << "BosonScenario::loadCompleteScenario(): broken scenario file!" << endl;
+	kdError() << "BosonScenario::loadScenarioSettings(): broken scenario file!" << endl;
 	kdError() << "maxPlayers > " << BOSON_MAX_PLAYERS << endl;
 	return false;
  }
  if ((int)minPlayers > maxPlayers) {
-	kdError() << "BosonScenario::loadCompleteScenario(): broken scenario file!" << endl;
+	kdError() << "BosonScenario::loadScenarioSettings(): broken scenario file!" << endl;
 	kdError() << "minPlayers > maxPlayers" << endl;
 	return false;
  }
  d->mMinPlayers = minPlayers;
  d->mMaxPlayers = maxPlayers;
-
-
  d->mUnits = new QValueList<ScenarioUnit>[d->mMaxPlayers];
- for (int i = 0; i < d->mMaxPlayers; i++) {
-	loadUnits(stream, i);
+ return true;
+}
+
+
+bool BosonScenario::saveScenarioSettings(QDataStream& stream)
+{
+ stream << (Q_UINT32)minPlayers();
+ stream << (Q_UINT32)maxPlayers();
+ return true;
+}
+
+bool BosonScenario::loadPlayers(QDataStream& stream)
+{
+ if (!d->mUnits) {
+	kdError() << "NULL units" << endl;
+	return false;
  }
- 
+ for (int i = 0; i < d->mMaxPlayers; i++) {
+	if (!loadPlayer(stream, i)) {
+		kdError() << "Could not load player " << i << endl;
+		return false;
+	}
+ }
+ return true;
+}
+
+bool BosonScenario::loadPlayers(QDomElement& node)
+{
+ if (!d->mUnits) {
+	kdError() << "NULL units" << endl;
+	return false;
+ }
+ QDomNodeList list = node.elementsByTagName("Player");
+ if (list.count() < minPlayers()) {
+	kdError() << "Not enough players in file - expected " << minPlayers() 
+			<< ", found " << list.count() << endl;
+	return false;
+ }
+ if ((int)list.count() > maxPlayers()) {
+	kdWarning() << "Too many players in file. Read only " << maxPlayers() 
+			<< endl;
+ }
+
+ for (int unsigned i = 0; i < list.count(); i++) {
+	QDomElement player = list.item(i).toElement();
+	if (player.isNull()) {
+		kdError() << i << " is not a QDomElement" << endl;
+		return false;
+	}
+	if (!loadPlayer(player)) {
+		kdError() << "Could not load player " << i << endl;
+		return false;
+	}
+ }
+ return true;
+}
+
+bool BosonScenario::loadPlayer(QDomElement& node)
+{
+ if (!node.hasAttribute("PlayerNumber")) {
+	kdError() << "Missing PlayerNumber" << endl;
+	return false;
+ }
+ QByteArray buffer;
+ QDataStream stream(buffer, IO_WriteOnly);
+ QDomNodeList list = node.elementsByTagName("Unit");
+ stream << (Q_UINT32)list.count();
+ for (unsigned int i = 0; i < list.count(); i++) {
+	QDomElement unit = list.item(i).toElement();
+	if (unit.isNull()) {
+		kdError() << "Unit is not a QDomElement" << endl;
+		return false;
+	}
+	if (!unit.hasAttribute("UnitType")) {
+		kdError() << "Missing UnitType" << endl;
+		return false;
+	}
+	if (!unit.hasAttribute("x")) {
+		kdError() << "Missing x" << endl;
+		return false;
+	}
+	if (!unit.hasAttribute("y")) {
+		kdError() << "Missing y" << endl;
+		return false;
+	}
+	// TODO: check if these were actually numbers
+	Q_INT32 unitType;
+	Q_INT32 x;
+	Q_INT32 y;
+
+	unitType = unit.attribute("UnitType").toInt();
+	x = unit.attribute("x").toInt();
+	y = unit.attribute("y").toInt();
+	
+	stream << (Q_INT32)TAG_UNIT;
+	stream << unitType;
+	stream << x;
+	stream << y;
+ }
+
+ // TODO: check if this was a number
+ unsigned int playerNumber = node.attribute("PlayerNumber").toUInt();
+
+ QDataStream readStream(buffer, IO_ReadOnly);
+ return loadPlayer(readStream, playerNumber);
+}
+
+bool BosonScenario::savePlayers(QDomElement& node)
+{
+ QDomDocument doc = node.ownerDocument();
+
+ for (int i = 0; i < maxPlayers(); i++) {
+	QDomElement player = doc.createElement("Player");
+	node.appendChild(player);
+	if (!savePlayer(player, i)) {
+		kdError() << "Error saving units of player " << i << endl;
+		return false;
+	}
+ }
+ return true;
+}
+
+bool BosonScenario::savePlayers(QDataStream& stream)
+{
+ for (int i = 0; i < maxPlayers(); i++) {
+	if (!savePlayer(stream, i)) {
+		kdError() << "Error saving units of player " << i << endl;
+		return false;
+	}
+ }
+ return true;
+}
+
+bool BosonScenario::savePlayer(QDomElement& node, unsigned int playerNumber)
+{
+ QDomDocument doc = node.ownerDocument();
+ node.setAttribute("PlayerNumber", playerNumber);
+
+ if (!d->mUnits) {
+	kdError() << "NULL units" << endl;
+	return false;
+ }
+ if (d->mUnits->count() <= playerNumber) {
+	kdError() << "Unknown player " << playerNumber << endl;
+	return false;
+ }
+ QValueList<ScenarioUnit>::Iterator it;
+ for (it = d->mUnits[playerNumber].begin(); it != d->mUnits[playerNumber].end(); ++it) {
+	ScenarioUnit s = (*it);
+	QDomElement unit = doc.createElement("Unit");
+	node.appendChild(unit);
+	unit.setAttribute("UnitType", s.unitType);
+	unit.setAttribute("x", s.x);
+	unit.setAttribute("y", s.y);
+ }
+ return true;
+}
+
+bool BosonScenario::saveValidityHeader(QDataStream& stream)
+{
+ // magic
+ // Qt marshalling for a string is 4-byte-len + data
+
+ int length = QString(TAG_FIELD).length();
+ stream << (Q_INT32)(length - 1);
+
+ for (int i = 0; i <= length; i++) {
+	stream << (Q_INT8)TAG_FIELD[i];
+ }
  return true;
 }
 
 bool BosonScenario::verifyScenario(QDataStream& stream)
 {
- char magic[ TAG_FIELD_LEN+4 ];  // paranoic spaces
+ int length = QString(TAG_FIELD).length();
+ // FIXME: use QString/QCString or so instead
+ char* magic = new char[ length + 4 ];  // paranoic spaces
  int i;
 
  // magic 
  // Qt marshalling for a string is 4-byte-len + data
  stream >> i;
- if (TAG_FIELD_LEN + 1 != i) {
-	kdError() << "BosonScenario::verifyScenario(): Magic doesn't match(len), check file name" << endl;
+ if (length + 1 != i) {
+//	kdError() << "BosonScenario::verifyScenario(): Magic doesn't match(len), check file name" << endl;
+	delete[] magic;
 	return false;
  }
 
- for (i = 0; i < TAG_FIELD_LEN + 1; i++) {
+ for (i = 0; i < length + 1; i++) {
 	Q_INT8  b;
 	stream >> b;
 	magic[i] = b;
  }
 
- if (strncmp(magic, TAG_FIELD, TAG_FIELD_LEN) ) {
-	kdError() << "BosonScenario::verifyScenario(): Magic doesn't match(string), check file name" << endl;
+ if (!QString::compare(magic, TAG_FIELD)) {
+//	kdError() << "BosonScenario::verifyScenario(): Magic doesn't match(string), check file name" << endl;
+	delete[] magic;
 	return false;
  }
+ delete[] magic;
  return true;
 }
 
-bool BosonScenario::loadUnits(QDataStream& stream, unsigned int playerNumber)
+bool BosonScenario::loadPlayer(QDataStream& stream, unsigned int playerNumber)
 {
  if (!d->mUnits) {
-	kdError() << "BosonScenario::loadUnits(): Unit array not yet created" 
+	kdError() << "BosonScenario::loadPlayer(): Unit array not yet created" 
 			<< endl;
 	return false;
  }
- if (playerNumber >= maxPlayers()) {
-	kdError() << "BosonScenario::loadUnits(): don't know player " 
+ if ((int)playerNumber >= maxPlayers()) {
+	kdError() << "BosonScenario::loadPlayer(): don't know player " 
 			<< playerNumber << endl;
 	return false;
  }
  Q_UINT32 unitCount;
  stream >> unitCount;
  for (unsigned int i = 0; i < unitCount; i++) {
+	Q_INT32 tag_unit;
 	Q_INT32 unitType;
 	Q_INT32 x;
 	Q_INT32 y;
 
-//TODO: TAG_UNIT
+	stream >> tag_unit;
 	stream >> unitType;
 	stream >> x;
 	stream >> y;
+
+	if (tag_unit != TAG_UNIT) {
+		kdError() << "Missing TAG_UNIT" << endl;
+		return false;
+	}
 		
 	ScenarioUnit s;
 	s.unitType = unitType;
 	s.x = x;
 	s.y = y;
 	d->mUnits[playerNumber].append(s);
+ }
+ return true;
+}
+
+bool BosonScenario::savePlayer(QDataStream& stream, unsigned int playerNumber)
+{
+ // TODO
+ if (!d->mUnits) {
+	kdError() << "NULL units" << endl;
+	return false;
+ }
+ if (d->mUnits->count() >= playerNumber) {
+	kdError() << "Unknown player " << playerNumber << endl;
+	return false;
+ }
+ stream << (Q_UINT32)d->mUnits[playerNumber].count();
+ QValueList<ScenarioUnit>::Iterator it;
+ for (it = d->mUnits[playerNumber].begin(); it != d->mUnits[playerNumber].end(); ++it) {
+	ScenarioUnit s = (*it);
+	stream << (Q_INT32)TAG_UNIT;
+	stream << (Q_INT32)s.unitType;
+	stream << (Q_INT32)s.x;
+	stream << (Q_INT32)s.y;
  }
  return true;
 }
