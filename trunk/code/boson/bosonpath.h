80851dafad9bf59d435cf740050fcf4ec67c94ea
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2001-12-17 15:53:38 +0000

    - adding copyright header to all files  -> authors of most classes still missing. I'll do this later today or so

--- trunk/code/boson/bosonpath.h
+++ trunk/code/boson/bosonpath.h
@@ -1,3 +1,21 @@
+/*
+    This file is part of the Boson game
+    Copyright (C) 2001 The Boson Team (boson-devel@lists.sourceforge.net)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
 #ifndef __BOSONPATH_H__
 #define __BOSONPATH_H__
 
@@ -33,6 +51,7 @@ enum Direction
  * Boson's pathfinder class
  * This class is used to get best path from point a (start) to point b (goal)
  * It is used by units
+ * @author Rivo Laks <rivolaks@hot.ee>
  */
 class BosonPath
 {
--- trunk/code/boson/kgamedialogbosonconfig.h
+++ trunk/code/boson/kgamedialogbosonconfig.h
@@ -1,9 +1,30 @@
+/*
+    This file is part of the Boson game
+    Copyright (C) 2001 The Boson Team (boson-devel@lists.sourceforge.net)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
 #ifndef __KGAMEDIALOGBOSONCONFIG_H__
 #define __KGAMEDIALOGBOSONCONFIG_H__
 
 #include <kgame/kgamedialogconfig.h>
 
 class KGameDialogBosonConfigPrivate;
+/**
+ * @author Andreas Beckermann <b_mann@gmx.de>
+ **/
 class KGameDialogBosonConfig : public KGameDialogGeneralConfig
 {
 	Q_OBJECT
