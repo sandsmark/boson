62b2fcf9dc30f7791f2cb336509d9fdb609cabf1
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-02-16 22:38:16 +0000

    - we use QCanvasSprite as cursors now. also provide KCursor/QCursor as an   alternative. currently #ifdef'ed out

---
+++ trunk/code/boson/bosoncursor.cpp
@@ -0,0 +1,202 @@
+/*
+    This file is part of the Boson game
+    Copyright (C) 2002 The Boson Team (boson-devel@lists.sourceforge.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "bosoncursor.h"
+#include "defines.h"
+
+#include <qcanvas.h>
+#include <qbitmap.h>
+
+#include <kcursor.h>
+#include <kstandarddirs.h>
+#include <kdebug.h>
+
+#define PIXMAP_CURSOR 1 // support for pixmaps as cursor
+
+class BosonCursor::BosonCursorPrivate
+{
+public:
+	BosonCursorPrivate()
+	{
+		mQCursor = 0;
+		mCursor = 0;
+		
+		mCursorPixmaps = 0;
+
+		mCanvas = 0;
+	}
+
+	CursorMode mMode;
+
+	QCursor* mQCursor;
+	QCanvasSprite* mCursor;
+
+	QCanvasPixmapArray* mCursorPixmaps;
+
+	QCanvas* mCanvas;
+
+	QPixmap mCursorMove;
+	QPixmap mCursorAttack;
+};
+
+
+BosonCursor::BosonCursor()
+{
+ d = new BosonCursorPrivate;
+}
+
+BosonCursor::~BosonCursor()
+{
+ delete d->mQCursor;
+ delete d->mCursor;
+ delete d->mCursorPixmaps;
+ delete d;
+}
+
+void BosonCursor::setCursor(CursorMode mode)
+{
+ if (d->mMode == mode) {
+	return;
+ }
+ d->mMode = mode;
+#ifdef PIXMAP_CURSOR
+ switch (d->mMode) {
+	case Hide:
+	case Default: // we do not (yet) have a pixmap for this
+	case OwnUnit: // we do not (yet) have a pixmap for this
+		d->mCursor->hide();
+		break;
+	default:
+		d->mCursor->setFrame((int)mode);
+		d->mCursor->show();
+		break;
+
+ }
+#endif
+ if (d->mQCursor) {
+	delete d->mQCursor;
+ }
+ switch (d->mMode) {
+	case Attack:
+		d->mQCursor = new QCursor(d->mCursorAttack);
+		break;
+	case Move:
+		d->mQCursor = new QCursor(d->mCursorMove);
+		break;
+	case Hide:
+		d->mQCursor = new QCursor(QCursor::BlankCursor);
+		break;
+	case OwnUnit:
+	case Default:
+		d->mQCursor = new QCursor(KCursor::arrowCursor());
+		break;
+ }
+}
+
+void BosonCursor::setCursor(QWidget* w)
+{
+#ifndef PIXMAP_CURSOR
+ w->setCursor(cursor());
+#else
+ switch (cursorMode()) {
+	case Default:
+	case OwnUnit:
+		w->setCursor(cursor());
+		break;
+	default:
+		w->setCursor(QCursor::BlankCursor);
+		break;
+ }
+#endif
+}
+
+const QCursor& BosonCursor::cursor() const
+{
+ return *d->mQCursor;
+}
+
+BosonCursor::CursorMode BosonCursor::cursorMode() const
+{
+ return d->mMode;
+}
+
+void BosonCursor::setCanvas(QCanvas* canvas)
+{
+ if (d->mCanvas) {
+	kdError() << k_funcinfo << "Canvas already set" << endl;
+	return;
+ }
+ d->mCanvas = canvas;
+ loadCursors();
+}
+
+void BosonCursor::loadCursors()
+{
+ if (d->mCursor) {
+	kdWarning() << "cursors already loaded..." << endl;
+	delete d->mCursor;
+ }
+ QString dir = KGlobal::dirs()->findResourceDir("data", "boson/themes/cursors/move.png") + QString::fromLatin1("boson/themes/cursors");
+ QString move = dir + QString::fromLatin1("/move.png");
+ QString attack = dir + QString::fromLatin1("/attack.png");
+ if (d->mCursorMove.load(move)) {
+	QBitmap mask(move);
+	d->mCursorMove.setMask(mask);
+ } else {
+	kdError() << k_funcinfo << "Could not load " << move << endl;
+ }
+ if (d->mCursorAttack.load(attack)) {
+	QBitmap mask(move);
+	d->mCursorAttack.setMask(mask);
+ } else {
+	kdError() << k_funcinfo << "Could not load " << attack << endl;
+ }
+
+
+#ifdef PIXMAP_CURSOR
+ QValueList<QPixmap> pixmaps;
+ for (unsigned int i = 0; i < Hide; i++) {
+	switch ((CursorMode)i) {
+		case Attack:
+			pixmaps.append(d->mCursorAttack);
+			break;
+		case Move:
+			pixmaps.append(d->mCursorMove);
+			break;
+		case OwnUnit:
+		case Default:
+//			pixmaps.append();//???
+			break;
+		case Hide:
+			break;
+	}
+ }
+ d->mCursorPixmaps = new QCanvasPixmapArray(pixmaps);
+ d->mCursor = new QCanvasSprite(d->mCursorPixmaps, d->mCanvas);
+ d->mCursor->setZ(Z_CANVAS_CURSOR);
+#endif
+}
+
+void BosonCursor::move(double x, double y)
+{
+#ifdef PIXMAP_CURSOR
+ d->mCursor->move(x, y);
+#endif
+}
+
---
+++ trunk/code/boson/bosoncursor.h
@@ -0,0 +1,98 @@
+/*
+    This file is part of the Boson game
+    Copyright (C) 2002 The Boson Team (boson-devel@lists.sourceforge.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef __BOSONMUSIC_H__
+#define __BOSONMUSIC_H__
+
+//#include <qobject.h>
+
+class QCanvas;
+class QPoint;
+class QWidget;
+class QCursor;
+
+/**
+ **/
+class BosonCursor
+{
+//	Q_OBJECT
+public:
+	enum CursorMode {
+		Attack = 0,
+		Move = 1,
+		OwnUnit = 2,
+		Default = 3,
+		
+		Hide // this MUST be the last entry!
+	};
+	BosonCursor();
+	~BosonCursor();
+
+	CursorMode cursorMode() const;
+
+	/**
+	 * Change the current cursor. You probably want to call setCursor(this)
+	 * from your widget, too in order to get the @ref QCanvas cursor (if it
+	 * is used)
+	 **/
+	void setCursor(CursorMode mode);
+	
+	/**
+	 * Change the @ref QCursor cursor of the specified widget. This function
+	 * uses @ref QWidget::setCursor if the feature is compiled in. Note that
+	 * boson uses currently a #define to decide whether to use it or not.
+	 **/
+	void setCursor(QWidget* w);
+
+	/**
+	 * @return The current @ref QCursor according to @ref cursorMode
+	 **/
+	const QCursor& cursor() const;
+
+	/**
+	 * Set the canvas for the pixmap cursor. There are currently two
+	 * different cursors, on @ref QCursor (see @ref cursor) and one @ref
+	 * QCanvasSprite. The latter would be <em>far</em> nicer but also is
+	 * slower and maybe it is not working at all. You need to call setCanvas
+	 * for this experimental cursor.
+	 **/
+	void setCanvas(QCanvas* canvas);
+
+	/**
+	 * Move the @ref QCanvasSprite cursor.
+	 * @param x x-coordinate on the @ref QCanvas
+	 * @param y y-coordinate on the @ref QCanvas
+	 **/
+	void move(double x, double y);
+
+	/**
+	 * Load the cursor pixmaps for both types, the @ref QCanvasSprite
+	 * cursors and the @ref QCursor cursors.
+	 *
+	 * Automatically called by @ref setCanvas
+	 **/
+	void loadCursors();
+
+protected:
+
+private:
+	class BosonCursorPrivate;
+	BosonCursorPrivate* d;
+};
+
+#endif
