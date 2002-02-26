e92765861faa5ec05d27b4c081a51c2b37757892
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-02-26 17:54:13 +0000

    - decrease default game speed

--- trunk/code/boson/defines.h
+++ trunk/code/boson/defines.h
@@ -46,7 +46,7 @@
 
 #define MAX_GAME_SPEED 10 // the advance period - lower means faster
 #define MIN_GAME_SPEED 800 // the advance period - higher means slower
-#define DEFAULT_GAME_SPEED 50
+#define DEFAULT_GAME_SPEED 70
 
 
 #define Z_MOBILE 400
