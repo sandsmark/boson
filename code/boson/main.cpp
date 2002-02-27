60a367debf77ac6279fb7dd27581102983022217
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-02-27 17:26:06 +0000

    Added myself to authors list

--- code/boson/main.cpp
+++ code/boson/main.cpp
@@ -55,6 +55,7 @@ int main(int argc, char **argv)
  about.addAuthor("Thomas Capricelli", I18N_NOOP("Initial Game Design & Coding"), "orzel@kde.org", "http://orzel.freehackers.org");
  about.addAuthor("Benjamin Adler", I18N_NOOP("Graphics & Homepage Design"), "benadler@bigfoot.de");
  about.addAuthor( "Andreas Beckermann", I18N_NOOP("Coding"), "b_mann@gmx.de" );
+ about.addAuthor( "Rivo Laks", I18N_NOOP("Coding"), "rivolaks@hot.ee" );
 
  KCmdLineArgs::init(argc, argv, &about);
  KCmdLineArgs::addCmdLineOptions(options);
