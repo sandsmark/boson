61751dbf9735f7d9135b380324e9b758c187e745
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-03-13 16:23:58 +0000

    - some documentations

--- trunk/code/boson/kgamedialogbosonconfig.h
+++ trunk/code/boson/kgamedialogbosonconfig.h
@@ -24,6 +24,14 @@
 class Player;
 
 /**
+ * This is the main widget of the newgame dialog. Here the user can choose a
+ * map, a scenario, a network or computer players and even his species.
+ *
+ * Please note that this is quite a complex class which must interact very much
+ * with @ref Boson! We need to ensure that as soon as one player changed a value
+ * in the widget it is transferred over network to <em>all</em> players!
+ * For example if a player changes the map the new map MUST be transferred to
+ * all players so that they know the map was changed.
  * @author Andreas Beckermann <b_mann@gmx.de>
  **/
 class KGameDialogBosonConfig : public KGameDialogGeneralConfig
@@ -41,8 +49,26 @@ signals:
 	void signalTeamColorChanged(const QColor& color);
 
 public slots:
+	/**
+	 * Called when the user changes the map in the combo box. Just sends a
+	 * message using @ref KGame::sendMessage. See also @ref
+	 * Boson::signalMapChanged
+	 **/
 	void slotMapChanged(int index);
+
+	/**
+	 * In contrary to the previous @ref slotMapChanged this is <em>not</em>
+	 * called when the user changed the map in the combo box but as soon as
+	 * @ref Boson::signalMapChanged is received.
+	 *
+	 * The reason for this is that in a 2-player game there are 2 players
+	 * who can change the map. Both must be able to see the change, so we
+	 * must transmit the change over network (the previous slotMapChanged)
+	 * and then (when we receive it from network) actually change the map
+	 * (this slot).
+	 **/
 	void slotMapChanged(const QString& mapIdentifier);
+	
 	void slotScenarioChanged(const QString& scenarioIdentifier);
 	void slotSpeciesChanged(Player*);
 	void slotTeamColorChanged(Player*);
