8a45090dae71d014ac12c234104ba6e427be0b2d
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-06-09 20:50:56 +0000

    - Fix for moving code (again). It's hackish, but it works(for me at least)

--- code/boson/boitemlist.h
+++ code/boson/boitemlist.h
@@ -86,6 +86,8 @@ public:
 	 **/
 	bool isOccupied(Unit* forUnit = 0, bool includeMoving = true) const;
 
+	void willBeOccupiedBy(Unit* unit);
+
 protected:
 	/**
 	 * You are meant to use e.g. @ref appendUnit but not append. We change the
