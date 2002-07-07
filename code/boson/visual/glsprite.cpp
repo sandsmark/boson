86c8b1f269a63e288b3040b3357775a25461e41d
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-07-07 23:42:33 +0000

    Those of you who have been on #boson recently know it already: for the last few weeks I have been working on OpenGL support. The bad news first: 3d is *not* yet supported. We also have a LOT of new bugs and missing features (e.g. select boxes are not yet integrated).
    But the basic canvas draw routines are now rendered using OpenGL (in 2d) so we'll probably move to 3d in the near future.
    
    Fell free to test this!
    
    It is not yet working completely on all computers, thats why I am creating a branch instead of committing to HEAD as I planned initially.
    
    You *need* 3d-accelleration in order to get the OpenGL working. I mean real hardware-accelleration, *not* software OpenGL! This won't work!
    
    As a test try to use a) glxgears which isn't really useful as a test app (the gears should be moving very slow!) b) tuxkart c)tux-aqfh
    Both of the games need hardware accellerated OpenGL support.
    
    Have a lot of fun with this!

---
+++ code/boson/visual/glsprite.cpp
@@ -0,0 +1,82 @@
+/*
+    This file is part of the Boson game
+    Copyright (C) 2002 The Boson Team (boson-devel@lists.sourceforge.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include "glsprite.h"
+#ifndef NO_OPENGL
+
+#include <kdebug.h>
+
+#include <qrect.h>
+
+GLSprite::GLSprite(BosonTextureArray* array, BosonCanvas* canvas)
+{
+ mCanvas = canvas;
+ for (int i = 0; i < 3*4; i++) {
+	mVertexPointer[i] = 0.0;
+ }
+ mX = mY = mZ = 0.0;
+
+ mIsVisible = false;
+
+ mFrame = 0;
+
+ mXVelocity = 0.0;
+ mYVelocity = 0.0;
+
+ mTextures = array;
+}
+
+GLSprite::~GLSprite()
+{
+}
+
+QRect GLSprite::boundingRect() const
+{
+ return QRect(leftEdge(), topEdge(), width(), height());
+}
+
+QRect GLSprite::boundingRectAdvanced() const
+{
+ return QRect(leftEdge() + xVelocity(), 
+		topEdge() + yVelocity(), 
+		width() + xVelocity(), 
+		height() + yVelocity());
+}
+
+void GLSprite::setVisible(bool v)
+{
+ // FIXME: remove from canvas ;
+ if (mIsVisible != v) {
+	mIsVisible = v;
+	if (v) {
+		// FIXME: add to cells ?
+	} else {
+		// FIXME: remove from cells ? 
+	}
+ }
+}
+
+void GLSprite::setCanvas(BosonCanvas* c)
+{
+ mCanvas = c;
+}
+
+
+
+
+#endif
