a299062570cb1048b24ff6fbf050d25435c05228
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-07-13 18:59:05 +0000

    - adding a debug dialog for boson players   i need to commit this to HEAD , too - if anyone volunteers? otherwise i'll do it once I'm back at home (if i still remember...)

--- branches/BOSON_OPENGL_BRANCH/code/boson/bosonwidget.cpp
+++ branches/BOSON_OPENGL_BRANCH/code/boson/bosonwidget.cpp
@@ -34,6 +34,7 @@
 #include "bosonconfig.h"
 #include "optionsdialog.h"
 #include "kgameunitdebug.h"
+#include "kgameplayerdebug.h"
 #include "bosonmusic.h"
 #include "bosoncursor.h"
 #include "commandinput.h"
@@ -436,9 +437,16 @@ void BosonWidget::initLayout()
 void BosonWidget::slotDebug()
 {
  KGameDebugDialog* dlg = new KGameDebugDialog(game(), this);
+ 
  QVBox* b = dlg->addVBoxPage(i18n("Debug &Units"));
  KGameUnitDebug* units = new KGameUnitDebug(b);
  units->setBoson(game());
+ 
+ b = dlg->addVBoxPage(i18n("Debug &Boson Players"));
+ KGamePlayerDebug* player = new KGamePlayerDebug(b);
+ player->setBoson(game());
+// player->setLocalPlayer(d->mLocalPlayer); // FIXME
+ 
  connect(dlg, SIGNAL(finished()), dlg, SLOT(slotDelayedDestruct()));
  connect(dlg, SIGNAL(signalRequestIdName(int,bool,QString&)),
 		this, SLOT(slotDebugRequestIdName(int,bool,QString&)));
