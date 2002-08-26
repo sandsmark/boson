4351651a68722b97b010578633354dcc1036bc2d
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-08-26 19:35:18 +0000

    - Oops, forgot to commit most improtant files (because cvschanged didn't show   them ;-))

---
+++ trunk/code/boson/upgradeproperties.cpp
@@ -0,0 +1,311 @@
+/*
+    This file is part of the Boson game
+    Copyright (C) 2002 The Boson Team (boson-devel@lists.sourceforge.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "upgradeproperties.h"
+
+#include <qstring.h>
+#include <qvaluelist.h>
+
+#include <ksimpleconfig.h>
+#include <klocale.h>
+
+#include "player.h"
+#include "unitproperties.h"
+#include "speciestheme.h"
+
+
+/**********  UpgradePropertiesBase  **********/
+
+UpgradePropertiesBase::UpgradePropertiesBase(bool isTechnology, unsigned long int id)
+{
+  mTechnology = isTechnology;
+
+  mResearched = false;
+  mId = id;
+  mName = "";
+  mMineralCost = 0;
+  mOilCost = 0;
+  mProducer = 0;
+  mProductionTime = 0;
+  mPixmapName = "";
+}
+
+UpgradePropertiesBase::~UpgradePropertiesBase()
+{
+}
+
+bool UpgradePropertiesBase::canBeResearched(Player* player)
+{
+  if(!mRequireUnits.isEmpty())
+  {
+    QValueList<unsigned long int>::Iterator it;
+    for(it = mRequireUnits.begin(); it != mRequireUnits.end(); ++it)
+    {
+      if(!player->hasUnitWithType(*it))
+      {
+        return false;
+      }
+    }
+  }
+
+  if(!mRequireTechnologies.isEmpty())
+  {
+    QValueList<unsigned long int>::Iterator it;
+    for(it = mRequireTechnologies.begin(); it != mRequireTechnologies.end(); ++it)
+    {
+      if(!player->hasTechnology(*it))
+      {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+void UpgradePropertiesBase::load(KSimpleConfig* cfg)
+{
+  kdDebug() << k_funcinfo << "Loading; isTech: " << isTechnology() << "; id: " << id() << "" << endl;
+  // Load basic stuff
+  if(mTechnology)
+  {
+    // Config group is set earlier
+    mId = cfg->readUnsignedLongNumEntry("Id", 0);
+    if(mId == 0) {
+      kdError() << k_funcinfo << "Invalid id: 0" << endl;
+    }
+    mName = cfg->readEntry("Name", i18n("unknown"));
+    mMineralCost = cfg->readUnsignedLongNumEntry("MineralCost", 100);
+    mOilCost = cfg->readUnsignedLongNumEntry("OilCost", 0);
+    mProducer = cfg->readUnsignedNumEntry("Producer", 0);
+    mProductionTime = cfg->readUnsignedNumEntry("ProductionTime", 100);
+    mPixmapName = cfg->readEntry("Pixmap", "none.png");
+    mRequireUnits = readUIntList(cfg, "RequireUnits");
+    mApplyToTypes = readUIntList(cfg, "ApplyToTypes");
+    mApplyToFacilities = cfg->readBoolEntry("ApplyToFacilities", false);
+    mApplyToMobiles = cfg->readBoolEntry("ApplyToMobiles", false);
+  }
+  else
+  {
+    if(!cfg->hasGroup(QString("Upgrade_%1").arg(mId)))
+    {
+      kdError() << k_funcinfo << "While loading upgrade with id " << mId << ": No config group for upgrade" << endl;
+      return;
+    }
+    cfg->setGroup(QString("Upgrade_%1").arg(mId));
+  }
+  mRequireTechnologies = readUIntList(cfg, "RequireTechnologies");
+
+  // Load upgrade properties
+  mHealthSpecified = cfg->hasKey("Health");
+  if(mHealthSpecified)
+  {
+    mHealth = cfg->readUnsignedLongNumEntry("Health", 100);
+  }
+  mWeaponRangeSpecified = cfg->hasKey("WeaponRange");
+  if(mWeaponRangeSpecified)
+  {
+    mWeaponRange = cfg->readUnsignedLongNumEntry("WeaponRange", 0);
+  }
+  mSightRangeSpecified = cfg->hasKey("SightRange");
+  if(mSightRangeSpecified)
+  {
+    mSightRange = cfg->readUnsignedNumEntry("SightRange", 5);
+  }
+  mWeaponDamageSpecified = cfg->hasKey("WeaponDamage");
+  if(mWeaponDamageSpecified)
+  {
+    mWeaponDamage = cfg->readLongNumEntry("WeaponDamage", 0);
+  }
+  mReloadSpecified = cfg->hasKey("Reload");
+  if(mReloadSpecified)
+  {
+    mReload = cfg->readUnsignedNumEntry("Reload", 0);
+  }
+  mUnitProductionTimeSpecified = cfg->hasKey("UnitProductionTime");
+  if(mUnitProductionTimeSpecified)
+  {
+    mUnitProductionTime = cfg->readUnsignedNumEntry("UnitProductionTime", 0);
+  }
+  mUnitMineralCostSpecified = cfg->hasKey("UnitMineralCost");
+  if(mUnitMineralCostSpecified)
+  {
+    mUnitMineralCost = cfg->readUnsignedLongNumEntry("UnitMineralCost", 100);
+  }
+  mUnitOilCostSpecified = cfg->hasKey("UnitOilCost");
+  if(mUnitOilCostSpecified)
+  {
+    mUnitOilCost = cfg->readUnsignedLongNumEntry("UnitOilCost", 0);
+  }
+  mArmorSpecified = cfg->hasKey("Armor");
+  if(mArmorSpecified)
+  {
+    mArmor = cfg->readUnsignedLongNumEntry("Armor", 0);
+  }
+  mShieldsSpecified = cfg->hasKey("Shield");
+  if(mShieldsSpecified)
+  {
+    mShields = cfg->readUnsignedLongNumEntry("Shield", 0);
+  }
+  mSpeedSpecified = cfg->hasKey("Speed");
+  if(mSpeedSpecified)
+  {
+    mSpeed = cfg->readDoubleNumEntry("Speed", 0);
+  }
+  mMaxResourcesSpecified = cfg->hasKey("MaxResources");
+  if(mMaxResourcesSpecified)
+  {
+    mMaxResources = cfg->readUnsignedLongNumEntry("MaxResources", 100);
+  }
+}
+
+QValueList<unsigned long int> UpgradePropertiesBase::readUIntList(KSimpleConfig* cfg, const char* key) const
+{
+  QValueList<unsigned long int> list;
+  QValueList<int> tmplist = cfg->readIntListEntry(key);
+  QValueList<int>::Iterator it;
+  for(it = tmplist.begin(); it != tmplist.end(); it++) {
+    list.append((unsigned long int)(*it));
+  }
+  return list;
+}
+
+// I do not usually like macros, but this is one place where I feel that macro is good
+// list: property will be applied to all unitproperties in list
+// player: pointer to owner of UnitProperties
+// myvar: name of variable in this class
+// upvar: name of variable in UnitProperties
+#define applyProperty(list,player,myvar,upvar, name) { \
+  QValueList<unsigned long int>::Iterator it; \
+  for(it = list.begin(); it != list.end(); it++) \
+  { \
+    UnitProperties* prop = player->speciesTheme()->nonConstUnitProperties(*it); \
+    prop->upvar = myvar; \
+  } \
+}
+
+void UpgradePropertiesBase::apply(Player* player)
+{
+  kdDebug() << k_funcinfo << "isTech: " << isTechnology() << "; id: " << id() << endl;
+  if(!isResearched())
+  {
+    kdError() << k_funcinfo << "Trying to apply non-researched upgrade" << endl;
+  }
+
+  // Add unit types to list
+  QValueList<unsigned long int> list = mApplyToTypes;
+  if(isTechnology())
+  {
+    if(mApplyToFacilities)
+    {
+      list += player->speciesTheme()->allFacilities();
+    }
+    if(mApplyToMobiles)
+    {
+      list += player->speciesTheme()->allMobiles();
+    }
+  }
+
+  if(mHealthSpecified)
+  {
+    applyProperty(list, player, mHealth, mHealth, "mHealth");
+  }
+  if(mWeaponRangeSpecified)
+  {
+    applyProperty(list, player, mWeaponRange, mWeaponRange, "mWeaponRange");
+  }
+  if(mSightRangeSpecified)
+  {
+    applyProperty(list, player, mSightRange, mSightRange, "mSightRange");
+  }
+  if(mWeaponDamageSpecified)
+  {
+    applyProperty(list, player, mWeaponDamage, mWeaponDamage, "mWeaponDamage");
+  }
+  if(mReloadSpecified)
+  {
+    applyProperty(list, player, mReload, mReload, "mReload");
+  }
+  if(mUnitProductionTimeSpecified)
+  {
+    applyProperty(list, player, mUnitProductionTime, mProductionTime, "mProductionTime");
+  }
+  if(mUnitMineralCostSpecified)
+  {
+    applyProperty(list, player, mUnitMineralCost, mMineralCost, "mMineralCost");
+  }
+  if(mUnitOilCostSpecified)
+  {
+    applyProperty(list, player, mUnitOilCost, mOilCost, "mOilCost");
+  }
+  if(mArmorSpecified)
+  {
+    applyProperty(list, player, mArmor, mArmor, "mArmor");
+  }
+  if(mShieldsSpecified)
+  {
+    applyProperty(list, player, mShields, mShields, "mShields");
+  }
+  if(mSpeedSpecified)
+  {
+    // This is tricky one
+    QValueList<unsigned long int>::Iterator it;
+    for(it = list.begin(); it != list.end(); it++)
+    {
+      UnitProperties* prop = player->speciesTheme()->nonConstUnitProperties(*it);
+      prop->setSpeed(mSpeed);
+    }
+  }
+  if(mMaxResourcesSpecified)
+  {
+    applyProperty(list, player, mHealth, mHealth, "mHealth");
+  }
+}
+
+
+/**********  UpgradeProperties  **********/
+
+UpgradeProperties::UpgradeProperties(const UnitProperties* parent, unsigned long int id) :
+    UpgradePropertiesBase(false, id), PluginProperties(parent)
+{
+}
+
+UpgradeProperties::~UpgradeProperties()
+{
+}
+
+QString UpgradeProperties::name() const
+{
+  return i18n("Upgrade plugin");
+}
+
+void UpgradeProperties::loadPlugin(KSimpleConfig* cfg)
+{
+  load(cfg);
+  mApplyToTypes.append(unitProperties()->typeId());
+}
+
+/**********  TechnologyProperties  **********/
+
+TechnologyProperties::TechnologyProperties() : UpgradePropertiesBase(true)
+{
+}
+
+TechnologyProperties::~TechnologyProperties()
+{
+}
---
+++ trunk/code/boson/upgradeproperties.h
@@ -0,0 +1,194 @@
+/*
+    This file is part of the Boson game
+    Copyright (C) 2002 The Boson Team (boson-devel@lists.sourceforge.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef UPGRADEPROPERTIES_H
+#define UPGRADEPROPERTIES_H
+
+#include <qvaluelist.h>
+
+#include "pluginproperties.h"
+
+class Player;
+class QString;
+class KSimpleConfig;
+class UnitProperties;
+
+/**
+ * @short Base class for properties of upgrades and technologies
+ *
+ * In Boson, there are 2 types of upgrades: upgrades and technologies.
+ * Upgrades are unit-specific, they only affect single unit, while technology
+ * can affect many units at once
+ *
+ * This class stores all properties of upgrade or technology.
+ * It has methods to check if upgrade can be researched, to apply changed
+ * values to UnitProperties etc.
+ *
+ * @author Rivo Laks <rivolaks@hot.ee>
+ **/
+class UpgradePropertiesBase
+{
+  public:
+    /**
+     * Constructs UpgradePropertiesBase
+     * @param isTechnology If it is technology or upgrade
+     * @param id Id of this _upgrade_. Technologies will get id when they're loaded
+     **/
+    UpgradePropertiesBase(bool isTechnology, unsigned long int id = 0);
+    virtual ~UpgradePropertiesBase();
+
+    /**
+     * Load upgrade or tech. properties
+     * Note that if it's technology, you _MUST_ have set right group before
+     **/
+    virtual void load(KSimpleConfig* cfg);
+
+    /**
+     * @return Whether it is possible by player to research this upgrade (all
+     * requirements are met)
+     **/
+    virtual bool canBeResearched(Player* player);
+
+    /**
+     * @return Whether this upgrade/tech. is already researched
+     **/
+     bool isResearched() const { return mResearched; };
+
+    /**
+     * Set 'researched' status of this upgrade (whether it's already researched)
+     * Use this only in Player class!!!
+     **/
+     void setResearched(bool r) { mResearched = r; };
+
+    /**
+     * @return Whether it is technology, not upgrade
+     **/
+    bool isTechnology() const { return mTechnology; };
+
+    /**
+     * One of the most important methods in this class
+     * It applies upgrade to units
+     * You must call setResearched(true) before this can have any effect
+     * @param player owner of this class (and UnitProperties)
+     **/
+    virtual void apply(Player* player);
+
+
+    /**
+     * @return Id of this upgrade
+     **/
+    unsigned long int id() const { return mId; };
+    /**
+     * @return Name of this upgrade
+     **/
+    QString upgradeName() const { return mName; };
+    /**
+     * @return Mineral cost of this upgrade
+     **/
+    unsigned long int mineralCost() const { return mMineralCost; };
+    /**
+     * @return Oil cost of this upgrade
+     **/
+    unsigned long int oilCost() const { return mOilCost; };
+    /**
+     * @return producer of this upgrade. Producer ids are used
+     **/
+    unsigned int producer() const { return mProducer; };
+    /**
+     * @return How much time it takes to produce this upgrade
+     **/
+    unsigned int productionTime() const { return mProductionTime; };
+    /**
+     * @return Filename of pixmap of this upgrade
+     **/
+    QString pixmapName() const { return mPixmapName; };
+    /**
+     * @return List of units required by this upgrade
+     **/
+    QValueList<unsigned long int> requiredUnits() const { return mRequireUnits; };
+    /**
+     * @return List of technologies required by this upgrade
+     **/
+    QValueList<unsigned long int> requiredTechnologies() const { return mRequireTechnologies; };
+
+  protected:
+    QValueList<unsigned long int> readUIntList(KSimpleConfig* cfg, const char* key) const;
+
+  protected:
+    unsigned long int mHealth;
+    unsigned long int mWeaponRange;
+    unsigned int mSightRange;
+    long int mWeaponDamage;
+    unsigned int mReload;
+    unsigned int mUnitProductionTime;
+    unsigned long int mUnitMineralCost;
+    unsigned long int mUnitOilCost;
+    unsigned long int mArmor;
+    unsigned long int mShields;
+    float mSpeed;
+    unsigned long int mMaxResources;
+
+    bool mHealthSpecified;
+    bool mWeaponRangeSpecified;
+    bool mSightRangeSpecified;
+    bool mWeaponDamageSpecified;
+    bool mReloadSpecified;
+    bool mUnitProductionTimeSpecified;
+    bool mUnitMineralCostSpecified;
+    bool mUnitOilCostSpecified;
+    bool mArmorSpecified;
+    bool mShieldsSpecified;
+    bool mSpeedSpecified;
+    bool mMaxResourcesSpecified;
+
+    bool mResearched;
+    bool mTechnology;
+
+    unsigned long int mId;
+    QString mName;
+    unsigned long int mMineralCost;
+    unsigned long int mOilCost;
+    unsigned int mProducer;
+    unsigned int mProductionTime;
+    QString mPixmapName;
+    QValueList<unsigned long int> mRequireUnits;
+    QValueList<unsigned long int> mRequireTechnologies;
+    QValueList<unsigned long int> mApplyToTypes;
+    bool mApplyToFacilities;
+    bool mApplyToMobiles;
+};
+
+class UpgradeProperties : public UpgradePropertiesBase, public PluginProperties
+{
+  public:
+    UpgradeProperties(const UnitProperties* parent, unsigned long int id);
+    virtual ~UpgradeProperties();
+
+    virtual QString name() const;
+    virtual int pluginType() const { return Upgrade; };
+    virtual void loadPlugin(KSimpleConfig* cfg);
+};
+
+class TechnologyProperties : public UpgradePropertiesBase
+{
+  public:
+    TechnologyProperties();
+    virtual ~TechnologyProperties();
+};
+
+#endif // UPGRADEPROPERTIES_H
