c1364ed10b7130b6c9c2b502bd87b3813010ccf1
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-09-02 15:04:19 +0000

    - Adding style options page. From there, you can select color scheme.   Note that you must have cookies enabled for this to work and also   browser must support CSS1 (every widely-used browser does) - Better CSS files (added some color: entries to remove validator warnings)

---
+++ web/options.php
@@ -0,0 +1,86 @@
+<?php
+
+
+/*****  Variables  *****/
+$filename="options.php";
+
+$change_style="";
+
+/*****  Some includes  *****/
+include("common.php");
+include("sidebar.php");
+include("main.php");
+include("counter.php");
+include("boxes.php");
+include("variables.php");
+
+/*****  Start of main stuff  *****/
+
+function draw_option($value, $desc)
+{
+global $style;
+$isselected;
+if($style == $value)
+  $isselected=" selected";
+echo "
+    <option value=\"$value\"$isselected>$desc</option>";
+}
+
+
+//$style;
+if($HTTP_GET_VARS["style"] != "")
+{
+  $change_style=$HTTP_GET_VARS["style"];
+  setcookie("Style", $change_style, time() + 3600 * 24 * 365 * 25); // Expires after 25 years
+}
+
+do_start_stuff();
+
+if($change_style != "")
+  $style=$change_style;
+
+// Headers
+html_print_header("Options, style: $style");
+print_header();
+
+// Main table
+main_table_begin();
+
+// Sidebar
+sidebar_begin();
+  sidebar_links_box();
+  sidebar_download_box();
+  sidebar_stats_box();
+sidebar_end();
+
+main_area_begin();
+
+// Contacts
+draw_box("Options");
+echo "
+    Here you can change style, or color scheme, of this homepage.<br>
+    Currently, there are 2 different styles: blue, which is default and looks
+    like style of KDE homepages and green which has green and black colors and
+    looks like <a href=\"http://politbuero.dyndns.org/websites/boson/\">homepage
+    that Ben once created</a>.<br>
+    Note that your browser must have cookies enabled for this site for this to
+    work.<br>
+    Your style: <form class=\"style\" action=\"$filename\" method=\"get\">
+    <select size=\"1\" name=\"style\">";
+draw_option("blue", "Default blue style");
+draw_option("green", "Green/black style");
+echo "
+    </select>
+    &nbsp;&nbsp;
+    <input type=\"submit\" value=\"Change!\">
+
+";
+main_area_end();
+main_table_end();
+
+// Footers
+print_footer();
+html_print_footer();
+
+
+?>
