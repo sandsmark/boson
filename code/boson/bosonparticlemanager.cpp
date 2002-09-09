77175b1ba1481401e50cd0534b8ad415988e3c7e
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-09-09 16:07:07 +0000

    - Facilities now burn and smoke when they have only half or less   of hitpoints left. Amount of smoke and flames does not yet depend on how   many hitpoints facility has (it doesn't change), because it's too difficult   at the moment. Maybe I'll do it in the future. - Added pretty much class documentation for BosonParticle*   PLEASE do same if you add new classes/methods and also try to write some   docus for old classes if you work on them. - Fixed some bugs in particle system - Made all private stuff protected in BosonParticleSystem (to ease subclassing) - Added methods to make fire and small smoke (for burning facilities) to   BosonParticleManager

--- code/boson/bosonparticlemanager.cpp
+++ code/boson/bosonparticlemanager.cpp
@@ -45,6 +45,8 @@ void BosonParticleManager::loadTextures(QString texdir)
   images.append(QImage(texdir + "/explosion.png"));
   images.append(QImage(texdir + "/smoke.png"));
   images.append(QImage(texdir + "/explosion.png"));
+  images.append(QImage(texdir + "/explosion.png"));
+  images.append(QImage(texdir + "/smoke.png"));
 
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -58,7 +60,7 @@ BosonParticleSystem* BosonParticleManager::newSystem(BoVector3 pos, Type type)
 //  kdDebug() << "PARTICLE:" << "        " << k_funcinfo << "called, type: " << type << "pos: (" << x << ", " << y << ", " << z  << ")" << endl;
   int maxnum = 0, initnum = 0, blendfunc = GL_ONE_MINUS_SRC_ALPHA;
   float rate = 0, size = 0, age = 0;
-  BosonParticleSystem::ExternalFunction initfunc = 0, updatefunc = 0;
+  BosonParticleSystem::ExternalFunction initfunc = 0, updatefunc = &updateFadeOutParticle;
 
   if(type == Explosion)
   {
@@ -66,7 +68,6 @@ BosonParticleSystem* BosonParticleManager::newSystem(BoVector3 pos, Type type)
     initnum = 200;
     size = 1.0;
     initfunc = &initExplosionParticle;
-    updatefunc = &updateExplosionParticle;
     blendfunc = GL_ONE;
   }
   else if(type == Smoke)
@@ -77,7 +78,6 @@ BosonParticleSystem* BosonParticleManager::newSystem(BoVector3 pos, Type type)
     size = 1.0;
     age = 20;
     initfunc = &initSmokeParticle;
-    updatefunc = &updateSmokeParticle;
   }
   else if(type == Shot)
   {
@@ -85,11 +85,30 @@ BosonParticleSystem* BosonParticleManager::newSystem(BoVector3 pos, Type type)
     initnum = 100;
     size = 0.5;
     initfunc = &initShotParticle;
-    updatefunc = &updateShotParticle;
     blendfunc = GL_ONE;
   }
+  else if(type == Fire)
+  {
+    maxnum = 200;
+    initnum = 0;
+    rate = 30;
+    size = 1.0;
+    age = 3600 * 24 * 30;  // 30 days ;-)
+    initfunc = &initFireParticle;
+    updatefunc = &updateFireParticle;
+    blendfunc = GL_ONE;
+  }
+  else if(type == SmallSmoke)
+  {
+    maxnum = 75;
+    initnum = 0;
+    rate = 25;
+    size = 0.7;
+    age = 3600 * 24 * 30;
+    initfunc = &initSmallSmokeParticle;
+  }
 
-  BosonParticleSystem* s = new BosonParticleSystem(maxnum, 0, rate, true,
+  BosonParticleSystem* s = new BosonParticleSystem(maxnum, rate, true,
       2, mTextures->texture((int)type - 1), initfunc, updatefunc);
   s->setPosition(pos);
   s->setSize(size);
@@ -103,7 +122,7 @@ BosonParticleSystem* BosonParticleManager::newSystem(BoVector3 pos, Type type)
 void BosonParticleManager::initExplosionParticle(BosonParticleSystem*, BosonParticle* particle)
 {
 //  kdDebug() << "PARTICLE:" << "        " << k_funcinfo << "initing particle" << endl;
-  particle->life = getFloat(0.5, 0.7);  // Particle's lifetime is between 0.5 and 2.0 seconds
+  particle->life = getFloat(0.5, 0.7);  // Particle's lifetime is between 0.5 and 0.7 seconds
   particle->maxage = particle->life;
   particle->velo = BoVector3(getFloat(-1.6, 1.6), getFloat(-1.6, 1.6), getFloat(0, 1.4));  // Random velocity (per second)
   particle->color = BoVector4(1.0, 0.07, 0.02, 0.25);  // Color of particle
@@ -111,46 +130,50 @@ void BosonParticleManager::initExplosionParticle(BosonParticleSystem*, BosonPart
   particle->pos = BoVector3(getFloat(-0.1, 0.1), getFloat(-0.1, 0.1), getFloat(0.2, 0.7));  // We randomize position little bit
 }
 
-void BosonParticleManager::updateExplosionParticle(BosonParticleSystem*, BosonParticle* particle)
-{
-  // Alpha will start decreasing when half of particle's life is over and will be 0 when it dies
-  if((particle->life * 2) < particle->maxage)
-  {
-    particle->color.data[3] = 0.25 * (particle->life * 2 / particle->maxage);
-  }
-}
-
 void BosonParticleManager::initSmokeParticle(BosonParticleSystem*, BosonParticle* particle)
 {
-//  kdDebug() << "PARTICLE:" << "        " << k_funcinfo << "initing particle" << endl;
-  particle->life = getFloat(2.0, 3.0);  // Particle's lifetime is between 0.5 and 2.0 seconds
+  particle->life = getFloat(2.0, 3.0);
   particle->maxage = particle->life;
-  particle->velo = BoVector3(getFloat(0.05, 0.55), getFloat(-0.15, 0.45), getFloat(0.4, 1.0));  // Random velocity (per second)
+  particle->velo = BoVector3(getFloat(0.05, 0.55), getFloat(-0.15, 0.45), getFloat(0.4, 1.0));
   particle->color = BoVector4(0.2, 0.2, 0.2, 0.7);
-  particle->pos = BoVector3(getFloat(-0.2, 0.2), getFloat(-0.2, 0.2), getFloat(-0.1, 0.25));  // We randomize position little bit
+  particle->pos = BoVector3(getFloat(-0.2, 0.2), getFloat(-0.2, 0.2), getFloat(-0.1, 0.25));
 }
 
-void BosonParticleManager::updateSmokeParticle(BosonParticleSystem*, BosonParticle* particle)
+void BosonParticleManager::initSmallSmokeParticle(BosonParticleSystem*, BosonParticle* particle)
 {
-  // Alpha will start decreasing when half of particle's life is over and will be 0 when it dies
-  if((particle->life * 2) < particle->maxage)
-  {
-    particle->color.data[3] = 0.7 * (particle->life * 2 / particle->maxage);
-  }
+  particle->life = getFloat(1.5, 2.3);
+  particle->maxage = particle->life;
+  particle->velo = BoVector3(getFloat(0.05, 0.55), getFloat(-0.15, 0.45), getFloat(0.4, 1.0));
+  particle->color = BoVector4(0.7, 0.7, 0.7, 0.7);
+  particle->pos = BoVector3(getFloat(-0.1, 0.1), getFloat(-0.1, 0.1), getFloat(0.5, 0.6));
 }
 
 void BosonParticleManager::initShotParticle(BosonParticleSystem*, BosonParticle* particle)
 {
-//  kdDebug() << "PARTICLE:" << "        " << k_funcinfo << "initing particle" << endl;
-  particle->life = getFloat(0.2, 0.4);  // Particle's lifetime is between 0.5 and 2.0 seconds
+  particle->life = getFloat(0.2, 0.4);
   particle->maxage = particle->life;
-  particle->velo = BoVector3(getFloat(-1.0, 1.0), getFloat(-1.0, 1.0), getFloat(0, 1.0));  // Random velocity (per second)
-  particle->color = BoVector4(1.0, 0.07, 0.02, 0.25);  // Color of particle
-  // Note that particle's position is relative to position of particle system
-  particle->pos = BoVector3(getFloat(-0.1, 0.1), getFloat(-0.1, 0.1), getFloat(0.5, 0.6));  // We randomize position little bit
+  particle->velo = BoVector3(getFloat(-1.0, 1.0), getFloat(-1.0, 1.0), getFloat(0, 1.0));
+  particle->color = BoVector4(1.0, 0.07, 0.02, 0.25);
+  particle->pos = BoVector3(getFloat(-0.1, 0.1), getFloat(-0.1, 0.1), getFloat(0.5, 0.6));
 }
 
-void BosonParticleManager::updateShotParticle(BosonParticleSystem*, BosonParticle* particle)
+void BosonParticleManager::initFireParticle(BosonParticleSystem*, BosonParticle* particle)
+{
+  particle->life = getFloat(1.5, 2.0);
+  particle->maxage = particle->life;
+  particle->velo = BoVector3(0.0, 0.0, getFloat(0.8, 1.2));
+  particle->color = BoVector4(1.0, 0.3, 0.15, 0.3);
+  particle->pos = BoVector3(getFloat(-0.05, 0.05), getFloat(-0.05, 0.05), getFloat(0.5, 0.6));
+}
+
+void BosonParticleManager::updateFireParticle(BosonParticleSystem*, BosonParticle* particle)
+{
+  particle->color.data[1] = 0.3 * (particle->life / particle->maxage);  // Green - makes fire more yellow
+  particle->color.data[2] = 0.15 * (particle->life / particle->maxage);  // Blue - makes fire more white
+  particle->color.data[3] = 0.3 * (particle->life / particle->maxage);  // Alpha
+}
+
+void BosonParticleManager::updateFadeOutParticle(BosonParticleSystem*, BosonParticle* particle)
 {
   // Alpha will start decreasing when half of particle's life is over and will be 0 when it dies
   if((particle->life * 2) < particle->maxage)
--- code/boson/bosonparticlesystem.h
+++ code/boson/bosonparticlesystem.h
@@ -22,13 +22,34 @@
 
 #include "bo3dtools.h"
 
+
+/**
+ * @short This class represents a single particle.
+ * Particle is part of particle system. Each particle has individual color,
+ * position, velocity, size and age.
+ * In BosonParticle, all these are public variables which means that you can
+ * easily access them.
+ * Note that velocity is velocity per second and life and maxage are also in seconds
+ * @see BosonParticleSystem
+ **/
 class BosonParticle
 {
   public:
+    /**
+     * Constructs particle. All properties are resetted (set to zero)
+     **/
     BosonParticle();
+    /**
+     * Constructs particle and sets properties to given arguments
+     **/
     BosonParticle(BoVector4 color, BoVector3 pos, BoVector3 velo, float size, float life = 0);
     ~BosonParticle();
 
+    /**
+     * Updates particle. This decreases it's lifetime (when lifetime is 0,
+     * particle dies) and updates positon accordingly to velocity.
+     * @param elapsed How much time in seconds has elapsed since last update.
+     **/
     void update(float elapsed);
     void reset();
 
@@ -40,49 +61,165 @@ class BosonParticle
     float maxage;
 };
 
+/**
+ * @short This class takes care of entire particle system (collection of similar particles)
+ * When should you use one particle system and when more? One particle system
+ * should only hold similar particles. This means that if you e.g. have a unit
+ * burning and smoking, you should use two particle systems, because smoke and
+ * flames are very different things. It doesn't matter that both are used for
+ * the same unit.
+ * 
+ * BosonParticleSystem should be very flexible. You can specify your own init 
+ * and update functions for particles. They are called every time when particle
+ * is updated or initialized (resetted). If you want even more flexibility, then
+ * you can write you own subclass; most important methods are virtual, so you
+ * can change them if you really want to.
+ **/
 class BosonParticleSystem
 {
   public:
     typedef void (*ExternalFunction)(BosonParticleSystem* system, BosonParticle* particle);
+    /**
+     * Constructs new BosonParticleSystem. Use this constructor if you don't
+     * want to use update- and init-functions feature.
+     * @param maxnum Maximum number of particles this system may have
+     * @param initialnum This number of initial particles are created
+     * @param size Size of particle
+     * @param createrate How many additional particles are created each second
+     * @param align Whether to align particles to camera
+     * @param maxradius Radius of bounding sphere of this system (not used yet)
+     * @param texture Id of particle texture
+     * @param color Color of particle
+     * @param particleage How long (in seconds) each particle lives
+     * @param age Age of this system (for how long it creates new particles)
+     * @param pos Position of this system
+     * @param velo Velocity of each particle
+     * @param initFunc This function, if specified, is called every time particle is inited
+     * @param updateFunc This function, if specified, is called every time particle is updated
+     **/
     BosonParticleSystem(int maxnum, int initialnum, float size,
         float createrate, bool align, float maxradius, int texture,
         BoVector4 color, float particleage, float age, BoVector3 pos, BoVector3 velo,
-        ExternalFunction initFunc = 0, ExternalFunction updateFunc = 0,
-        ExternalFunction deleteFunc = 0);
-    BosonParticleSystem(int maxnum, int initialnum, float createrate,
+        ExternalFunction initFunc = 0, ExternalFunction updateFunc = 0);
+    /**
+     * Constructs new BosonParticleSystem. This constructor is often enough if
+     * you use functions for initing and updating particles.
+     * If you use this constructor, use accessor methods to set different
+     * variables and then @ref createParticles to create initial particles
+     * @param maxnum Maximum number of particles this system may have
+     * @param createrate How many additional particles are created each second
+     * @param align Whether to align particles to camera
+     * @param maxradius Radius of bounding sphere of this system (not used yet)
+     * @param texture Id of particle texture
+     * @param initFunc This function, if specified, is called every time particle is inited
+     * @param updateFunc This function, if specified, is called every time particle is updated
+     **/
+    BosonParticleSystem(int maxnum, float createrate,
         bool align, float maxradius, int texture, ExternalFunction initFunc,
-        ExternalFunction updateFunc, ExternalFunction deleteFunc = 0);
+        ExternalFunction updateFunc);
+    /**
+     * Destructs BosonParticleSystem. This deleted all particles
+     **/
     virtual ~BosonParticleSystem();
 
+    /**
+     * Updates all particles within this system and creates new ones if needed
+     * and if age is more than 0.
+     **/
     virtual void update(float elapsed);
+    /**
+     * Draws all particles.
+     * Note that blending must be enabled before you call this and if you use
+     * custom blending functions (see @ref setBlendFunc), you may want to reset
+     * blending functions after calling this.
+     **/
     virtual void draw();
 
-    virtual void initParticle(BosonParticle* particle);
-    inline virtual void updateParticle(BosonParticle* particle) { if(mUpdateFunc) (*mUpdateFunc)(this, particle); };
-    inline virtual void deleteParticle(BosonParticle* particle) { if(mDeleteFunc) (*mDeleteFunc)(this, particle); };
-
-    inline void setPosition(BoVector3 v) { mPos = v; };
+    /**
+     * Sets current position of this system. Position of particles are relative
+     * to position of system they belong to, so you can easily move all
+     * particles with this method
+     * @param p New position of this system
+     **/
+    inline void setPosition(BoVector3 p) { mPos = p; };
+    /**
+     * @return Current position of this system
+     **/
     inline BoVector3 position() { return mPos; };
-    
+
+    /**
+     * @return Average size of particles in this system
+     **/
     inline float size() { return mSize; };
+    /** 
+     * Sets average size of particles in this system. This is used for some
+     * drawing calculations.
+     * @param s New average size of particles
+     **/
     inline void setSize(float s) { mSize = s; };
 
+    /**
+     * @return Current age of this system
+     **/
     inline float age() { return mAge; };
+    /**
+     * Sets age of this system (in seconds).
+     * Age is decreased every time @ref update is called and if it's 0, no new
+     * particles are created anymore
+     * @param a New age of this system
+     **/
     inline void setAge(float a) { mAge = a; };
 
+    /**
+     * @return Current creation rate of particles (per second)
+     **/
+    inline float createRate() { return mCreateRate; };
+    /**
+     * Sets current creation rate of particles (per second).
+     * New particles are periodically created every @ref update call.
+     * @param r New creation rate of particles
+     **/
+    inline void setCreateRate(float r) { mCreateRate = r; };
+
+    /**
+     * @return Radius of bounding sphere of this system. This can be used for culling
+     **/
     inline float boundingSphereRadius()  { return mRadius; };
+    
+    /**
+     * @return Whether this system is active
+     * Active means that this system has at least 1 living particle
+     **/
     inline bool isActive()  { return (mNum > 0); };
 
-    inline void setBlendFunc(int f1, int f2) { mBlendFunc[0] = f1; mBlendFunc[1] = f2; };
+    /**
+     * Sets OpenGL blending function of this system. This function is used in
+     * @ref draw mehod. You can use this to specify your own blending functions
+     * if needed. Defaults are GL_SRC_ALPHA and GL_ONE_MINUS_SRC_ALPHA.
+     * @param sf Source blend function
+     * @param df Destination blend function
+     **/
+    inline void setBlendFunc(int sf, int df) { mBlendFunc[0] = sf; mBlendFunc[1] = df; };
 
+    /**
+     * @return Number of living (active) particles in this system
+     **/
     inline int particleCount() { return mNum; };
 
+    /**
+     * Creates specified number of new particles.
+     * @param count How many new particles to create
+     **/
     void createParticles(int count);
 
   protected:
     virtual void init(int initialnum);
 
-  private:
+    virtual void initParticle(BosonParticle* particle);
+    inline virtual void updateParticle(BosonParticle* particle) { if(mUpdateFunc) (*mUpdateFunc)(this, particle); };
+    inline virtual void deleteParticle(BosonParticle*) {};
+
+  protected:
     BosonParticle* mParticles;  // Array of particles
     int mMaxNum;  // Maximum number of particles
     int mNum;  // Current number of particle (aka number of active particles)
@@ -102,7 +239,6 @@ class BosonParticleSystem
 
     ExternalFunction mInitFunc;
     ExternalFunction mUpdateFunc;
-    ExternalFunction mDeleteFunc;
 };
 
 #endif // BOSONPARTICLESYSTEM_H
