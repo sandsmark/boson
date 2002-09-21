c49ec6dc4fa7b073ee19928e6a859175765bc455
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-09-21 17:03:18 +0000

    - add feature plan. mostly what needs to be done for 0.7 here.

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -38,7 +38,40 @@ draw_bigbox_text("Boson is currently still under heavy development.
 	<br><br>In the latest CVS version, the biggest changes, compared to latest stable release ($latestversion),
 	are OpenGL support (Boson is now 3D), support for upgrades, a nicer commandframe and many other little modifications.
 	Also, a large number of bugs have been fixed.<br><br>We're currently heading for release of version 0.7
-	which will probably be released late 2002 or early 2003 (Note that these dates are <b>not</b> official and may change).");
+	which will probably be released late 2002 or early 2003 (Note that these
+	dates are <b>not</b> official and may change).<br><br>");
+
+draw_bigbox_subheader("Feature list");
+draw_bigbox_text("This is a list of features that I consider as
+	&quot;to-be-done&quot; before release. Note that this is <b>not</b>
+	the list of features for the next release, but just a TODO list!
+	<h2>TODO</h2>
+	<ul>
+	  <li>Rename all .desktop files to .boson</li>
+	  <li>Make map/scenario editor work. Maps should at least be modifyable,
+	  optionally you should be able to create new maps. Loading/Saving code
+	  is necessary for this.</li>
+	  <li>Use UnitProperties in bounit</li>
+	  <li>Commandframe (un)-docking must be fixed. See #48058</li>
+	  <li>All units must have their own 3D model</li>
+	</ul>
+	<h2>DONE</h2>
+	<ul>
+	  <li>Port to OpenGL</li>
+	  <li>Full 3D support using .3ds models</li>
+	  <li>Explosions using particle effects</li>
+	  <li>Lots of good documentations. There is also a technology tree that
+	  is generated dynamically.</li>
+	  <li>Improved pathfinding</li>
+	  <li>Experimental support for visible missiles</li>
+	  <li>Greatly improved configurability for the cursor (including GUI for
+	  the config files)</li>
+	  <li>Options dialog has an Apply button - configs change when they are
+	  pplied only, not immediately. There are also defaults for every entry.</li>
+	  <li>Experimental support for unit animations (currenlty comsat only)</li>
+	</ul>
+	");
+
 draw_bigbox_end();
 //"
 
