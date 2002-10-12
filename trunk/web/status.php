ba4879c8435bc9901dd1b734de25a06ae40b02f7
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-10-12 12:44:33 +0000

    - move reload progress bar to done

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -50,7 +50,6 @@ draw_bigbox_text("This is a list of features that I consider as
 	  <li>Use UnitProperties in bounit</li>
 	  <li>All units must have their own 3D model</li>
 	  <li>Generate the technology tree dynamically for documentation.</li>
-	  <li>Remove the reloadstate progress bar</li>
 	</ul>
 	<h2>DONE</h2>
 	<ul>
@@ -70,6 +69,7 @@ draw_bigbox_text("This is a list of features that I consider as
 	  <li>Renamed all index.desktop files to appropriate names</li>
 	  <li>Map/scenario editor is working again. Maps are at least modifyable,
 	  you can add/delete a few units.</li>
+	  <li>Removed the reloadstate progress bar</li>
 	</ul>
 	");
 
