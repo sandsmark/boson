fdf22aa326cf81e15a4c3a9510728b7f11321469
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-10-31 21:52:10 +0000

    - some history.   i couldn't find any boson-0.5 announcement, so i used the pre-announcement on   the dot.

--- web/announcements.php
+++ web/announcements.php
@@ -35,8 +35,15 @@ main_area_begin();
 // Contacts
 draw_bigbox_begin("Announcements");
 draw_bigbox_text("All Boson announcements:
-    <ul><li><a href=\"announces/boson-0.6.php\">Boson 0.6 press release</a></li>
-    <li><a href=\"announces/boson-0.6.1.php\">Boson 0.6.1 press release</a></li></ul>");
+    <ul>
+    <li><a href=\"http://lists.kde.org/?l=kde-announce&m=93741789332505&w=2\">Boson 0.1 announcement</a></li>
+    <li><a href=\"http://lists.kde.org/?l=kde-announce&m=94069330417799&w=2\">Boson 0.2 announcement</a></li>
+    <li><a href=\"http://lists.kde.org/?l=kde-announce&m=94677285723944&w=2\">Boson 0.3 announcement</a></li>
+    <li><a href=\"http://lists.kde.org/?l=kde-announce&m=95700975705364&w=2\">Boson 0.4 announcement</a></li>
+    <li><a href=\"http://www.kdenews.org/972864519/\">Boson 0.5 announcement</a></li>
+    <li><a href=\"announces/boson-0.6.php\">Boson 0.6 press release</a></li>
+    <li><a href=\"announces/boson-0.6.1.php\">Boson 0.6.1 press release</a></li>
+    </ul>");
 draw_bigbox_end();
 
 main_area_end();
