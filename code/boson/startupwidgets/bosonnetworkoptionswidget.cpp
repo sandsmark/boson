a967d685ed1589edc4cdda0b2f0d1262a7e1348e
halux2001 <halux2001@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-11-07 16:49:43 +0000

    Fexed the default font here too: + header_font.setFamily( "verdana" );

--- code/boson/startupwidgets/bosonnetworkoptionswidget.cpp
+++ code/boson/startupwidgets/bosonnetworkoptionswidget.cpp
@@ -53,6 +53,7 @@ BosonNetworkOptionsWidget::BosonNetworkOptionsWidget(QWidget* parent)
 
   mHeader = new QLabel( this, "header" );
   QFont header_font(  mHeader->font() );
+  header_font.setFamily( "verdana" );
   header_font.setPointSize( 30 );
   header_font.setBold( TRUE );
   mHeader->setFont( header_font ); 
