72fbe7be09c2753e83c18ad5fd09ac8915a47298
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-11-17 15:32:58 +0000

    - readding the TODO list. this also gives us an easy way for the changelog...

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -40,6 +40,24 @@ draw_bigbox_text("Boson is currently still under heavy development.<br><br>
   We're currently heading for release of version 0.8, but it is currently
   unclear when it will be released.<br><br>");
 
+draw_bigbox_subheader("<a name=\"features\"></a>Feature list");
+draw_bigbox_text("This is a list of features that I consider as
+  &quot;to-be-done&quot; before release. Note that this is <b>not</b>
+  the list of features for the next release, but just a TODO list!
+  <h2>TODO</h2>
+  <ul>
+    <li>Support for height maps in editor</li>
+    <li>3d-terrains in map files</li>
+  </ul>
+  <h2>DONE</h2>
+  <ul>
+    <li>Height map for 3d-terrain</li>
+    <li>New map file format (.tar.gz instead of .gz)</li>
+    <li>Preload map information on startup</li>
+    <li>Decrease size of textures (fixes problems for 24bpp and 32bpp)</li>
+    <li>Don't draw invisible particles</li>
+  </ul>
+  ");
 main_area_end();
 main_table_end();
 
