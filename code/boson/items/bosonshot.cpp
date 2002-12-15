40d2295d00e3a607920732bf445962f597d57fce
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-12-15 17:42:28 +0000

    - don't use moveBy() in BosonShot - this is called by BosonCanvas::slotAdvance()   anyway. - actually move the items in z-direction - use 3 parameters for setVelocity()

--- code/boson/items/bosonshot.cpp
+++ code/boson/items/bosonshot.cpp
@@ -98,8 +98,9 @@ void BosonShot::advance(unsigned int phase)
   factor = -4 * (factor * factor) + 1;  // Factor is now  0 ... 1 ... 0  depending of current step
   // How much will be added to current z position
   float addZ = (mProp->maxHeight() * factor) * BO_TILE_SIZE;
-  moveBy(mVelo[0], mVelo[1], mVelo[2] + (addZ - mZ));
+  setVelocity(mVelo[0], mVelo[1], mVelo[2] + (addZ - mZ));
   setXRotation(rotationToPoint(mLength / mTotalSteps, addZ - mZ + mVelo[2]) - 90 );
+  //AB: maybe reimplement moveBy() for this?
   // Move all "fly" particles.
   BoVector3 move(mVelo[0], -(mVelo[1]), mVelo[2] + (addZ - mZ));
   move.scale(1 / (float)BO_TILE_SIZE);
