c46f5c8ed634de34fae1b1cfdf597c7ff92e172c
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-12-16 19:27:52 +0000

    - 0.8pre

--- code/boson/bocursormain.cpp
+++ code/boson/bocursormain.cpp
@@ -42,7 +42,7 @@
 static const char *description =
     I18N_NOOP("Cursor Editor for Boson");
 
-static const char *version = "0.7";
+static const char *version = "0.8pre";
 
 static KCmdLineOptions options[] =
 {
