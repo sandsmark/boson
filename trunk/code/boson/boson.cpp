321ef899c883bc4254b97cb60103dcccc4073d0e
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-12-23 13:35:05 +0000

    - cosmetic

--- trunk/code/boson/boson.cpp
+++ trunk/code/boson/boson.cpp
@@ -1327,7 +1327,7 @@ bool Boson::buildProducedUnit(ProductionPlugin* factory, unsigned long int unitT
 	boError() << k_funcinfo << "NULL owner" << endl;
 	return false;
  }
- if (!((BosonCanvas*)d->mCanvas)->canPlaceUnitAt(p->unitProperties(unitType), QPoint(x, y), 0)) {
+ if (!(d->mCanvas)->canPlaceUnitAt(p->unitProperties(unitType), QPoint(x, y), 0)) {
 	boDebug() << k_funcinfo << "Cannot create unit here" << endl;
 	return false;
  }
