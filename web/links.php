44b40762772cb2f1bd2307d96ae18044339abc7a
halux2001 <halux2001@14664f6f-540d-0410-967e-c81f6ba7cd39>
2002-12-31 06:12:40 +0000

    exernal links open in new window Added boson at sf.net smaller things

--- web/links.php
+++ web/links.php
@@ -35,11 +35,12 @@ main_area_begin();
 // Contacts
 draw_bigbox_begin("Links");
 draw_bigbox_text("Here you will find some (hopefully) useful links:<br><br>
-  <a href=\"http://www.kde.org/\">KDE homepage</a> Boson uses KDE<br>
-  <a href=\"http://www.trolltech.com\">Trolltech (creators of QT)</a> Boson also uses Qt<br>
-  <a href=\"http://www.freehackers.org/about/content.html\">Other free software</a><br>
-  <a href=\"http://www.freecraft.org/\">Freecraft homepage</a> Freecraft is another RTS for Linux<br>
-  <a href=\"http://sourceforge.net\">SF.net</a> Boson uses SourceForge.net's services<br>");
+  <a href=\"http://sf.net/projects/boson/\" target=\"_blank\">Boson at SourceForge</a> Boson uses SourceForge.net's services<br>
+  <a href=\"http://www.kde.org/\" target=\"_blank\">KDE homepage</a> Boson uses KDE<br>
+  <a href=\"http://www.trolltech.com\" target=\"_blank\">Trolltech (creators of QT)</a> Boson also uses Qt<br>
+  <a href=\"http://www.freehackers.org/about/content.html\" target=\"_blank\">freehackers.org</a>Other free software<br>
+  <a href=\"http://www.freecraft.org/\" target=\"_blank\">Freecraft homepage</a> Freecraft is another RTS for Linux<br>
+  <a href=\"http://sourceforge.net\" target=\"_blank\">SF.net</a> SourceForge.net<br>");
 draw_bigbox_end();
 
 main_area_end();
