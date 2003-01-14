75b97f8e8a0ab5a0477980ec3e9ff637c9eb9959
halux2001 <halux2001@14664f6f-540d-0410-967e-c81f6ba7cd39>
2003-01-14 20:50:27 +0000

    Less confusing text about the todo list
    And:
     Todo:
      +    <li>New startup widgets</li>
      +    <li>New save game format</li>
    
     Done:
           some fixes

--- trunk/web/status.php
+++ trunk/web/status.php
@@ -57,13 +57,14 @@ draw_bigbox_text("Boson is currently still under heavy development.<br><br>
   unclear when it will be released.<br><br>");
 
 draw_bigbox_subheader("<a name=\"features\"></a>Feature list");
-draw_bigbox_text("This is a list of features that I consider as
-  &quot;to-be-done&quot; before release. Note that this is <b>not</b>
-  the list of features for the next release, but just a TODO list!
+draw_bigbox_text("This is a list of features that we consider as
+  &quot;to-be-done&quot; for the next release.
   <h2>TODO</h2>
   <ul>
     <li>Support for height maps in editor</li>
     <li>3d-terrains in map files</li>
+    <li>New startup widgets</li>
+    <li>New save game format</li>
   </ul>
   <h2><a name=\"done\"></a>DONE</h2>
   <blockquote>
@@ -83,7 +84,7 @@ draw_bigbox_text("This is a list of features that I consider as
     <li>Technology upgrades are working now and were improved</li>
     <li>Added minimap image for disabled minimap</li>
     <li>Weapon specific sound support</li>
-    <li>Smoke support for powerplant, oilrefinery, mineralrefinery etc.</li>
+    <li>Smoke support for powerplant, oilrefinery, mineral refinery etc.</li>
     <li>Particle system improvements</li>
     <li>Damaged units are getting darker</li>
     <li>Save screenshots as jpg and show the full path as a chat message</li>
@@ -95,11 +96,11 @@ draw_bigbox_text("This is a list of features that I consider as
     <li>New unit: Ferry</li>
     <li>Unit models improvements</li>
     <li>Warfactory is now Weapons Factory</li>
-    <li>Handbook switched from png images to jpg</li>
+    <li>Handbook switched from png to jpg images</li>
   </ul>
   <h4>Editor</h4>
   <ul>
-    <li>Some bugs are fixed</li>
+    <li>Many bugs are fixed</li>
     <li>Buildings are placed in constructed mode</li>
     <li>Better support for big maps</li>
     <li>Support for changing map name and description</li>
