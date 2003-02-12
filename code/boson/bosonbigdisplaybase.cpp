3b6947ee15b1f19611993114b374bf1cc0c13595
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2003-02-12 20:26:37 +0000

    - Less diffuse lighting.   It should make scene lighter. Please test if it works, I didn't because   my local version is not compilable right now ;-)

--- code/boson/bosonbigdisplaybase.cpp
+++ code/boson/bosonbigdisplaybase.cpp
@@ -588,7 +588,7 @@ void BosonBigDisplayBase::initializeGL()
 
 #ifdef BO_LIGHT
  float lightAmb[] = {0.8, 0.8, 0.8, 1.0};
- float lightDif[] = {1.0, 1.0, 1.0, 1.0};
+ float lightDif[] = {0.2, 0.2, 0.2, 1.0};
 
  glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmb);
  glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDif);
