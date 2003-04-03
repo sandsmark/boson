afd10ac420d079d3bc86c508729cfdf74f723307
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2003-04-03 22:00:23 +0000

    - adding at() and indexAt() which both are inline and take row and column in   the order used by mathematicians (i.e. usual matrix order).

--- trunk/code/boson/bo3dtools.cpp
+++ trunk/code/boson/bo3dtools.cpp
@@ -466,21 +466,17 @@ void BoMatrix::multiply(const GLfloat* mat)
  // we use matmul4() from mesa only, not matmul34(). this means we are slower
  // than mesa! (and also less complex).
  // AB: this function multiplies mData by mat and places the result into mData.
-#define A(row,col)  mData[(col<<2)+row] // matrix A
-#define B(row,col)  mat[(col<<2)+row] // matrix B
-#define P(row,col)  mData[(col<<2)+row] // product (A * B)
+#define B(row,col)  mat[indexAt(row, col)]
  GLint i;
  for (i = 0; i < 4; i++)
  {
-   const GLfloat ai0=A(i,0),  ai1=A(i,1),  ai2=A(i,2),  ai3=A(i,3);
-   P(i,0) = ai0 * B(0,0) + ai1 * B(1,0) + ai2 * B(2,0) + ai3 * B(3,0);
-   P(i,1) = ai0 * B(0,1) + ai1 * B(1,1) + ai2 * B(2,1) + ai3 * B(3,1);
-   P(i,2) = ai0 * B(0,2) + ai1 * B(1,2) + ai2 * B(2,2) + ai3 * B(3,2);
-   P(i,3) = ai0 * B(0,3) + ai1 * B(1,3) + ai2 * B(2,3) + ai3 * B(3,3);
+   const GLfloat ai0=at(i,0),  ai1=at(i,1),  ai2=at(i,2),  ai3=at(i,3);
+   mData[indexAt(i, 0)] = ai0 * B(0,0) + ai1 * B(1,0) + ai2 * B(2,0) + ai3 * B(3,0);
+   mData[indexAt(i, 1)] = ai0 * B(0,1) + ai1 * B(1,1) + ai2 * B(2,1) + ai3 * B(3,1);
+   mData[indexAt(i, 2)] = ai0 * B(0,2) + ai1 * B(1,2) + ai2 * B(2,2) + ai3 * B(3,2);
+   mData[indexAt(i, 3)] = ai0 * B(0,3) + ai1 * B(1,3) + ai2 * B(2,3) + ai3 * B(3,3);
  }
-#undef A
 #undef B
-#undef P
 }
 
 void BoMatrix::rotate(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
