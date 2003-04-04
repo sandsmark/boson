68a86603f52cf9de1a94cee4fb8df8075b90b4b0
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2003-04-04 00:32:43 +0000

    - fix coding style at some points
    and a lot of vector fixes:
    - i am not a great mathematician, so i either need a maths book next to me, a
      flipcode.com browser window open, or good docs.
      adding these docs :-)
    - changing some parameters to const references instead of values. inlining is
      useless if an object needs to be created
    - removing (#ifdefing away) setScaledSum(). it isn't used anywhere and imho
      pretty confusing. is this needed at all?
    - reduce code duplication: e.g. use set() in operator=().

--- code/boson/bo3dtools.h
+++ code/boson/bo3dtools.h
@@ -1,6 +1,6 @@
 /*
     This file is part of the Boson game
-    Copyright (C) 2002 The Boson Team (boson-devel@lists.sourceforge.net)
+    Copyright (C) 2002-2003 The Boson Team (boson-devel@lists.sourceforge.net)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -40,55 +40,229 @@ void pointByRotation(float* x, float* y, const float angle, const float radius);
 class BoVector3
 {
   public:
-    BoVector3()  { reset(); };
+    BoVector3()  { reset(); }
     BoVector3(GLfloat x, GLfloat y, GLfloat z)  { set(x, y, z); }
     BoVector3(GLfloat* data) { set(data[0], data[1], data[2]); }
-    ~BoVector3() {};
+    ~BoVector3() {}
 
-    inline void reset()  { mData[0] = mData[1] = mData[2] = 0; };
+    /**
+     * Make this vector a null vector.
+     **/
+    inline void reset()  { mData[0] = mData[1] = mData[2] = 0.0f; }
 
-    inline GLfloat x() const { return mData[0]; };
-    inline GLfloat y() const { return mData[1]; };
-    inline GLfloat z() const { return mData[2]; };
+    /**
+     * @return The first (x) coordinate of the vector.
+     **/
+    inline GLfloat x() const { return mData[0]; }
+    /**
+     * @return The second (y) coordinate of the vector.
+     **/
+    inline GLfloat y() const { return mData[1]; }
+    /**
+     * @return The third (z) coordinate of the vector.
+     **/
+    inline GLfloat z() const { return mData[2]; }
 
-    inline void set(GLfloat x, GLfloat y, GLfloat z)  { mData[0] = x;  mData[1] = y;  mData[2] = z; };
-    inline void setX(GLfloat x) { mData[0] = x; }
-    inline void setY(GLfloat y) { mData[1] = y; }
-    inline void setZ(GLfloat z) { mData[2] = z; }
+    /**
+     * Assign the values @p x, @p y, @p z to the vector.
+     **/
+    inline void set(GLfloat x, GLfloat y, GLfloat z)
+    {
+      mData[0] = x;  mData[1] = y;  mData[2] = z; 
+    }
+    /**
+     * @overload
+     **/
+    inline void set(const BoVector3& v) { set(v.data()); }
+    /**
+     * @overload
+     **/
     inline void set(const float* v) { set(v[0], v[1], v[2]); }
 
-    inline void addScaled(BoVector3 v, GLfloat s)  { mData[0] += v.mData[0] * s;  mData[1] += v.mData[1] * s;  mData[2] += v.mData[2] * s; };
-    inline void setScaledSum(BoVector3 a, BoVector3 b,  GLfloat s)
-        { mData[0] = a.mData[0] + b.mData[0] * s;   mData[1] = a.mData[1] + b.mData[1] * s;   mData[2] = a.mData[2] + b.mData[2] * s; };
-    inline void add(BoVector3 v)  { mData[0] += v.mData[0]; mData[1] += v.mData[1]; mData[2] += v.mData[2]; };
-    inline void normalize()  { scale(1 / length()); };
-    inline void scale(float s)  {mData[0] = mData[0] * s;  mData[1] = mData[1] * s;  mData[2] = mData[2] * s; };
+    /**
+     * Assign the x coordinate to the vector.
+     **/
+    inline void setX(GLfloat x) { mData[0] = x; }
+    /**
+     * Assign the y coordinate to the vector.
+     **/
+    inline void setY(GLfloat y) { mData[1] = y; }
+    /**
+     * Assign the z coordinate to the vector.
+     **/
+    inline void setZ(GLfloat z) { mData[2] = z; }
+
+    /**
+     * Scale @p v by s and then add it to this vector.
+     **/
+    inline void addScaled(const BoVector3& v, GLfloat s)
+    {
+      mData[0] += v.mData[0] * s;  mData[1] += v.mData[1] * s;  mData[2] += v.mData[2] * s;
+    }
+
+#if 0
+    // AB: this function is NOT used all over boson's code and I find it very
+    // confusing. The name looks like it would scale both vertices and add the
+    // result together, but it scales b only.
+    // So I am removing this code for now - do we actually need it? I don't
+    // think so!
+    /**
+     * Scale the vector @p b by @p s, add the vector @p a to the result and
+     * assign the result to this vector.
+     **/
+    inline void setScaledSum(const BoVector3& a, const BoVector3& b,  GLfloat s)
+    {
+      mData[0] = a.mData[0] + b.mData[0] * s;   mData[1] = a.mData[1] + b.mData[1] * s;   mData[2] = a.mData[2] + b.mData[2] * s;
+    }
+#endif
+
+    /**
+     * Add @p v to this vector.
+     **/
+    inline void add(const BoVector3& v)
+    {
+      mData[0] += v.mData[0]; mData[1] += v.mData[1]; mData[2] += v.mData[2];
+    }
+
+    /**
+     * Normalize this vector.
+     *
+     * Normalizing a vector means to make it a so-called "unit-vector", that is
+     * a vector with @ref length 1.
+     *
+     * Practically this means dividing all elements in the vector by the @ref
+     * length of the vector.
+     **/
+    inline void normalize()
+    {
+      scale(1 / length());
+    }
+
+    /**
+     * Scale the vector by @p s. This is just scalar multiplication, i.e. all
+     * elements/coordinates of the vector are multiplied by @p s.
+     **/
+    inline void scale(float s)
+    {
+      mData[0] = mData[0] * s;  mData[1] = mData[1] * s;  mData[2] = mData[2] * s;
+    }
+
 
     //AB: this calls sqrt() and therefore is slow!
+    /**
+     * @return The length of the vector.
+     *
+     * The length of a vector v is defined as sqrt(v[0]^2 + v[1]^2 + v[2]^2) (in
+     * case of 3d).
+     *
+     * Notice that this function actually uses sqrt(), so it is slow in
+     * situations where you use it often!
+     **/
     float length() const;
-    inline float dotProduct() const  { return mData[0] * mData[0] + mData[1] * mData[1] + mData[2] * mData[2]; };
 
+    /**
+     * @return The dot product of the two vectors @p v and @p w.
+     *
+     * The dot product v*w is equal to |v|*|w|*cos(alpha), where alpha is the
+     * angle between both vectors and |v| is the length of v.
+     **/
+    static inline float dotProduct(const BoVector3& v, const BoVector3& w)
+    {
+      return v[0] * w[0] + v[1] * w[1] + v[2] * w[2];
+    }
+
+    /**
+     * @return The dot product of this vector with itself, i.e. (v * v).
+     **/
+    inline float dotProduct() const
+    {
+      return dotProduct(*this, *this);
+    }
+
+    /**
+     * @return A pointer to the internal array.
+     **/
     inline const GLfloat* data() const { return mData; }
-    inline void operator=(BoVector3 v)  { mData[0] = v.mData[0];  mData[1] = v.mData[1];  mData[2] = v.mData[2]; };
-    inline void operator=(const GLfloat* v)  { mData[0] = v[0];  mData[1] = v[1];  mData[2] = v[2]; };
-    inline void operator+=(BoVector3 v)  { mData[0] += v.mData[0]; mData[1] += v.mData[1]; mData[2] += v.mData[2]; };
-    inline GLfloat operator[](int i) const  { return mData[i]; };
 
-    inline BoVector3 operator+(const BoVector3& v) const  { return BoVector3(mData[0] + v.mData[0], mData[1] + v.mData[1], mData[2] + v.mData[2]); };
-    inline BoVector3 operator-(const BoVector3& v) const  { return BoVector3(mData[0] - v.mData[0], mData[1] - v.mData[1], mData[2] - v.mData[2]); };
-    inline BoVector3 operator*(float f) const  { return BoVector3(mData[0] * f, mData[1] * f, mData[2] * f); };
-    inline BoVector3 operator/(float f) const  { return BoVector3(mData[0] / f, mData[1] / f, mData[2] / f); };
-    inline BoVector3 operator-() const  { return BoVector3(-mData[0], -mData[1], -mData[2]); };
+    /**
+     * See @ref set
+     **/
+    inline void operator=(const BoVector3& v)
+    {
+      set(v);
+    }
 
-    // Conversion from one coordinate system to another. Should we use BO_GL_CELL_SIZE here?
-    inline void canvasToCell()  { mData[0] /= (float)BO_TILE_SIZE; mData[1] /= (float)BO_TILE_SIZE; };
-    inline void cellToCanvas()  { mData[0] *= (float)BO_TILE_SIZE; mData[1] *= (float)BO_TILE_SIZE; };
-    inline void canvasToWorld()  { mData[0] /= (float)BO_TILE_SIZE; mData[1] = -(mData[1] / (float)BO_TILE_SIZE); };
+    /**
+     * @overload
+     **/
+    inline void operator=(const GLfloat* v)
+    {
+      set(v);
+    }
+
+    /**
+     * See @ref add
+     **/
+    inline void operator+=(const BoVector3& v)
+    {
+      add(v);
+    }
+
+    /**
+     * @return The component / coordinate at @p i of this vector
+     **/
+    inline GLfloat operator[](int i) const
+    {
+      return mData[i];
+    }
+
+    /**
+     * @return A copy of this vector with @p v added.
+     **/
+    inline BoVector3 operator+(const BoVector3& v) const
+    {
+      return BoVector3(mData[0] + v.mData[0], mData[1] + v.mData[1], mData[2] + v.mData[2]);
+    }
+
+    /**
+     * @return A copy of this vector, @p v subtracted.
+     **/
+    inline BoVector3 operator-(const BoVector3& v) const
+    {
+      return BoVector3(mData[0] - v.mData[0], mData[1] - v.mData[1], mData[2] - v.mData[2]);
+    }
+
+    /**
+     * @return A copy of this vector, scaled by @p f.
+     **/
+    inline BoVector3 operator*(float f) const
+    {
+      return BoVector3(mData[0] * f, mData[1] * f, mData[2] * f);
+    }
+
+    /**
+     * @return A copy of this vector, scaled by 1 / @p f
+     **/
+    inline BoVector3 operator/(float f) const
+    {
+      return BoVector3(mData[0] / f, mData[1] / f, mData[2] / f);
+    }
+
+    /**
+     * @return A copy of this vector scaled by -1
+     **/
+    inline BoVector3 operator-() const
+    {
+      return BoVector3(-mData[0], -mData[1], -mData[2]);
+    }
 
     /**
      * @return Whether all components of this vector are zeros
      **/
-    inline bool isNull() const  { return ((mData[0] == 0.0) && (mData[1] == 0.0) && (mData[2] == 0.0)); };
+    inline bool isNull() const
+    {
+      return ((mData[0] == 0.0f) && (mData[1] == 0.0f) && (mData[2] == 0.0f));
+    }
 
     /**
      * Create 3 vectors from @p face in @p mesh and place them into @p v.
@@ -139,10 +313,13 @@ class BoVector3
     void save(KConfig* cfg, const QString key) const;
 
     /**
-     * @return TRUE when @p v is at the same position (x,y,z are all equal).
-     * Otherwise FALSE.
+     * @return TRUE when the coordinates of this vector equal x,y and z,
+     * otherwise FALSE.
      **/
-    bool isEqual(const BoVector3& v) const { return v.mData[0] == mData[0] && v.mData[1] == mData[1] && v.mData[2] == mData[2]; }
+    inline bool isEqual(float x, float y, float z) const
+    {
+      return mData[0] == x && mData[1] == y && mData[2] == z;
+    }
 
     /**
      * @overload
@@ -150,22 +327,15 @@ class BoVector3
      * Same as above, except that it takes an array of 3 floats, such as e.g.
      * Lib3dsVector.
      **/
-    bool isEqual(float* v) const { return mData[0] == v[0] && mData[1] == v[1] && mData[2] == v[2]; }
+    inline bool isEqual(const float* v) const { return isEqual(v[0], v[1], v[2]); }
 
     /**
      * @overload
-     *
-     * Same as above, except that it takes 3 separate floats as arguments.
      **/
-    bool isEqual(float x, float y, float z) const { return mData[0] == x && mData[1] == y && mData[2] == z; }
-
-    static QString debugString(const BoVector3& v);
-    static void debugVector(const BoVector3& v);
-
-    /**
-     * Convenience method for BoVector3::debugString(*this)
-     **/
-    QString debugString() const;
+    inline bool isEqual(const BoVector3& v) const
+    {
+      return isEqual(v.data());
+    }
 
     /**
      * @overload
@@ -174,7 +344,42 @@ class BoVector3
      * this static method without a BoVector3 instance - useful for comparing
      * Lib3dsVectors.
      **/
-   static bool isEqual(float* v1, float* v2) { return v1[0] == v2[0] && v1[1] == v2[1] && v1[2] && v2[2]; }
+    static bool isEqual(float* v1, float* v2)
+    {
+      return v1[0] == v2[0] && v1[1] == v2[1] && v1[2] && v2[2];
+    }
+
+    /**
+     * @return A string that contains the vector @p v. This string can be used
+     * for debugging.
+     **/
+    static QString debugString(const BoVector3& v);
+
+    /**
+     * Dump this vector onto the console. See also @ref debugString
+     **/
+    static void debugVector(const BoVector3& v);
+
+    /**
+     * Convenience method for BoVector3::debugString(*this)
+     **/
+    QString debugString() const;
+
+
+    // Conversion from one coordinate system to another. Should we use BO_GL_CELL_SIZE here?
+    inline void canvasToCell()
+    {
+      mData[0] /= (float)BO_TILE_SIZE; mData[1] /= (float)BO_TILE_SIZE;
+    }
+    inline void cellToCanvas()
+    {
+      mData[0] *= (float)BO_TILE_SIZE; mData[1] *= (float)BO_TILE_SIZE;
+    }
+    inline void canvasToWorld()
+    {
+      mData[0] /= (float)BO_TILE_SIZE; mData[1] = -(mData[1] / (float)BO_TILE_SIZE);
+    }
+
 
   private:
     friend class BoMatrix;
@@ -198,39 +403,139 @@ class BoVector4
     BoVector4(GLfloat x, GLfloat y, GLfloat z, GLfloat w)  { set(x, y, z, w); };
     ~BoVector4() {};
 
-    inline void reset()  {  mData[0] = mData[1] = mData[2] = mData[3] = 0; };
-
-    inline void set(GLfloat x, GLfloat y, GLfloat z, GLfloat w)  { mData[0] = x;  mData[1] = y;  mData[2] = z; mData[3] = w; };
-    inline void setX(GLfloat x) { mData[0] = x; }
-    inline void setY(GLfloat y) { mData[1] = y; }
-    inline void setZ(GLfloat z) { mData[2] = z; }
-    inline void setW(GLfloat w) { mData[3] = w; }
-
-    inline GLfloat x() const { return mData[0]; };
-    inline GLfloat y() const { return mData[1]; };
-    inline GLfloat z() const { return mData[2]; };
-    inline GLfloat w() const { return mData[3]; };
-
-    inline void addScaled(BoVector4 v, GLfloat s)
-        { mData[0] += v.mData[0] * s;  mData[1] += v.mData[1] * s;  mData[2] += v.mData[2] * s;  mData[3] += v.mData[3] * s; };
-    inline void setScaledSum(BoVector4 a, BoVector4 b,  GLfloat s)
-        { mData[0] = a.mData[0] + b.mData[0] * s;   mData[1] = a.mData[1] + b.mData[1] * s;   mData[2] = a.mData[2] + b.mData[2] * s;   mData[3] = a.mData[3] + b.mData[3] * s; };
-    inline void setBlended(BoVector4 a, float af, BoVector4 b, float bf)
-        { mData[0] = a.mData[0] * af + b.mData[0] * bf;   mData[1] = a.mData[1] * af + b.mData[1] * bf;
-        mData[2] = a.mData[2] * af + b.mData[2] * bf;   mData[3] = a.mData[3] * af + b.mData[3] * af; };
-    inline void add(BoVector4 v)  { mData[0] += v.mData[0]; mData[1] += v.mData[1]; mData[2] += v.mData[2]; mData[3] += v.mData[3]; };
-
-    inline const GLfloat* data() const { return mData; }
-    inline void operator=(BoVector4 v)  { mData[0] = v.mData[0];  mData[1] = v.mData[1];  mData[2] = v.mData[2];  mData[3] = v.mData[3]; };
-    inline void operator=(const GLfloat* v)  { mData[0] = v[0];  mData[1] = v[1];  mData[2] = v[2];  mData[3] = v[3]; };
-    inline GLfloat operator[](int i) const { return mData[i]; };
+    /**
+     * Make this vector a null vector.
+     **/
+    inline void reset()
+    {
+      mData[0] = mData[1] = mData[2] = mData[3] = 0.0f;
+    }
 
     /**
-     * Loads BoVector4 from KConfig
+     * @return The first (x) coordinate of the vector.
+     **/
+    inline GLfloat x() const { return mData[0]; }
+    /**
+     * @return The second (y) coordinate of the vector.
+     **/
+    inline GLfloat y() const { return mData[1]; }
+    /**
+     * @return The third (z) coordinate of the vector.
+     **/
+    inline GLfloat z() const { return mData[2]; }
+    /**
+     * @return The fourth (w) coordinate of the vector.
+     **/
+    inline GLfloat w() const { return mData[3]; }
+
+
+    /**
+     * Assign the values @p x, @p y, @p z, @p w to the vector.
+     **/
+    inline void set(GLfloat x, GLfloat y, GLfloat z, GLfloat w)
+    {
+      mData[0] = x;  mData[1] = y;  mData[2] = z; mData[3] = w;
+    }
+    /**
+     * @overload
+     **/
+    inline void set(const float* v)
+    {
+      set(v[0], v[1], v[2], v[3]);
+    }
+    /**
+     * @overload
+     **/
+    inline void set(const BoVector4& v)
+    {
+      set(v.data());
+    }
+
+    /**
+     * Assign the x coordinate to the vector.
+     **/
+    inline void setX(GLfloat x) { mData[0] = x; }
+    /**
+     * Assign the y coordinate to the vector.
+     **/
+    inline void setY(GLfloat y) { mData[1] = y; }
+    /**
+     * Assign the z coordinate to the vector.
+     **/
+    inline void setZ(GLfloat z) { mData[2] = z; }
+    /**
+     * Assign the w coordinate to the vector.
+     **/
+    inline void setW(GLfloat w) { mData[3] = w; }
+
+    /**
+     * Scale @p v by s and then add it to this vector.
+     **/
+    inline void addScaled(const BoVector4& v, GLfloat s)
+    {
+      mData[0] += v.mData[0] * s;  mData[1] += v.mData[1] * s;  mData[2] += v.mData[2] * s;  mData[3] += v.mData[3] * s;
+    }
+
+#if 0
+    // AB: see BoVector3::setScaledSum()
+    inline void setScaledSum(BoVector4 a, BoVector4 b,  GLfloat s)
+        { mData[0] = a.mData[0] + b.mData[0] * s;   mData[1] = a.mData[1] + b.mData[1] * s;   mData[2] = a.mData[2] + b.mData[2] * s;   mData[3] = a.mData[3] + b.mData[3] * s; };
+#endif
+
+    inline void setBlended(const BoVector4& a, float af, const BoVector4& b, float bf)
+    {
+      mData[0] = a.mData[0] * af + b.mData[0] * bf;
+      mData[1] = a.mData[1] * af + b.mData[1] * bf;
+      mData[2] = a.mData[2] * af + b.mData[2] * bf;
+      mData[3] = a.mData[3] * af + b.mData[3] * af;
+    }
+
+    /**
+     * Add @p v to this vector.
+     **/
+    inline void add(const BoVector4& v)
+    {
+      mData[0] += v.mData[0]; mData[1] += v.mData[1]; mData[2] += v.mData[2]; mData[3] += v.mData[3];
+    }
+
+    /**
+     * @return A pointer to the internal array.
+     **/
+    inline const GLfloat* data() const { return mData; }
+
+    /**
+     * See @ref set
+     **/
+    inline void operator=(const BoVector4& v)
+    {
+      set(v);
+    }
+    /**
+     * See @ref set
+     **/
+    inline void operator=(const GLfloat* v)
+    {
+      set(v);
+    }
+    /**
+     * @return The component / coordinate at @p i of this vector
+     **/
+    inline GLfloat operator[](int i) const { return mData[i]; }
+
+    /**
+     * Load a BoVector4 from @ref KConfig
      **/
     static BoVector4 load(const KConfig* cfg, const QString key, const BoVector4& aDefault = BoVector4());
 
+    /**
+     * @return A string that contains the vector @p v. This string can be used
+     * for debugging.
+     **/
     static QString debugString(const BoVector4& v);
+
+    /**
+     * Dump this vector onto the console. See also @ref debugString
+     **/
     static void debugVector(const BoVector4& v);
 
     /**
@@ -354,7 +659,6 @@ class BoMatrix
      **/
     const GLfloat* data() const { return mData; }
 
-
     /**
      * @return TRUE if <em>all</em> elements of this matrix are 0. Otherwise
      * FALSE.
@@ -451,7 +755,7 @@ class BoMatrix
     bool isEqual(const BoMatrix& matrix) const;
 
     /**
-     * @return @return The element at index @p i in the internal array. See @ref
+     * @return The element at index @p i in the internal array. See @ref
      * indexAt.
      **/
     inline GLfloat operator[](int i) const { return mData[i]; }
