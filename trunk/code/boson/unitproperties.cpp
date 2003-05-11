3989422cd976773d13294a6e8c3f29ea01b7a41e
benadler <benadler@14664f6f-540d-0410-967e-c81f6ba7cd39>
2003-05-11 20:37:19 +0000

    much better acceleration and deceleration values.
    I think we'll sometimes need a unit config-entry for accel and decel, since a big tank doesn't accel as fast as wolf which in turn doesn't accel as fast as
    warthog.

--- trunk/code/boson/unitproperties.cpp
+++ trunk/code/boson/unitproperties.cpp
@@ -243,8 +243,8 @@ void UnitProperties::loadMobileProperties(KSimpleConfig* conf)
 			" for unit " << typeId() << ", defaulting to 0" << endl;
 	mMobileProperties->mSpeed = 0;
  }
- mMobileProperties->mAccelerationSpeed = (float)conf->readDoubleNumEntry("AccelerationSpeed", 0.5);
- mMobileProperties->mDecelerationSpeed = (float)conf->readDoubleNumEntry("DecelerationSpeed", 1.0);
+ mMobileProperties->mAccelerationSpeed = (float)conf->readDoubleNumEntry("AccelerationSpeed", 0.02);
+ mMobileProperties->mDecelerationSpeed = (float)conf->readDoubleNumEntry("DecelerationSpeed", 0.05);
  mMobileProperties->mRotationSpeed = conf->readNumEntry("RotationSpeed", (int)(mMobileProperties->mSpeed * 2));
  mMobileProperties->mCanGoOnLand = conf->readBoolEntry("CanGoOnLand",
 		(isLand() || isAircraft()));
