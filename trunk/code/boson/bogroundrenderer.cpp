07f3b6e8525370c89d2ed9633a1d5ceb97c2dbd8
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2003-09-10 15:03:21 +0000

    - fixing #63624.   instead of using boUnProject() (which gives wrong values when the camera got   changed, but the screen not yet updated), we use the view frustum only. we   start by 4 lines that make a rectangle that covers the complete map. using all   6 planes of the frusutm we cut them down more and more until only the visible   ground is covered.
      this is a pretty complex function now, as a lot of things can happen (biggest
      problem is when multiple intersections with the same plane occur).
    
      since I have not been able to prove correctly how many lines can be created
      maximal, i used 30 lines as maximum. relaistic value is less than 10 or 12 in
      worst case. this makes the thing a bit slower, but only slightly. it won't be
      noticed, as the groundcelllist is updated only when camera is moved
    
      -> this is a pretty accurate function now (assuming that there are actually
      no bugs left). we still convert the generated lines into an axis aligned rect,
      one day we should fix this (it would improve speed greatly).
    
      note that z != 0 is not supported, so maps that make use of height maps might
      cause trouble. this is only a small problem, as the previous version had the
      same problem :)

--- trunk/code/boson/bogroundrenderer.cpp
+++ trunk/code/boson/bogroundrenderer.cpp
@@ -42,6 +42,13 @@
 #include <GL/gl.h>
 #include <math.h>
 
+// a couple of helper functions. these should be in Bo3dTools.
+static bool isInFrontOfPlane(const double* plane, const BoVector3& vector);
+static bool lineIntersects(const double* plane, const BoVector3& pos, const BoVector3& direction, BoVector3* intersection);
+static bool lineIntersects_points(const double* plane, const BoVector3& start, const BoVector3& end, BoVector3* intersection);
+static bool lineSegmentIntersects(const double* plane, const BoVector3& start, const BoVector3& end, BoVector3* intersection);
+#define EPSILON 0.0001f // zero for floating point numbers
+
 class BoGroundRendererPrivate
 {
 public:
@@ -234,6 +241,403 @@ Cell** BoGroundRenderer::createVisibleCellList(int* cells, PlayerIO* playerIO)
  return renderCells;
 }
 
+static bool isInFrontOfPlane(const double* plane, const BoVector3& vector)
+{
+ float d = vector[0] * plane[0] + vector[1] * plane[1] + vector[2] * plane[2] + plane[3];
+ if (d >= 0.0f) {
+	return true;
+ }
+ return false;
+}
+
+/**
+ * @return FALSE if the line segment does not intersect with the plane,
+ * otherwise TRUE. If the line segment is parallel to the @p plane, @p
+ * intersection is kept untouched, otherwise it will be the intersection point
+ * of the @p plane and the line (a line is infinite long and goes through @p
+ * start and @p end). If this function returns TRUE, that point will be on the
+ * line segment.
+ * @param t internal
+ **/
+static bool lineIntersects(const double* plane, const BoVector3& pos, const BoVector3& direction, BoVector3* intersection)
+{
+ const float planeDistance = plane[3];
+ const BoVector3 planeNormal = BoVector3(plane[0], plane[1], plane[2]);
+
+ // see count 2.1.0, base/SbPlane.cpp
+ if (fabsf(planeDistance) <= EPSILON) {
+	boError() << k_funcinfo << "plane distance is 0" << endl;
+	return false;
+ }
+
+ // we will _always_ have an intersection of line and plane if the line is not
+ // parallel to the plane.
+ // (AB: note that I said _line_ not line segment)
+ if (fabsf(BoVector3::dotProduct(direction, planeNormal)) <= EPSILON) {
+	return false;
+ }
+
+ // this formula is from coin 2.1.0, base/SbPlane.cpp, SbPlane::intersect(). it
+ // is documented there very well.
+ // AB: to me this looks like an error in coin. plib1.5 uses a slightly
+ // different version and this seems to work better
+ float t = -(planeDistance + BoVector3::dotProduct(planeNormal, pos)) / BoVector3::dotProduct(planeNormal, direction);
+
+ *intersection = pos + direction * t;
+
+ // AB: t is between 0 and 1 if the intersection is on the segment (at least it
+ // should be)
+
+ return true;
+}
+
+// just like above, but takes 2 points on the line, not 1 point and a direction
+static bool lineIntersects_points(const double* plane, const BoVector3& start, const BoVector3& end, BoVector3* intersection)
+{
+ const BoVector3 pos = start;
+ BoVector3 direction = end - start;
+ return lineIntersects(plane, pos, direction, intersection);
+}
+
+static bool lineSegmentIntersects(const double* plane, const BoVector3& start, const BoVector3& end, BoVector3* intersection)
+{
+ bool ret = lineIntersects_points(plane, start, end, intersection);
+
+ if (!ret) {
+	return false;
+ }
+
+ // we have the intersection of the plane with the line now. we still have to
+ // find out whether it is on the line segment.
+
+ bool f1 = isInFrontOfPlane(plane, start);
+ bool f2 = isInFrontOfPlane(plane, end);
+ if (f1 == f2) {
+	return false;
+ }
+ // one point is in front of and one behind the plane. so there must be an
+ // intersection between them. there can be at most one intersection with a
+ // plane, as the line (segment) is not parallel to the plane.
+ return true;
+}
+
+void BoGroundRenderer::calculateWorldRect(const QRect& rect, int mapWidth, int mapHeight, float* minX, float* minY, float* maxX, float* maxY)
+{
+ GLfloat posX, posY;
+ GLfloat posZ;
+
+ // AB: fallback. if everything fails, _nothing_ is visible at all.
+ *minX = *minY = *maxX = *maxY = 0.0f;
+
+ for (int i = 0; i < 6; i++) {
+	for (int j = 0; j < 4; j++) {
+		if (isnan(d->mViewFrustum[i * 4 + j])) {
+			boError() << k_funcinfo << "plane " << i << " is invalid" << endl;
+			return;
+		}
+	}
+ }
+
+
+ const float mapRight = (mapWidth - 1) * BO_GL_CELL_SIZE;
+ const float mapBottom = -(mapHeight - 1) * BO_GL_CELL_SIZE;
+
+ class LineSegment {
+ public:
+	// AB: a line has infinte length, a line segment has two end points.
+	LineSegment()
+	{
+		mNext = 0;
+		mPrevious = 0;
+		id = 0;
+	}
+	~LineSegment()
+	{
+		if (mNext) {
+			mNext->mPrevious = 0;
+		}
+		if (mPrevious) {
+			mPrevious->mNext = 0;
+		}
+	}
+
+	BoVector3 mStart;
+	BoVector3 mEnd;
+	LineSegment* mNext;
+	LineSegment* mPrevious;
+	int id;
+ };
+
+ // AB: we use at most 30 lines. this is a random value and we will never use
+ // all of them. if someone is willing to find out how many we need at most and
+ // is able to prove that - let me know and/or change this!
+#define MAX_LINES 30
+ LineSegment line[MAX_LINES];
+ for (int i = 0; i < MAX_LINES; i++) {
+	line[i].id = i;
+ }
+ LineSegment* first = &line[0];
+
+ line[0].mStart.set(0.0f, 0.0f, 0.0f);
+ line[0].mEnd.set(mapRight, 0.0f, 0.0f);
+ line[0].mNext = &line[1];
+
+ line[1].mPrevious = &line[0];
+ line[1].mStart.set(mapRight, 0.0f, 0.0f);
+ line[1].mEnd.set(mapRight, mapBottom, 0.0f);
+ line[1].mNext = &line[2];
+
+ line[2].mPrevious = &line[1];
+ line[2].mStart.set(mapRight, mapBottom, 0.0f);
+ line[2].mEnd.set(0.0f, mapBottom, 0.0f);
+ line[2].mNext = &line[3];
+
+ line[3].mPrevious = &line[2];
+ line[3].mStart.set(0.0f, mapBottom, 0.0f);
+ line[3].mEnd.set(0.0f, 0.0f, 0.0f);
+ line[3].mNext = &line[0];
+
+ line[0].mPrevious = &line[3];
+ int newLine = 4;
+
+ for (int i = 0; i < 6; i++) {
+	const double* plane = &d->mViewFrustum[i * 4];
+	// the possibilities:
+	// - the line is completely in front of the plane. nothing to do then.
+	// - the line intersects with the plane at some point.
+	//    we "cut" the line here, i.e. replace the start/end by the
+	//    intersecting point. this will create an additional line with
+	//    the start=intersecting point and end=start/end of the
+	//    next/previous line.
+	// - the line is completely behind the plane.
+	//    here we will have intersections with the plane and the next and
+	//    previous line (otherwise nothing would be visible at all). we
+	//    replace this line by the intersecting points of the next/previous
+	//    lines.
+	//
+	// UPDATE: the code using the above possibilities is working now
+	// (03/09/10). the above 3 possibilities are correct, but the code is
+	// gonna be a lot more complex, es a plane might intersect with several
+	// lines at once and we must fix the start/end points of these lines
+	// before adding the new lines.
+
+	// first pass - test whether the line segments intersect at all.
+	// non-intersecting non-visible lines get removed.
+	LineSegment* l = first;
+	do {
+		if (!first) {
+			first = l;
+		}
+		bool intersects = false;
+		BoVector3 intersection; // point where the line intersects
+		intersects = lineSegmentIntersects(plane, l->mStart, l->mEnd, &intersection);
+		if (!l->mNext || !l->mPrevious) {
+			boError() << k_funcinfo << "oops null next/previous pointer (pass1)" << endl;
+			return;
+		}
+		if (!intersects) {
+			if (isInFrontOfPlane(plane, l->mStart)) {
+				// good - the entire line is in front of the
+				// plane. we are done here.
+			} else {
+				// the entire line is behind the plane
+				// it is removed now.
+				l->mPrevious->mNext = l->mNext;
+				l->mNext->mPrevious = l->mPrevious;
+//				boDebug() << "removing line " << l->id << " start=" << l->mStart.debugString() << " end=" << l->mEnd.debugString() << endl;
+				if (l == first) {
+					first = 0;
+				}
+			}
+		} else {
+			// we care about this in the 2nd pass
+		}
+		l = l->mNext;
+	} while (l && l != first);
+
+
+	if (!first) {
+		return;
+	}
+
+	bool fixStart[MAX_LINES];
+	bool fixEnd[MAX_LINES];
+	LineSegment* addedLines[MAX_LINES];
+	int addedPos = 0;
+	for (int j = 0; j < MAX_LINES; j++) {
+		fixStart[j] = fixEnd[j] = false;
+		addedLines[j] = 0;
+	}
+
+	// 2nd pass. all lines are either in front of the plane or do intersect
+	// with it.
+	l = first;
+	do {
+		bool intersects = false;
+		BoVector3 intersection; // point where the line intersects
+		intersects = lineSegmentIntersects(plane, l->mStart, l->mEnd, &intersection);
+		if (!intersects) {
+//			boDebug() << "no intersection - line start=" << l->mStart.debugString() << " end=" << l->mEnd.debugString() << endl;
+			if (isInFrontOfPlane(plane, l->mStart)) {
+				// good - the entire line is in front of the
+				// plane. we are done here.
+			} else {
+				// should not be possible in pass 2!
+				boError() << k_funcinfo << "line does not intersect with plane! baaaad at this point - line " << l->id << endl;
+				return;
+			}
+		} else {
+			// here we also have to add a new line!
+//			boDebug() << "line " << l->id << " intersects start=" << l->mStart.debugString() << " end=" << l->mEnd.debugString() << " intersection=" << intersection.debugString() << endl;
+			if (isInFrontOfPlane(plane, l->mStart)) {
+				// mEnd must be replaced by intersection
+				l->mEnd = intersection;
+				line[newLine].mPrevious = l;
+				if (!l) {
+					return;
+				}
+				if (!l->mNext) {
+					return;
+				}
+				line[newLine].mNext = l->mNext;
+				line[newLine].mEnd = l->mNext->mStart;
+				line[newLine].mStart = intersection;
+				fixEnd[newLine] = true;
+				newLine++;
+			} else {
+				// mStart must be replaced by intersection
+				l->mStart = intersection;
+				line[newLine].mNext = l;
+				line[newLine].mPrevious = l->mPrevious;
+				line[newLine].mStart = l->mPrevious->mEnd;
+				line[newLine].mEnd = intersection;
+				fixStart[newLine] = true;
+				newLine++;
+			}
+			addedLines[addedPos] = &line[newLine-1];
+			addedPos++;
+//			boDebug() << " added line " << newLine-1 << " start=" << line[newLine-1].mStart.debugString() << " end=" << line[newLine-1].mEnd.debugString() << endl;
+			if (newLine >= MAX_LINES) {
+				// something evil must have happened
+				boError() << k_funcinfo << "too many new lines!" << endl;
+				return;
+			}
+		}
+		l = l->mNext;
+	} while (l && l != first);
+
+	if (!first) {
+		boWarning() << "nothing visible?" << endl;
+		return;
+	}
+
+	// 3rd pass. add new lines to the list
+	for (int j = 0; j < MAX_LINES && addedLines[j] != 0; j++) {
+		l = addedLines[j];
+		if (l->id == -1) {
+			continue;
+		}
+		LineSegment* n = l->mNext;
+		LineSegment* p = l->mPrevious;
+		if (!n || !p) {
+			boError() << k_funcinfo << "invalid line segment" << endl;
+			continue;
+		}
+		if (n->mPrevious != p) {
+			boError() << k_funcinfo << "cannot insert at invalid position - n->previous != previous" << endl;
+			continue;
+//			return;
+		}
+		if (p->mNext != n) {
+			boError() << k_funcinfo << "cannot insert at invalid position - p->next != next" << endl;
+			continue;
+//			return;
+		}
+		// nothing went wrong. let's insert.
+		p->mNext = l;
+		n->mPrevious = l;
+
+		// fix position
+		if (fixStart[l->id]) {
+			l->mStart = l->mPrevious->mEnd;
+		} else if (fixEnd[l->id]) {
+			l->mEnd = l->mNext->mStart;
+		}
+		for (int k = j + 1; k < MAX_LINES && addedLines[k] != 0; k++) {
+			LineSegment* s = addedLines[k];
+			if (s->mNext == n) {
+				if (s->mPrevious != p) {
+					// s->next == next && s->previous !=
+					// previous must not happen.
+					return;
+				}
+				if (s->mStart.isEqual(l->mEnd)) {
+					s->mPrevious = l;
+				} else if (s->mEnd.isEqual(l->mStart)) {
+					s->mNext = l;
+				} else if (s->mEnd.isEqual(l->mEnd) && s->mStart.isEqual(l->mStart)) {
+					// we already have this line.
+					s->id = -1;
+				} else {
+					// something weird happened.
+					// dont return - just skip it
+					s->id = -1;
+				}
+			} else if (s->mPrevious == p) {
+				// s->next == next && s->previous !=
+				// previous must not happen.
+				return;
+			}
+		}
+	}
+ }
+
+
+ if (!first) {
+	boWarning() << k_funcinfo << "nothing visible" << endl;
+	return;
+ }
+
+
+ // we return an axis aligned rect - in a future version we should drop that and
+ // return all lines
+ LineSegment* l = first;
+ // initialize max x/y to the minimum and min x/y to the maximum.
+ *maxX = *maxY = 0.0f;
+ *minX = (mapWidth - 1) * BO_GL_CELL_SIZE;
+ *minY = (mapHeight - 1) * BO_GL_CELL_SIZE;
+ int lineCount = 0;
+ do {
+	*maxX = QMAX(*maxX, l->mStart[0]);
+	*maxX = QMAX(*maxX, l->mEnd[0]);
+	*minX = QMIN(*minX, l->mStart[0]);
+	*minX = QMIN(*minX, l->mEnd[0]);
+
+	*maxY = QMAX(*maxY, -l->mStart[1]);
+	*maxY = QMAX(*maxY, -l->mEnd[1]);
+	*minY = QMIN(*minY, -l->mStart[1]);
+	*minY = QMIN(*minY, -l->mEnd[1]);
+//	boDebug() << l->id << ": " << l->mStart.debugString() << " " << l->mEnd.debugString() << endl;
+	l = l->mNext;
+	lineCount++;
+ } while (l && l != first);
+
+// boDebug() << "relevant lines: " << lineCount << endl;
+
+ *maxX = QMAX(0, *maxX);
+ *maxY = QMAX(0, *maxY);
+ *minX = QMAX(0, *minX);
+ *minY = QMAX(0, *minY);
+ *maxX = QMIN((mapWidth - 1) * BO_GL_CELL_SIZE, *maxX);
+ *minX = QMIN((mapWidth - 1) * BO_GL_CELL_SIZE, *minX);
+ *maxY = QMIN((mapHeight - 1) * BO_GL_CELL_SIZE, *maxY);
+ *minY = QMIN((mapHeight - 1) * BO_GL_CELL_SIZE, *minY);
+
+ *minY *= -1;
+ *maxY *= -1;
+
+}
+
 void BoGroundRenderer::generateCellList(const BosonMap* map)
 {
  // we need to regenerate the cell list whenever the modelview or the projection
@@ -263,6 +667,7 @@ void BoGroundRenderer::generateCellList(const BosonMap* map)
  calculateWorldRect(QRect(QPoint(0, 0), QPoint(d->mViewport[2], d->mViewport[3])),
 		map->width(), map->height(),
 		&minX, &minY, &maxX, &maxY);
+// boDebug() << "world rect: minX=" << minX << " maxX=" << maxX << " minY=" << minY << " maxY=" << maxY << endl;
  minY *= -1;
  maxY *= -1;
 
@@ -339,40 +744,6 @@ void BoGroundRenderer::generateCellList(const BosonMap* map)
  mRenderCellsCount = count;
 }
 
-void BoGroundRenderer::calculateWorldRect(const QRect& rect, int mapWidth, int mapHeight, float* minX, float* minY, float* maxX, float* maxY)
-{
- GLfloat posX, posY;
- GLfloat posZ;
- Bo3dTools::mapCoordinates(*d->mModelviewMatrix, *d->mProjectionMatrix, d->mViewport, rect.topLeft(), &posX, &posY, &posZ);
- *maxX = *minX = posX;
- *maxY = *minY = -posY;
- Bo3dTools::mapCoordinates(*d->mModelviewMatrix, *d->mProjectionMatrix, d->mViewport, rect.topRight(), &posX, &posY, &posZ);
- *maxX = QMAX(*maxX, posX);
- *maxY = QMAX(*maxY, -posY);
- *minX = QMIN(*minX, posX);
- *minY = QMIN(*minY, -posY);
- Bo3dTools::mapCoordinates(*d->mModelviewMatrix, *d->mProjectionMatrix, d->mViewport, rect.bottomLeft(), &posX, &posY, &posZ);
- *maxX = QMAX(*maxX, posX);
- *maxY = QMAX(*maxY, -posY);
- *minX = QMIN(*minX, posX);
- *minY = QMIN(*minY, -posY);
- Bo3dTools::mapCoordinates(*d->mModelviewMatrix, *d->mProjectionMatrix, d->mViewport, rect.bottomRight(), &posX, &posY, &posZ);
- *maxX = QMAX(*maxX, posX);
- *maxY = QMAX(*maxY, -posY);
- *minX = QMIN(*minX, posX);
- *minY = QMIN(*minY, -posY);
-
- *maxX = QMAX(0, *maxX);
- *maxY = QMAX(0, *maxY);
- *minX = QMAX(0, *minX);
- *minY = QMAX(0, *minY);
- *maxX = QMIN((mapWidth - 1) * BO_GL_CELL_SIZE, *maxX);
- *minX = QMIN((mapWidth - 1) * BO_GL_CELL_SIZE, *minX);
- *maxY = QMIN((mapHeight - 1) * BO_GL_CELL_SIZE, *maxY);
- *minY = QMIN((mapHeight - 1) * BO_GL_CELL_SIZE, *minY);
- *minY *= -1;
- *maxY *= -1;
-}
 
 BoDefaultGroundRenderer::BoDefaultGroundRenderer() : BoGroundRenderer()
 {
