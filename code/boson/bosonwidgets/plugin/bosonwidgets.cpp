4a78041b8ee2c1cc7fe759af573f85fa5acd3c94
halux2001 <halux2001@14664f6f-540d-0410-967e-c81f6ba7cd39>
2003-11-22 14:24:16 +0000

    first part of designer segfault fix, remove kdDebug() << k_funcinfo << key << endl; from create()
    Next one is harder...

--- code/boson/bosonwidgets/plugin/bosonwidgets.cpp
+++ code/boson/bosonwidgets/plugin/bosonwidgets.cpp
@@ -38,59 +38,59 @@
 class BosonWidgetsPlugin : public QWidgetPlugin
 {
 public:
-	BosonWidgetsPlugin();
-	virtual ~BosonWidgetsPlugin();
+  BosonWidgetsPlugin();
+  virtual ~BosonWidgetsPlugin();
 
-	virtual QStringList keys() const
-	{
-		QStringList list;
-		QMap<QString, WidgetInfo>::ConstIterator it;
-		for (it = mWidgets.begin(); it != mWidgets.end(); ++it) {
-			list << it.key();
-		}
-		return list;
-	}
-	virtual QWidget* create(const QString& key, QWidget* parent = 0, const char* name = 0);
-	virtual QIconSet iconSet(const QString& key) const
-	{
-		QString path = locate("data", "bosonwidgets/pics/" + mWidgets[key].iconSet);
-		return QIconSet(path);
-	}
-	virtual bool isContainer(const QString& key) const
-	{
-		return mWidgets[key].isContainer;
-	}
-	virtual QString group(const QString& key) const
-	{
-		return mWidgets[key].group;
-	}
-	virtual QString includeFile(const QString& key) const
-	{
-		return mWidgets[key].includeFile;
-	}
-	virtual QString tooltip(const QString& key) const
-	{
-		return mWidgets[key].toolTip;
-	}
-	virtual QString whatsThis(const QString& key) const
-	{
-		return mWidgets[key].whatsThis;
-	}
+  virtual QStringList keys() const
+  {
+    QStringList list;
+    QMap<QString, WidgetInfo>::ConstIterator it;
+    for (it = mWidgets.begin(); it != mWidgets.end(); ++it) {
+      list << it.key();
+    }
+    return list;
+  }
+  virtual QWidget* create(const QString& key, QWidget* parent = 0, const char* name = 0);
+  virtual QIconSet iconSet(const QString& key) const
+  {
+    QString path = locate("data", "bosonwidgets/pics/" + mWidgets[key].iconSet);
+    return QIconSet(path);
+  }
+  virtual bool isContainer(const QString& key) const
+  {
+    return mWidgets[key].isContainer;
+  }
+  virtual QString group(const QString& key) const
+  {
+    return mWidgets[key].group;
+  }
+  virtual QString includeFile(const QString& key) const
+  {
+    return mWidgets[key].includeFile;
+  }
+  virtual QString tooltip(const QString& key) const
+  {
+    return mWidgets[key].toolTip;
+  }
+  virtual QString whatsThis(const QString& key) const
+  {
+    return mWidgets[key].whatsThis;
+  }
 
 protected:
-	void addBosonWidget(const QString& className, const QString& includeFile, const QString& toolTip, const QString& whatsThis, const QString& iconSet = QString::null);
+  void addBosonWidget(const QString& className, const QString& includeFile, const QString& toolTip, const QString& whatsThis, const QString& iconSet = QString::null);
 
 private:
-	struct WidgetInfo
-	{
-		QString group;
-		QString iconSet;
-		QString includeFile;
-		QString toolTip;
-		QString whatsThis;
-		bool isContainer;
-	};
-	QMap<QString, WidgetInfo> mWidgets;
+  struct WidgetInfo
+  {
+    QString group;
+    QString iconSet;
+    QString includeFile;
+    QString toolTip;
+    QString whatsThis;
+    bool isContainer;
+  };
+  QMap<QString, WidgetInfo> mWidgets;
 };
 
 
@@ -126,20 +126,19 @@ void BosonWidgetsPlugin::addBosonWidget(const QString& className, const QString&
 
 QWidget* BosonWidgetsPlugin::create(const QString& key, QWidget* parent, const char* name)
 {
- kdDebug() << k_funcinfo << key << endl;
  if (key == "BoGameChatWidget") {
-	return new BoGameChatWidget(parent, name);
+  return new BoGameChatWidget(parent, name);
  } else if (key == "BosonMiniMap") {
-	// we cannot create an actual minimap widget, as it depends on Unit,
-	// Player, ...
-	// uic should generate code that does use BosonMiniMap.
-	return new BosonMiniMap(parent, name);
+  // we cannot create an actual minimap widget, as it depends on Unit,
+  // Player, ...
+  // uic should generate code that does use BosonMiniMap.
+  return new BosonMiniMap(parent, name);
  } else if (key == "BosonPlayFieldView") {
-	return new BosonPlayFieldView(parent, name);
+  return new BosonPlayFieldView(parent, name);
  } else if (key == "BoIntNumInput") {
-	return new BoIntNumInput(parent, name);
+  return new BoIntNumInput(parent, name);
  } else if (key == "BoColorChooser") {
-	return new BoColorChooser(parent, name);
+  return new BoColorChooser(parent, name);
  }
  return 0;
 }
