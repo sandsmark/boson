5bb2e71d36abb25e6b013e13096ee9b2ebe60ac9
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2003-12-13 22:31:18 +0000

    create the RessourceMineProperties plugin

--- code/boson/unitproperties.cpp
+++ code/boson/unitproperties.cpp
@@ -275,6 +275,9 @@ void UnitProperties::loadAllPluginProperties(KSimpleConfig* conf)
 	boDebug() << k_funcinfo << "loading refinery plugin properties" << endl;
 	loadPluginProperties(new RefineryProperties(this), conf);
  }
+ if (conf->hasGroup(RessourceMineProperties::propertyGroup())) {
+	loadPluginProperties(new RessourceMineProperties(this), conf);
+ }
 }
 
 void UnitProperties::loadPluginProperties(PluginProperties* prop, KSimpleConfig* conf)
