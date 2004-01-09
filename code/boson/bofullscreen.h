c9d276fa299dfadff68422f32b034307d4adbca5
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2004-01-09 21:21:03 +0000

    - a few BoFullScreen docs - support for the "usual" QWidget::showFullScreen() method   actually we only emulate it, we don't use it. we use the same code when we   change the resolution.
      there are a couple of problems left here, e.g. there are quite a lot of X
      errors. I have no idea which errors that are :(
      anyone knows a bit of X programming?
      anyway - its working for me. please test this (menu->window->show fullscreen
      and settings->configure boson->opengl->resolution)

--- code/boson/bofullscreen.h
+++ code/boson/bofullscreen.h
@@ -20,6 +20,7 @@
 #define BOFULLSCREEN_H
 
 class QStringList;
+class QWidget;
 
 class BoFullScreenPrivate;
 class BoFullScreen
@@ -31,22 +32,56 @@ public:
 	void initModes();
 
 	/**
+	 * Leave fullscreen mode. This will call @ref enterOriginalMode and
+	 * will additionally make the application a windowed application again.
+	 **/
+	static void leaveFullScreen();
+
+	/**
 	 * @return @ref availableModeList
 	 **/
 	static QStringList availableModes();
 
 	/**
+	 * See @ref enterModeInList
 	 * @param index Index as provided by @ref availableModes
 	 **/
 	static bool enterMode(int index);
 
 	/**
 	 * Enter the mode that was set when boson was started.
+	 * @return TRUE
 	 **/
 	static bool enterOriginalMode();
 
+	/**
+	 * Resize the @p widget to @p width and @p height. This method assumes
+	 * that @p width and @p height exactly describe the size of the current
+	 * resolution. The resized window will not have a menubar or a border,
+	 * i.e. it is treated as a fullscreen window (AB: probably this will
+	 * cause trouble with other window managers!)
+	 *
+	 * This method does <em>not</em> change the resolution! See also @ref
+	 * enterMode.
+	 *
+	 * Usually you don't need to call this, as it is called internally.
+	 **/
+	static void resizeToFullScreen(QWidget* widget, int width, int height);
+
 protected:
+	/**
+	 * @return A list of modes that we can switch to. If we cannot switch
+	 * the resolution an empty list is returned (not even the
+	 * default/current mode is returned!)
+	 **/
 	QStringList availableModeList();
+
+	/**
+	 * Change the resolution mode to @p index. @p index must be the index
+	 * of a mode in @ref availableModeList or -1 (meaning to go into usual
+	 * @ref QWidget::showFullScreen emulation mode)
+	 * @return TRUE on success, FALSE on failure.
+	 **/
 	bool enterModeInList(int index);
 
 private:
