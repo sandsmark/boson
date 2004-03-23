7f6d8ede7404bc8f7d243e824485b4cc9371f11f
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2004-03-23 20:57:33 +0000

    - Update some camera controlling methods (they were changed some time ago) - Add free camera mode demo (tested with "we are under attack" map, not enabled by default)

--- code/boson/script/localplayer-script.py
+++ code/boson/script/localplayer-script.py
@@ -25,6 +25,40 @@ def advance():
   cycle = cycle + 1
   #advanceBigCircle(cycle)
   #advanceA10Game(cycle)
+  #advanceFreeCamTest(cycle)
+
+
+def advanceFreeCamTest(cycle):
+  if (cycle % 20) == 0:
+    print "cycle: ", cycle
+
+  if cycle == 10:
+    print "checkpoint 1"
+    #BoScript.setCameraFree(1)
+  if cycle == 100:
+    print "checkpoint 2"
+    BoScript.setCameraLookAt(15, -15, 0)
+    BoScript.setCameraRotation(45)
+    BoScript.setCameraZ(7)
+    BoScript.setCameraRadius(5)
+    BoScript.commitCameraChanges(1)
+  elif cycle == 120:
+    print "checkpoint 3"
+    BoScript.setCameraFreeMode(1)
+  elif cycle == 140:
+    print "checkpoint 4"
+    # get current camera pos
+    camerapos = BoScript.cameraPos()
+    # camera will go 6 units down (will be 1 unit from the ground)
+    BoScript.setCameraPos(camerapos[0], camerapos[1], camerapos[2] - 6)
+    # lookat will be 2 units up from the new camera pos (camera will be looking upwards)
+    BoScript.setCameraLookAt(15, -15, camerapos[2] - 5)
+    # and camera will be slightly rotated
+    BoScript.setCameraUp(-0.5, 0, 1.0)
+    BoScript.commitCameraChanges(200)
+  elif cycle == 360:
+    print "checkpoint 5"
+    BoScript.setCameraFreeMode(0)
 
 
 def advanceA10Game(cycle):
@@ -47,16 +81,16 @@ def advanceA10Game(cycle):
 def advanceMaxPayne(cycle):
   x = cycle % 150
   if x == 9:
-    BoScript.moveCamera(5, -5, 0)
+    BoScript.setCameraLookAt(5, -5, 0)
     BoScript.setCameraRotation(45)
     BoScript.setCameraZ(4)
     BoScript.setCameraRadius(0)
     BoScript.commitCameraChanges(1)
   elif x == 10:
-    BoScript.moveCamera(25, -25, 0)
+    BoScript.setCameraLookAt(25, -25, 0)
     BoScript.commitCameraChanges(50)
   elif x == 60:
-    BoScript.moveCamera(45, -45, 0)
+    BoScript.setCameraLookAt(45, -45, 0)
     BoScript.setCameraZ(10)
     BoScript.setCameraRadius(10)
     BoScript.commitCameraChanges(50)
@@ -64,7 +98,7 @@ def advanceMaxPayne(cycle):
 def advanceBigCircle(cycle):
   if cycle == 100:
     BoScript.setCameraMoveMode(2)
-    BoScript.moveCamera(25, -25, 0)
+    BoScript.setCameraLookAt(25, -25, 0)
     BoScript.setCameraRotation(0)
     BoScript.setCameraZ(20)
     BoScript.setCameraRadius(20)
@@ -88,29 +122,29 @@ def advanceA10Video(cycle):
     boprint("info", "Let's move the camera!")
     BoScript.setCameraMoveMode(2)
     BoScript.setCameraRotation(-140)
-    BoScript.moveCamera(25, -33, 0)
+    BoScript.setCameraLookAt(25, -33, 0)
     BoScript.setCameraZ(10)
     BoScript.setCameraRadius(8)
     BoScript.commitCameraChanges(100)
   elif cycle == 250:
-    BoScript.moveCamera(27.6, -34.85, 0)
+    BoScript.setCameraLookAt(27.6, -34.85, 0)
     BoScript.setCameraRotation(400)
     BoScript.commitCameraChanges(100)
   elif cycle == 350:
-    BoScript.moveCamera(28.11, -35.22, 0)
+    BoScript.setCameraLookAt(28.11, -35.22, 0)
     BoScript.commitCameraChanges(20)
   elif cycle == 370:
-    BoScript.moveCamera(32, -38, 0)
+    BoScript.setCameraLookAt(32, -38, 0)
     BoScript.setCameraZ(15)
     BoScript.setCameraRadius(4)
     BoScript.commitCameraChanges(100)
   elif cycle == 470:
-    BoScript.moveCamera(35.9, -40.77, 0)
+    BoScript.setCameraLookAt(35.9, -40.77, 0)
     BoScript.setCameraZ(20)
     BoScript.setCameraRotation(-60)
     BoScript.commitCameraChanges(150)
   elif cycle == 620:
-    BoScript.moveCamera(37.2, -41.7, 0)
+    BoScript.setCameraLookAt(37.2, -41.7, 0)
     BoScript.setCameraZ(22)
     BoScript.setCameraRotation(270)
     BoScript.commitCameraChanges(100)
