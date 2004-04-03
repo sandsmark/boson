918530b7483e8d0cf260dd868ff83129f18e67cb
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2004-04-03 10:55:38 +0000

    area 270

--- trunk/code/boson/bosonmap.cpp
+++ trunk/code/boson/bosonmap.cpp
@@ -424,64 +424,64 @@ bool BosonMap::createNewMap(unsigned int width, unsigned int height, BosonGround
 
 bool BosonMap::loadMapFromFile(const QByteArray& mapXML)
 {
- boDebug() << k_funcinfo << endl;
+ boDebug(270) << k_funcinfo << endl;
  QDomDocument doc(QString::fromLatin1("BosonMap"));
  QString errorMsg;
  int line, column;
  if (!doc.setContent(QString(mapXML), &errorMsg, &line, &column)) {
-	boError() << k_funcinfo << "unable to load from mapXML (error in line " << line << ", column " << column << ", msg=" << errorMsg << ")" << endl;
+	boError(270) << k_funcinfo << "unable to load from mapXML (error in line " << line << ", column " << column << ", msg=" << errorMsg << ")" << endl;
 	return false;
  }
  QDomElement root = doc.documentElement();
  if (root.isNull()) {
-	boError() << k_funcinfo << "no root element" << endl;
+	boError(270) << k_funcinfo << "no root element" << endl;
 	return false;
  }
  if (!root.hasAttribute("Version")) {
-	boError() << k_funcinfo << "missing attribute: Version" << endl;
+	boError(270) << k_funcinfo << "missing attribute: Version" << endl;
 	return false;
  }
  int version = root.attribute("Version").toInt();
  if (version != BOSONMAP_VERSION) {
-	boError() << k_funcinfo << "invalid version " << version << " - need: " << BOSONMAP_VERSION << endl;
+	boError(270) << k_funcinfo << "invalid version " << version << " - need: " << BOSONMAP_VERSION << endl;
 	return false;
  }
  if (!root.hasAttribute(QString::fromLatin1("GroundTheme"))) {
-	boError() << k_funcinfo << "no GroundTheme attribute in root element" << endl;
+	boError(270) << k_funcinfo << "no GroundTheme attribute in root element" << endl;
 	return false;
  }
  QDomElement geometry = root.namedItem(QString::fromLatin1("Geometry")).toElement();
  if (geometry.isNull()) {
-	boError() << k_funcinfo << "no geometry element" << endl;
+	boError(270) << k_funcinfo << "no geometry element" << endl;
 	return false;
  }
  if (!geometry.hasAttribute(QString::fromLatin1("Width"))) {
-	boError() << k_funcinfo << "no Width attribute in Geometry element" << endl;
+	boError(270) << k_funcinfo << "no Width attribute in Geometry element" << endl;
 	return false;
  }
  if (!geometry.hasAttribute(QString::fromLatin1("Height"))) {
-	boError() << k_funcinfo << "no Height attribute in Geometry element" << endl;
+	boError(270) << k_funcinfo << "no Height attribute in Geometry element" << endl;
 	return false;
  }
  bool ok = false;
  QString groundTheme = root.attribute(QString::fromLatin1("GroundTheme"));
  int width = geometry.attribute(QString::fromLatin1("Width")).toInt(&ok);
  if (!ok || width < 0) {
-	boError() << k_funcinfo << "width is not a valid number" << endl;
+	boError(270) << k_funcinfo << "width is not a valid number" << endl;
 	return false;
  }
  int height = geometry.attribute(QString::fromLatin1("Height")).toInt(&ok);
  if (!ok || height < 0) {
-	boError() << k_funcinfo << "height is not a valid number" << endl;
+	boError(270) << k_funcinfo << "height is not a valid number" << endl;
 	return false;
  }
 
  if (!loadMapGeo((unsigned int)width, (unsigned int)height)) {
-	boError() << k_funcinfo << "Could not load map geo" << endl;
+	boError(270) << k_funcinfo << "Could not load map geo" << endl;
 	return false;
  }
  if (!loadGroundTheme(groundTheme)) {
-	boError() << k_funcinfo << "Could not load the ground theme" << endl;
+	boError(270) << k_funcinfo << "Could not load the ground theme" << endl;
 	return false;
  }
  return true;
