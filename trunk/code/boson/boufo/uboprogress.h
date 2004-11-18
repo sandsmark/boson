81e34fba1a613fc23244821c62c601884e8e7b59
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2004-11-18 23:04:47 +0000

    - adding UBoProgress, an implementation of a progress bar.   is supposed to be a replacement for KGameProgress in our code
      note that atm it is designed for use in the command frame, i.e. the progress
      bar is a gradient from red to green and not yet configurable.
    - support for NULL icons

---
+++ trunk/code/boson/boufo/uboprogress.h
@@ -0,0 +1,68 @@
+/***************************************************************************
+    LibUFO - UI For OpenGL
+    copyright         : (C) 2004 by Andreas Beckermann
+    email             : b_mann at gmx.de
+                             -------------------
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *  This library is free software; you can redistribute it and/or          *
+ * modify it under the terms of the GNU Lesser General Public              *
+ * License as published by the Free Software Foundation; either            *
+ * version 2.1 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ * This library is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       *
+ * Lesser General Public License for more details.                         *
+ *                                                                         *
+ * You should have received a copy of the GNU Lesser General Public        *
+ * License along with this library; if not, write to the Free Software     *
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA *
+ ***************************************************************************/
+
+
+#ifndef UBOPROGRESS_H
+#define UBOPROGRESS_H
+
+#include <ufo/widgets/uwidget.hpp>
+
+namespace ufo {
+
+class UFO_EXPORT UBoProgress : public UWidget {
+	UFO_DECLARE_DYNAMIC_CLASS(UBoProgress)
+	UFO_UI_CLASS(UBoProgressUI)
+public:
+	UBoProgress(Orientation orientation = Horizontal);
+
+	void setOrientation(Orientation o);
+	Orientation getOrientation() const {
+		return m_orientation;
+	}
+	void setMinimumValue(double v);
+	void setMaximumValue(double v);
+	void setValue(double v);
+	double getMinimumValue() const {
+		return m_min;
+	}
+	double getMaximumValue() const {
+		return m_max;
+	}
+	double getValue() const {
+		return m_value;
+	}
+
+protected:
+
+private:  // Private attributes
+	double m_min;
+	double m_max;
+	double m_value;
+	Orientation m_orientation;
+};
+
+} // namespace ufo
+
+#endif // UBOLABELUI_H
+
