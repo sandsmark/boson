c0241259a26551ab1a055cdab67caba10ec73254
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2004-12-11 11:10:47 +0000

    - Oops   Fix the compilation issues

--- trunk/code/boson/script/bosonscript.cpp
+++ trunk/code/boson/script/bosonscript.cpp
@@ -526,18 +526,13 @@ void BosonScript::placeProduction(int player, int factoryid, float x, float y)
   {
     boError() << k_lineinfo << "not producing unit!" << endl;
     return;
-#warning fixme
-//    break;
   }
   int unitType = prod->completedProductionId();
   if (unitType <= 0) {
     // hope this is working...
     boWarning() << k_lineinfo << "not yet completed" << endl;
     return;
-#warning fixme
-//    break;
   }
-  //game()->buildProducedUnit(prod, unitType, BoVector2Fixed(x, y));
 
   QByteArray b;
   QDataStream stream(b, IO_WriteOnly);
