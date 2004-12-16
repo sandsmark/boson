8923cc11d4af466b0a60cdcbb5ed24a92a81982c
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2004-12-16 00:26:39 +0000

    adding missing break statement

--- code/boson/boufo/boufoaction.cpp
+++ code/boson/boufo/boufoaction.cpp
@@ -1333,6 +1333,7 @@ BoUfoAction* BoUfoStdAction::create(StdAction id, const QObject* receiver, const
 		const KAboutData* aboutData = KGlobal::instance()->aboutData();
 		QString appName = (aboutData) ? aboutData->programName() : QString::fromLatin1(qApp->name());
 		label = i18n(info->label).arg(appName);
+		break;
 	}
 	default:
 		label = i18n(info->label);
