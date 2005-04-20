0cb3b8c4b3a4fdecfe3499afda9998c97568e624
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2005-04-20 03:50:14 +0000

    Porting the main startup widget to BoUfo. The not-yet-ported widgets have been #ifdefed out.
    The port is untested, however I expect little problems here only.

---
+++ trunk/code/boson/startupwidgets/boufostartupwidget.cpp
@@ -0,0 +1,554 @@
+/*
+    This file is part of the Boson game
+    Copyright (C) 2002-2005 The Boson Team (boson-devel@lists.sourceforge.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "boufostartupwidget.h"
+#include "boufostartupwidget.moc"
+
+#include "bosonwelcomewidget.h"
+#include "bosonloadingwidget.h"
+#include "boufonewgamewidget.h"
+#include "bosonneweditorwidget.h"
+#include "bosonnetworkoptionswidget.h"
+#include "kloadsavegamewidget.h"
+#include "bosonstartupnetwork.h"
+#include "bodebug.h"
+#include "../boson.h"
+#include "../defines.h"
+#include "welcomewidget.h"
+
+#include <kmainwindow.h> // AB: urghs
+#include <kstandarddirs.h>
+#include <kmessagebox.h>
+#include <klocale.h>
+#include <kcmdlineargs.h>
+
+#include <qpixmap.h>
+#include <qlabel.h>
+#include <qlayout.h>
+#include <qpopupmenu.h>
+#include <qcursor.h>
+#include <qobjectlist.h>
+#include <qguardedptr.h>
+
+#include <stdlib.h>
+
+#define LOADSAVE_WIDGET 0
+#define LOADING_WIDGET 0
+#define NETWORK_WIDGET 0
+#define EDITOR_WIDGET 0
+
+class BoUfoStartupWidgetPrivate
+{
+public:
+	BoUfoStartupWidgetPrivate()
+	{
+		mWidgetStack = 0;
+
+		mBackgroundPix = 0;
+		mLogoPix = 0;
+
+		mNetworkInterface = 0;
+
+		mLocalPlayer = 0;
+	}
+
+	BoUfoWidgetStack* mWidgetStack;
+
+	QPixmap* mBackgroundPix;
+	QPixmap* mLogoPix;
+
+	BosonStartupNetwork* mNetworkInterface;
+
+	QGuardedPtr<Player> mLocalPlayer;
+};
+
+BoUfoStartupWidget::BoUfoStartupWidget() : BoUfoWidget()
+{
+ init();
+}
+
+void BoUfoStartupWidget::init()
+{
+ d = new BoUfoStartupWidgetPrivate;
+ d->mWidgetStack = new BoUfoWidgetStack();
+
+ d->mBackgroundPix = new QPixmap(locate("data", "boson/pics/boson-startup-bg.png"));
+ d->mLogoPix = new QPixmap(locate("data", "boson/pics/boson-startup-logo.png"));
+
+#if 0
+ setBackgroundMode(FixedPixmap);
+ setPaletteBackgroundPixmap(*d->mBackgroundPix);
+ d->mWidgetStack->setPaletteBackgroundPixmap(*d->mBackgroundPix);
+#endif
+
+ BoUfoLabel* logo = new BoUfoLabel();
+// logo->setPixmap(*d->mLogoPix);
+ logo->setIcon(*d->mLogoPix);
+
+ setLayoutClass(BoUfoWidget::UVBoxLayout);
+ addSpacing(5); // FIXME: hardcoded
+ addWidget(logo); // TODO: AlignHCenter
+ addWidget(d->mWidgetStack); // TODO: stretch factor 1
+
+ d->mNetworkInterface = new BosonStartupNetwork(this);
+ d->mNetworkInterface->setGame(boGame);
+
+ installEventFilter(this); // for the popup menu
+}
+
+BoUfoStartupWidget::~BoUfoStartupWidget()
+{
+ delete d->mLogoPix;
+ delete d->mBackgroundPix;
+ delete d;
+}
+
+void BoUfoStartupWidget::setLocalPlayer(Player* p)
+{
+ d->mLocalPlayer = p;
+ BoUfoNewGameWidget* w = (BoUfoNewGameWidget*)d->mWidgetStack->widget(IdNewGame);
+ if (w) {
+	w->setLocalPlayer(d->mLocalPlayer);
+ }
+}
+
+void BoUfoStartupWidget::slotLoadGame()
+{
+ showWidget(IdLoadSaveGame);
+ KLoadSaveGameWidget* loadSave = (KLoadSaveGameWidget*)d->mWidgetStack->widget(IdLoadSaveGame);
+ if (!loadSave) {
+	boError() << k_funcinfo << "load/save widget hasn't been initialized?!" << endl;
+	return;
+ }
+ loadSave->setSaveMode(false);
+ loadSave->updateGames();
+}
+
+void BoUfoStartupWidget::slotSaveGame()
+{
+ // TODO pause game!
+ showWidget(IdLoadSaveGame);
+ KLoadSaveGameWidget* loadSave = (KLoadSaveGameWidget*)d->mWidgetStack->widget(IdLoadSaveGame);
+ if (!loadSave) {
+	boError() << k_funcinfo << "load/save widget hasn't been initialized?!" << endl;
+	return;
+ }
+ loadSave->setSaveMode(true);
+ loadSave->updateGames();
+}
+
+void BoUfoStartupWidget::slotNewGame(KCmdLineArgs* args)
+{
+ showWidget(IdNewGame);
+ if (!args) {
+	return;
+ }
+ BoUfoNewGameWidget* w = (BoUfoNewGameWidget*)d->mWidgetStack->widget(IdNewGame);
+ if (!w) {
+	boError() << k_funcinfo << "Oops - NULL newgame widget" << endl;
+	return;
+ }
+ // here we can check for some things like --playfield and call the functions
+ // e.g.:
+ if (args->isSet("playfield")) {
+	QString identifier = args->getOption("playfield");
+	if (identifier.right(4) != QString::fromLatin1(".bpf")) {
+		identifier = identifier + QString::fromLatin1(".bpf");
+	}
+	networkInterface()->sendChangePlayField(identifier);
+ }
+ if (args->isSet("computer")) {
+	QString count = args->getOption("computer");
+	bool ok = true;
+	int c = count.toInt(&ok);
+	if (!ok) {
+		boError() << k_funcinfo << "computer player count was not a valid number" << endl;
+		return;
+	}
+	if (c >= BOSON_MAX_PLAYERS) {
+		boWarning() << k_funcinfo << "Cannot add " << c << " players" << endl;
+		boDebug() << k_funcinfo << "changing to " << BOSON_MAX_PLAYERS - 1<< endl;
+		c = BOSON_MAX_PLAYERS - 1;
+	}
+	
+	for (int i = 0; i < c; i++) {
+		// note: we don't have to care about which playfield was loaded,
+		// as we can *always* add unlimited players. player number is
+		// checked when starting game only
+		w->addAIPlayer();
+	}
+ }
+
+
+ if (args->isSet("start")) {
+	w->slotStartGame();
+ }
+}
+
+void BoUfoStartupWidget::slotStartEditor(KCmdLineArgs* args)
+{
+ showWidget(IdStartEditor);
+ if (!args) {
+	return;
+ }
+ // here we can check for some things like --playfield and call the functions
+ // e.g.:
+ if (args->isSet("playfield")) {
+	QString identifier = args->getOption("playfield");
+	if (identifier.right(4) != QString::fromLatin1(".bpf")) {
+		identifier = identifier + QString::fromLatin1(".bpf");
+	}
+	networkInterface()->sendChangePlayField(identifier);
+ }
+
+
+ if (args->isSet("start")) {
+	networkInterface()->sendStartGameClicked();
+ }
+}
+
+void BoUfoStartupWidget::showWidget(WidgetId widgetId)
+{
+ initWidget(widgetId);
+
+ if (!d->mWidgetStack->widget((int)widgetId)) {
+	boError() << k_funcinfo << "NULL widget " << widgetId << endl;
+	return;
+ }
+
+ // TODO: see Top::raiseWidget() ?
+ d->mWidgetStack->raiseWidget((int)widgetId);
+ d->mWidgetStack->widget((int)widgetId)->show();
+}
+
+void BoUfoStartupWidget::initWidget(WidgetId widgetId)
+{
+ if (d->mWidgetStack->widget((int)widgetId)) {
+	// already initialized
+	return;
+ }
+ BoUfoWidget* w = 0;
+ switch (widgetId) {
+	case IdWelcome:
+	{
+		BoUfoWelcomeWidget* welcome = new BoUfoWelcomeWidget();
+		connect(welcome, SIGNAL(signalQuit()), this, SIGNAL(signalQuit()));
+		connect(welcome, SIGNAL(signalNewGame()), this, SLOT(slotNewGame()));
+		connect(welcome, SIGNAL(signalLoadGame()), this, SLOT(slotLoadGame()));
+		connect(welcome, SIGNAL(signalStartEditor()), this, SLOT(slotStartEditor()));
+		w = welcome;
+		break;
+	}
+	case IdLoading:
+	{
+#if LOADING_WIDGET
+		BosonLoadingWidget* loadingWidget = new BosonLoadingWidget(d->mWidgetStack);
+
+		w = loadingWidget;
+#endif
+		break;
+	}
+	case IdLoadSaveGame:
+	{
+#if LOADSAVE_WIDGET
+		KLoadSaveGameWidget* loadSaveWidget = new KLoadSaveGameWidget(d->mWidgetStack);
+		loadSaveWidget->setSuffix(QString::fromLatin1("bsg"));
+		connect(loadSaveWidget, SIGNAL(signalLoadGame(const QString&)),
+				this, SIGNAL(signalLoadGame(const QString&)));
+		connect(loadSaveWidget, SIGNAL(signalSaveGame(const QString&, const QString&)),
+				this, SIGNAL(signalSaveGame(const QString&, const QString&)));
+		connect(loadSaveWidget, SIGNAL(signalCancel()),
+				this, SIGNAL(signalCancelLoadSave()));
+		w = loadSaveWidget;
+#endif
+		break;
+	}
+	case IdNewGame:
+	{
+		BoUfoNewGameWidget* startGame = new BoUfoNewGameWidget(networkInterface());
+		connect(startGame, SIGNAL(signalCancelled()),
+				this, SLOT(slotShowWelcomeWidget()));
+		connect(startGame, SIGNAL(signalShowNetworkOptions()),
+				this, SLOT(slotShowNetworkOptions()));
+		connect(startGame, SIGNAL(signalKickedOut()),
+				this, SLOT(slotKickedOut()));
+
+		// AB: this does nothing, as d->mLocalPlayer is NULL. but maybe
+		// that will change.
+		startGame->setLocalPlayer(d->mLocalPlayer);
+
+		w = startGame;
+		break;
+	}
+	case IdNetwork:
+	{
+#if NETWORK_WIDGET
+		BosonNetworkOptionsWidget* networkOptions = new BosonNetworkOptionsWidget(this);
+		connect(networkOptions, SIGNAL(signalOkClicked()),
+				this, SLOT(slotHideNetworkOptions()));
+		connect(networkOptions, SIGNAL(signalOfferingConnections()),
+				this, SLOT(slotOfferingConnections()));
+		connect(networkOptions, SIGNAL(signalConnectingToServer()),
+				this, SLOT(slotConnectingToServer()));
+		connect(networkOptions, SIGNAL(signalConnectedToServer()),
+				this, SLOT(slotConnectedToServer()));
+
+		w = networkOptions;
+#endif
+		break;
+	}
+	case IdStartEditor:
+	{
+#if EDITOR_WIDGET
+		BosonNewEditorWidget* startEditor = new BosonNewEditorWidget(networkInterface(), this);
+		connect(startEditor, SIGNAL(signalCancelled()),
+				this, SLOT(slotShowWelcomeWidget()));
+
+		w = startEditor;
+#endif
+		break;
+	}
+	case IdLast:
+		boError() << k_funcinfo << "invalid id " << (int)widgetId << endl;
+		return;
+ }
+ if (!w) {
+	boError() << k_funcinfo << "NULL widget" << endl;
+	return;
+ }
+ w->installEventFilter(this); // for the popup menu
+
+ d->mWidgetStack->insertWidget(w, (int)widgetId);
+
+ if (widgetId == IdNewGame) {
+	// the new game widget requires a local player. this gets added
+	// here.
+	// note that the new player ends up in boGame->playerList() once we
+	// return to the event loop only, NOT immediately
+	emit signalAddLocalPlayer();
+ }
+}
+
+BosonLoadingWidget* BoUfoStartupWidget::loadingWidget() const
+{
+ return (BosonLoadingWidget*)d->mWidgetStack->widget(IdLoading);
+}
+
+void BoUfoStartupWidget::showLoadingWidget()
+{
+ showWidget(IdLoading);
+}
+
+void BoUfoStartupWidget::slotLoadingType(int type)
+{
+ if (loadingWidget()) {
+	loadingWidget()->setLoading((BosonLoadingWidget::LoadingType)type);
+ }
+}
+
+void BoUfoStartupWidget::slotLoadingShowProgressBar(bool s)
+{
+ if (loadingWidget()) {
+	loadingWidget()->showProgressBar(s);
+ }
+}
+
+void BoUfoStartupWidget::slotLoadingReset()
+{
+ if (loadingWidget()) {
+	loadingWidget()->resetProgress();
+ }
+}
+
+void BoUfoStartupWidget::slotLoadingSetAdmin(bool isAdmin)
+{
+ if (loadingWidget()) {
+	loadingWidget()->setAdmin(isAdmin);
+ }
+}
+
+void BoUfoStartupWidget::slotLoadingSetLoading(bool isLoading)
+{
+ if (loadingWidget()) {
+	loadingWidget()->setLoading(isLoading);
+ }
+}
+
+void BoUfoStartupWidget::slotLoadingPlayersCount(int count)
+{
+ if (loadingWidget()) {
+	loadingWidget()->setTotalPlayers(count);
+ }
+}
+
+void BoUfoStartupWidget::slotLoadingPlayer(int current)
+{
+ if (loadingWidget()) {
+	loadingWidget()->setCurrentPlayer(current);
+ }
+}
+
+void BoUfoStartupWidget::slotLoadingUnitsCount(int count)
+{
+ if (loadingWidget()) {
+	loadingWidget()->setTotalUnits(count);
+ }
+}
+
+void BoUfoStartupWidget::slotLoadingUnit(int current)
+{
+ if (loadingWidget()) {
+	loadingWidget()->setCurrentUnit(current);
+ }
+}
+
+void BoUfoStartupWidget::slotShowNetworkOptions()
+{
+ showWidget(IdNetwork);
+}
+
+void BoUfoStartupWidget::slotHideNetworkOptions()
+{
+ BoUfoNewGameWidget* startGame = (BoUfoNewGameWidget*)d->mWidgetStack->widget(IdNewGame);
+ if (!startGame) {
+	// strange as network widget gets shown for newgame widget only
+	boError() << k_funcinfo << "NULL new game widget??" << endl;
+	return;
+ }
+ removeWidget(IdNetwork);
+ startGame->slotSetAdmin(boGame->isAdmin());
+ showWidget(IdNewGame);
+}
+
+bool BoUfoStartupWidget::eventFilter(QObject* o, QEvent* e)
+{
+ switch (e->type()) {
+	case QEvent::MouseButtonPress:
+		if (((QMouseEvent*)e)->button() == RightButton) {
+			// this is pretty ugly, but im afraid its necessary.
+			KMainWindow* main = (KMainWindow*)qApp->mainWidget();
+			if (!main) {
+				boError() << k_funcinfo << "NULL main widget?!" << endl;
+				return true;
+			}
+			if (!main->factory()) {
+				boError() << k_funcinfo << "NULL factory (this is expected until the startup widgets are ported to libufo)" << endl;
+				return true;
+			}
+			QPopupMenu* p = (QPopupMenu*)main->factory()->container("welcomepopup", main);
+			if (p) {
+				p->popup(QCursor::pos());
+			}
+			return true;
+		}
+		break;
+	case IdLast:
+	default:
+		break;
+ }
+ return BoUfoWidget::eventFilter(o, e);
+}
+
+// this shows the welcome widget and emits signalResetGame(), i.e. resets the
+// game!
+void BoUfoStartupWidget::slotShowWelcomeWidget()
+{
+ boDebug() << k_funcinfo << endl;
+ showWidget(IdWelcome);
+
+ // reset the game now:
+ // first remove all widgets except the welcome widget
+ BoUfoWidget* w = d->mWidgetStack->widget((int)IdWelcome);
+ if (w) {
+	d->mWidgetStack->removeWidget(w);
+ }
+ resetWidgets();
+ if (w) {
+	d->mWidgetStack->insertWidget(w, (int)IdWelcome);
+ }
+ d->mWidgetStack->raiseWidget((int)IdWelcome);
+ emit signalResetGame();
+ networkInterface()->setGame(boGame);
+
+ // the startup widget gets hidden when game is started, so when we want to show
+ // the welcome widget we also need to show the startup widget
+ show();
+}
+
+void BoUfoStartupWidget::resetWidgets()
+{
+ for (int i = 0; i < IdLast; i++) {
+	removeWidget((WidgetId)i);
+ }
+}
+
+void BoUfoStartupWidget::removeWidget(WidgetId widgetId)
+{
+ BoUfoWidget* w = d->mWidgetStack->widget((int)widgetId);
+ if (w) {
+	d->mWidgetStack->removeWidget(w);
+	delete w;
+ }
+}
+
+BosonStartupNetwork* BoUfoStartupWidget::networkInterface() const
+{
+ return d->mNetworkInterface;
+}
+
+void BoUfoStartupWidget::slotOfferingConnections()
+{
+ BoUfoWidget* w = d->mWidgetStack->widget(IdNewGame);
+ if (!w) {
+	boError() << k_funcinfo << "No new game widget!!!" << endl;
+	return;
+ }
+ ((BoUfoNewGameWidget*)w)->slotOfferingConnections();
+}
+
+void BoUfoStartupWidget::slotConnectingToServer()
+{
+ BoUfoWidget* w = d->mWidgetStack->widget(IdNewGame);
+ if (!w) {
+	boError() << k_funcinfo << "No new game widget!!!" << endl;
+	return;
+ }
+ ((BoUfoNewGameWidget*)w)->slotConnectingToServer();
+}
+
+void BoUfoStartupWidget::slotConnectedToServer()
+{
+ BoUfoWidget* w = d->mWidgetStack->widget(IdNewGame);
+ if (!w) {
+	boError() << k_funcinfo << "No new game widget!!!" << endl;
+	return;
+ }
+ ((BoUfoNewGameWidget*)w)->slotConnectedToServer();
+}
+
+
+void BoUfoStartupWidget::slotKickedOut()
+{
+ boDebug() << k_funcinfo << "disconnect" << endl;
+ boGame->disconnect();
+ boDebug() << k_funcinfo << "disconnect DONE" << endl;
+ boDebug() << k_funcinfo << "re-adding local player" << endl;
+ emit signalAddLocalPlayer();
+}
+
