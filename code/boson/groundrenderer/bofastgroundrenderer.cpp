b10f9921b47f0a36b30814d54fe9f4b1c5a800c5
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2005-06-17 21:48:35 +0000

    fix copyright

--- code/boson/groundrenderer/bofastgroundrenderer.cpp
+++ code/boson/groundrenderer/bofastgroundrenderer.cpp
@@ -1,6 +1,6 @@
 /*
     This file is part of the Boson game
-    Copyright (C) 2003 The Boson Team (boson-devel@lists.sourceforge.net)
+    Copyright (C) 2003-2005 Andreas Beckermann (b_mann@gmx.de)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
