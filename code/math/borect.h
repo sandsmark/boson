b6810802ab3ba673b530843dd8c69a6e0012bdce
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2005-07-01 20:07:47 +0000

    fix copyright

--- code/math/borect.h
+++ code/math/borect.h
@@ -1,6 +1,6 @@
 /*
     This file is part of the Boson game
-    Copyright (C) 2002-2005 The Boson Team (boson-devel@lists.sourceforge.net)
+    Copyright (C) 2002-2005 Rivo Laks (rivolaks@hot.ee)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
