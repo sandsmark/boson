34b865908d83b498b405ed111bd05380a9d7932e
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2005-09-02 22:35:03 +0000

    - Add 0.11 announcement

--- trunk/web/announcements.php
+++ trunk/web/announcements.php
@@ -57,6 +57,7 @@ draw_bigbox_text("All Boson announcements:
     <li><a href=\"announces/boson-0.9.php\">Boson 0.9 announcement</a></li>
     <li><a href=\"announces/boson-0.9.1.php\">Boson 0.9.1 announcement</a></li>
     <li><a href=\"announces/boson-0.10.php\">Boson 0.10 announcement</a></li>
+    <li><a href=\"announces/boson-0.11.php\">Boson 0.11 announcement</a></li>
     </ul>
     <p class=\"bigboxtext\">");
 draw_bigbox_end();
