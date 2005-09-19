d091aefd5817d5907397c0ac8ffc107980b2a7f6
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2005-09-19 18:21:28 +0000

    - greatly improve facility placement: instead of using the simple   BoScript.cellOccupied(), we now use the new BoScript.canPlaceProductionAt().   this reduces the delay until a facility is placed _greatly_ - in addition: use a randomized distance (between 0 and 20) from the factory for   the placement   this makes the base less crowded. however with many facilities, it still will   be very crowded. we somehow need some way to find the border of the base of   the player, so that we can place facilities there. - flip rule 2 (build refineries) and rule 3 (build weapns factory): without a   weapons factory the refineries are useless anyway (no harvesters). and when we   can't build them all, then with a weaponsfactory we at least can build some   tanks with the remaining resources - don't produce complex (i.e. expensive) facilities if resources are low, unless   we already have harvesters. - prefer mineral to oil harvesters. this makes sense because most things cost   more minerals than oil, so we must gain minerals faster than oil. - a little bit more randomizing, especially on unit productions: make a list of   wanted units and shuffle() that list. then try to build items from it.
    the production scripts work pretty fine for me already: on oneunit the AI can
    build up a base nearly without user interaction - only for "unlocking"
    harvesters, sometimes user interaction is required.
    however this is mostly specific to this map, as the resources are very close to
    the base here.

--- data/themes/scripts/ai_produce.py
+++ data/themes/scripts/ai_produce.py
@@ -1,6 +1,6 @@
 from sys import exit
 from utils import *
-from random import randint
+from random import randint, shuffle
 
 # This is just to be able to test the syntax whith you
 # Python interpreter on the console
@@ -24,7 +24,7 @@ def unitProduced(ownerid, pos, type, factorid):
   if(ownerid != ai.player):
     return
   boprint("debug","unit with id %s produced" % type)
-  placeUnit(factorid, type)
+  placeUnit(int(factorid), int(type))
 
 def placeUnit(factory, unitType):
   if(BoScript.isUnitTypeMobile(ai.player, int(unitType)) == False):
@@ -32,22 +32,21 @@ def placeUnit(factory, unitType):
     pos = BoScript.unitPosition(int(factory))
     x = int(pos[0])
     y = int(pos[1])
-    i = 0
-    tmpx = randint(x-i, x+i)
-    tmpy = randint(y-i, y+i)
-    if(tmpx < 0):
-      tmpx = x
-    if(tmpy < 0):
-      tmpy = y
-    while (BoScript.cellOccupied( tmpx,tmpy) == True):
-      tmpx = randint(x-i, x+i)
-      tmpy = randint(y-i, y+i)
+    tmpx = x
+    tmpy = y
+
+    # AB: don't make unlimitied tries (avoid infinite loop)
+    tries = 0
+    while BoScript.canPlaceProductionAt(ai.player, factory, unitType, int(tmpx), int(tmpy)) != 1 and tries < 30:
+      distance = randint(0, 20)
+      tmpx = randint(x-distance, x+distance)
+      tmpy = randint(y-distance, y+distance)
       if(tmpx < 0):
         tmpx = x
       if(tmpy < 0):
         tmpy = y
-      i = i + 1
-    boprint("debug","placed tmpx %s,tmpy %s " % (tmpx,tmpy))
+      tries = tries + 1
+    boprint("debug","placed tmpx %d,tmpy %d " % (tmpx,tmpy))
     BoScript.placeProduction(ai.player, int(factory), tmpx, tmpy)
 
 
@@ -102,31 +101,44 @@ def produceFacilities(factory):
         return
     boprint("debug", "  rule 1: cannot fullfill rule")
 
-  # buld mineral/oil refineries
-  boprint("debug", "  rule 2: refineries!")
-  haveMineralRefinery = BoScript.playerUnitsOfTypeCount(ai.player, 13)
-  haveOilRefinery = BoScript.playerUnitsOfTypeCount(ai.player, 8)
-  if haveMineralRefinery < 1 or haveOilRefinery < 1:
-    boprint("debug", "  rule 2: not fullfilled")
-    for p in prod:
-      if p == 13 and haveMineralRefinery < 1:
-        BoScript.produceUnit(ai.player, factory, p)
-        return
-      if p == 8 and haveOilRefinery < 1:
-        BoScript.produceUnit(ai.player, factory, p)
-        return
-    boprint("debug", "  rule 2: cannot fullfill rule")
-
   # build weapon factory (ID=3) if necessary
   boprint("debug", "  rule 2: weaponsfactory!")
   haveWeaponsFactory = BoScript.playerUnitsOfTypeCount(ai.player, 3)
   if haveWeaponsFactory < 1:
-    boprint("debug", "  rule 3: not fullfilled")
+    boprint("debug", "  rule 2: not fullfilled")
     for p in prod:
       if p == 3:
         BoScript.produceUnit(ai.player, factory, p)
         return
+    boprint("debug", "  rule 2: cannot fullfill rule")
+
+
+  # buld mineral/oil refineries
+  boprint("debug", "  rule 3: refineries!")
+  haveMineralRefinery = BoScript.playerUnitsOfTypeCount(ai.player, 13)
+  haveOilRefinery = BoScript.playerUnitsOfTypeCount(ai.player, 8)
+  if haveMineralRefinery < 1 or haveOilRefinery < 1:
+    boprint("debug", "  rule 3: not fullfilled")
+    # AB: we use 2 different loops - we prefer mineral refinieries over oil
+    #     refineries (we need more minerals to build them)
+    for p in prod:
+      if p == 13 and haveMineralRefinery < 1:
+        BoScript.produceUnit(ai.player, factory, p)
+        return
+    for p in prod:
+      if p == 8 and haveOilRefinery < 1:
+        BoScript.produceUnit(ai.player, factory, p)
+        return
     boprint("debug", "  rule 3: cannot fullfill rule")
+
+
+  minerals = BoScript.minerals(ai.player)
+  oil = BoScript.minerals(ai.player)
+  haveMineralHarvesters = BoScript.playerUnitsOfTypeCount(ai.player, 10003)
+  haveOilHarvesters = BoScript.playerUnitsOfTypeCount(ai.player, 10002)
+  if (minerals < 1500 and haveMineralHarvesters < 1) or (oil < 1500 and haveOilHarvesters < 1):
+    boprint("debug", "unsufficient resources - delay facility production (need to build harvesters first")
+    return
 
   # build at least a minimum amount of defense facilities (turrets, samsites)
   # TODO
@@ -149,6 +161,13 @@ def produceFacilities(factory):
         BoScript.produceUnit(ai.player, factory, p)
         return
     boprint("debug", "  rule 4: cannot fullfill rule")
+
+
+  # only build the expensive stuff if we have enough resources or at least
+  # potential to gain new resources
+  if (minerals < 3000 and haveMineralHarvesters < 1) or (oil < 3000 and haveOilHarvesters < 1):
+    boprint("debug", "unsufficient resources - delay facility production (need to build harvesters first")
+    return
 
 
   # build secondary production facilities and comsat
@@ -209,54 +228,70 @@ def produceMobiles(factory):
   boprint("debug", "produceMobiles()")
   prod = BoScript.productionTypes(factory)
 
+  minerals = BoScript.minerals(ai.player)
+  oil = BoScript.minerals(ai.player)
+
   # build at least one harvester per kind
   boprint("debug", "  rule 1: harvesters!")
   haveMineralHarvesters = BoScript.playerUnitsOfTypeCount(ai.player, 10003)
   haveOilHarvesters = BoScript.playerUnitsOfTypeCount(ai.player, 10002)
   if haveMineralHarvesters < 1 or haveOilHarvesters < 1:
     boprint("debug", "  rule 1: not fullfilled")
+    # AB: we use two loops, as we prefer mineral harvesters over oil harvesters
+    #     (we usually need more minerals faster than oil)
+    for p in prod:
+      if p == 10003 and haveMineralHarvesters < 1:
+        BoScript.produceUnit(ai.player, factory, p)
+        return
     for p in prod:
       if p == 10002 and haveOilHarvesters < 1:
         BoScript.produceUnit(ai.player, factory, p)
         return
-      if p == 10003 and haveMineralHarvesters < 1:
-        BoScript.produceUnit(ai.player, factory, p)
-        return
-    boprint("debug", "  rule 1: cannot fullfill rule")
+    boprint("debug", "  rule 1: cannot fullfill rule.")
+
+    if minerals < 10000 or oil < 10000: # don't abort production if we have many resources
+      # AB: this is a two-sided sword.
+      #     it is important that we don't do any further productions: if we
+      #     produce tanks now, then we maybe won't be able to build harvesters
+      #     once the refineries are constructed.
+      #
+      #     however the other side is that if we cannot/don't want to build 
+      #     harvesters on this map (e.g. no resources on this map), then we
+      #     won't ever build any mobiles.
+      boprint("debug", "          aborting productions, we need to build harvesters first!")
+      return
 
 
   # have at least 2 harvester per kind if minerals/oil is low
   boprint("debug", "  rule 2: harvesters for bad times!")
-  minerals = BoScript.minerals(ai.player)
-  oil = BoScript.minerals(ai.player)
   needMineralHarvesters = 0
   needOilHarvesters = 0
-  if haveMineralHarvesters < 2 and minerals < 1000:
+  if haveMineralHarvesters < 2 and minerals < 1500:
     needMineralHarvesters = 1
-  if haveOilHarvesters < 2 and oil < 1000:
+  if haveOilHarvesters < 2 and oil < 1500:
     needOilHarvesters = 1
   if needMineralHarvesters > 0 or needOilHarvesters > 0:
     boprint("debug", "  rule 2: not fullfilled")
     for p in prod:
-      if p == 10002 and needOilHarvesters > 0:
+      if p == 10003 and needMineralHarvesters > 0:
         BoScript.produceUnit(ai.player, factory, p)
         return
-      if p == 10003 and needMineralHarvesters > 0:
+    for p in prod:
+      if p == 10002 and needOilHarvesters > 0:
         BoScript.produceUnit(ai.player, factory, p)
         return
     boprint("debug", "  rule 2: cannot fullfill rule")
 
   # have at least 3 tanks
   boprint("debug", "  rule 3: battle units!")
-  haveTanks = countUnitsOfType([10010, 1008, 10018])
+  tankTypes = [10010, 10008, 10018] # tiger, leopard, wolf
+  haveTanks = countUnitsOfType(tankTypes)
   if haveTanks < 3:
     boprint("debug", "  rule 3: not fullfilled")
-    if tryProduce(factory, 10010): # tiger
-      return
-    if tryProduce(factory, 10008): # leopard
-      return
-    if tryProduce(factory, 10018): # wolf
-      return
+    shuffle(tankTypes)
+    for type in tankTypes:
+      if tryProduce(factory, type):
+        return
     boprint("debug", "  rule 3: cannot fullfill rule")
 
 
@@ -271,10 +306,11 @@ def produceMobiles(factory):
   if needMineralHarvesters > 0 or needOilHarvesters > 0:
     boprint("debug", "  rule 4: not fullfilled")
     for p in prod:
-      if p == 10002 and needOilHarvesters > 0:
+      if p == 10003 and needMineralHarvesters > 0:
         BoScript.produceUnit(ai.player, factory, p)
         return
-      if p == 10003 and needMineralHarvesters > 0:
+    for p in prod:
+      if p == 10002 and needOilHarvesters > 0:
         BoScript.produceUnit(ai.player, factory, p)
         return
     boprint("debug", "  rule 4: cannot fullfill rule")
@@ -290,13 +326,17 @@ def produceMobiles(factory):
     needOilHarvesters = 1
   if needMineralHarvesters > 0 or needOilHarvesters > 0:
     boprint("debug", "  rule 5: not fullfilled")
-    for p in prod:
-      if p == 10002 and needOilHarvesters > 0:
-        BoScript.produceUnit(ai.player, factory, p)
-        return
-      if p == 10003 and needMineralHarvesters > 0:
-        BoScript.produceUnit(ai.player, factory, p)
-        return
+    list = []
+    if needOilHarvesters > 0:
+      list = list + [10002]
+    if needMineralHarvesters > 0:
+      list = list + [10003]
+    shuffle(list)
+    for harvester in list:
+      for p in prod:
+        if p == harvester:
+          BoScript.produceUnit(ai.player, factory, p)
+          return
     boprint("debug", "  rule 5: cannot fullfill rule")
 
 
@@ -307,9 +347,11 @@ def produceMobiles(factory):
   for p in prod:
     if BoScript.isUnitTypeMobile(ai.player, p):
       if p != 10002 and p != 10003:
-        allowedList = allowedList + p
+        allowedList = allowedList + [p]
+  shuffle(allowedList)
   for p in allowedList:
     BoScript.produceUnit(ai.player, factory, p)
+    return
 
   boprint("debug", "  rule 6: cannot fullfill rule")
 
