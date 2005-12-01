24023d70fae0464dde75cb4e244010365a30a35e
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2005-12-01 18:09:41 +0000

    removing our ufo::UBoxLayout patch from libufo. it won't make it to official libufo cvs.
    don't use ufo::UBoxLayout in any boson code, use ufo::UBoBoxLayout instead.
    (code outside of BoUfo is guaranteed to do so anyway)

--- trunk/code/ufo/src/layouts/uboxlayout.cpp
+++ trunk/code/ufo/src/layouts/uboxlayout.cpp
@@ -2,8 +2,6 @@
     LibUFO - UI For OpenGL
     copyright         : (C) 2001-2005 by Johannes Schmidt
     email             : schmidtjf at users.sourceforge.net
-    copyright         : (C) 2005 by Andreas Beckermann
-    email             : b_mann at gmx.de
                              -------------------
 
     file              : src/layouts/upopuplayout.cpp
@@ -37,9 +35,6 @@
 
 using namespace ufo;
 
-static std::vector<URectangle>
-getBounds(int orientation, int hgap, int vgap, const UWidget* container, const URectangle & bounds, bool useAvailableSize = true);
-
 UFO_IMPLEMENT_DYNAMIC_CLASS(UBoxLayout, ULayoutManager)
 
 UBoxLayout::UBoxLayout()
@@ -71,209 +66,181 @@ UBoxLayout::~UBoxLayout() {}
 UDimension
 UBoxLayout::getPreferredLayoutSize(const UWidget * container,
 		const UDimension & maxSize) const {
+	UDimension ret;
 	int orientation = m_orientation;
 	if (orientation == NoOrientation) {
 		orientation = container->getOrientation();
 	}
-	URectangle bounds = container->getInnerBounds();
-	// AB: x, y don't matter in this method
-	bounds.w = maxSize.w;
-	bounds.h = maxSize.h;
-	std::vector<URectangle> childBounds = getBounds(orientation, m_hgap, m_vgap, container, bounds, false);
-	UDimension ret(0, 0);
-
 
 	for (unsigned int i = 0; i < container->getWidgetCount(); i++) {
-		const URectangle& b = childBounds[i];
-		ret.w = std::max(b.x + b.w, ret.w);
-		ret.h = std::max(b.y + b.h, ret.h);
-	}
-	ret.clamp(maxSize);
+		UWidget * w = container->getWidget(i);
 
-#if 0
-	// AB: the above seems not to work correctly (e.g. for menus).
-	// note that with the following code we'll exceed maxSize!
-	if (orientation == Horizontal) {
-		ret.h = 0;
-	} else {
-		ret.w = 0;
-	}
-	for (unsigned int i = 0; i < container->getWidgetCount(); i++) {
-		if (orientation == Horizontal) {
-			ret.h = std::max(ret.h, childBounds[i].h);
-		} else {
-			ret.w = std::max(ret.w, childBounds[i].w);
+		if (w->isVisible() || !w->testState(WidgetForceInvisible)) {
+			// FIXME: Resize maxSize to respect already "consumed" space
+			const UDimension & size = w->getPreferredSize(maxSize);
+
+			if (orientation == Horizontal) {
+				ret.h = std::max(size.h, ret.h);
+				ret.w += size.w;
+
+				ret.w += m_hgap;
+			} else {
+				ret.w = std::max(size.w, ret.w);
+				ret.h += size.h;
+
+				ret.h += m_vgap;
+			}
 		}
 	}
-#endif
-
-
+	if (orientation == Horizontal) {
+		ret.w -= m_hgap;
+	} else {
+		ret.h -= m_vgap;
+	}
 
 	return ret;
 }
-
-std::vector<URectangle>
-getBounds(int orientation, int hgap, int vgap, const UWidget * container, const URectangle & bounds, bool useAvailableSize) {
-	std::vector<URectangle> ret(container->getWidgetCount());
+/*
+void
+UBoxLayout::layoutContainer(const UWidget * container) {
+	URectangle bounds = container->getInnerBounds();
 
 	int x = bounds.x;
 	int y = bounds.y;
-	UDimension dim(bounds.w, bounds.h);
-
-	for (unsigned int i = 0 ; i < container->getWidgetCount() ; i++) {
-		const UWidget * w = container->getWidget(i);
-
-		if (w->isVisible()) {
-			if (orientation == Horizontal) {
-				if (x > bounds.x) {
-					x += hgap;
-					if (dim.w >= hgap) {
-						dim.w -= hgap;
-					} else {
-						dim.w = 0;
-					}
-				}
-			} else {
-				if (y > bounds.y) {
-					y += vgap;
-					if (dim.h >= vgap) {
-						dim.h -= vgap;
-					} else {
-						dim.h = 0;
-					}
-				}
-			}
-			UDimension d = w->getPreferredSize(dim);
-			d.clamp(dim);
-
-			ret[i].x = x;
-			ret[i].y = y;
-			ret[i].w = d.w;
-			ret[i].h = d.h;
-			if (orientation == Horizontal) {
-				x += d.w;
-				dim.w -= d.w;
-			} else {
-				y += d.h;
-				dim.h -= d.h;
-			}
-		} else {
-			ret[i].x = x;
-			ret[i].y = y;
-			ret[i].w = 0;
-			ret[i].h = 0;
-		}
-	}
-
-	// When useAvailableSize is true, we use the whole size in bounds, by
-	// assigning additional space to the widgets. However this is not always
-	// intended (getPreferredSize() must not use that).
-	if (!useAvailableSize) {
-		return ret;
-	}
-
-	unsigned int stretchCount = 0;
-	unsigned int visibleWidgets = 0;
-	for (unsigned int i = 0 ; i < container->getWidgetCount() ; i++) {
-		const UWidget * w = container->getWidget(i);
-
-		if (w->isVisible()) {
-			visibleWidgets++;
-			ufo::UObject* o = w->get("stretch");
-			if (o) {
-				int stretch = UInteger::toInt(o->toString());
-				if (stretch > 0) {
-					stretchCount += stretch;
-				}
-			}
-		}
-	}
-
-	// the visible widgets share the remaining space
-	// TODO:
-	// 1. maintain the aspect? probably it makes little sense if a widget
-	//    with w=10 receives the same additional width as a widget with
-	//    w=1000.
-	//    testing will reveal whether aspect maintaining looks better or
-	//    not.
-	// 2. we totally ignore any maximum size currently
-	int addW = 0;
-	if (visibleWidgets > 0) {
-		if (stretchCount > 0) {
-			addW = dim.w / stretchCount;
-		} else {
-			addW = dim.w / visibleWidgets;
-		}
-	}
-	int addH = 0;
-	if (visibleWidgets > 0) {
-		if (stretchCount > 0) {
-			addH = dim.h / stretchCount;
-		} else {
-			addH = dim.h / visibleWidgets;
-		}
-	}
-	int fixX = 0;
-	int fixY = 0;
-	for (unsigned int i = 0 ; i < container->getWidgetCount() ; i++) {
-		const UWidget * w = container->getWidget(i);
-
-		ret[i].x += fixX;
-		ret[i].y += fixY;
-
-		if (w->isVisible()) {
-			int stretchFactor = 0;
-			if (stretchCount > 0) {
-				ufo::UObject* o = w->get("stretch");
-				if (o) {
-					stretchFactor = UInteger::toInt(o->toString());
-					if (stretchFactor < 0) {
-						stretchFactor = 0;
-					}
-				}
-			} else {
-				stretchFactor = 1;
-			}
-
-			if (orientation == Horizontal) {
-				int add = addW;
-				add *= stretchFactor;
-
-				fixX += add;
-				ret[i].w += add;
-			} else {
-				int add = addH;
-				add *= stretchFactor;
-
-				fixY += add;
-				ret[i].h += add;
-			}
-		}
-	}
-	dim.w = 0;
-	dim.h = 0;
-
-	return ret;
-}
-
-void
-UBoxLayout::layoutContainer(const UWidget * container) {
-	int orientation = m_orientation;
-	if (orientation == NoOrientation) {
-		orientation = container->getOrientation();
-	}
-	const URectangle & bounds = container->getInnerBounds();
-	std::vector<URectangle> childBounds = getBounds(orientation, m_hgap, m_vgap, container, bounds);
 
 	for (unsigned int i = 0 ; i < container->getWidgetCount() ; i++) {
 		UWidget * w = container->getWidget(i);
 
 		if (w->isVisible()) {
-			const URectangle & b = childBounds[i];
-			if (orientation == Horizontal) {
-				w->setBounds(b.x, b.y, b.w, bounds.h);
+			const UDimension & d = w->getPreferredSize();
+
+			if (m_axis == XAxis) {
+				if (x > bounds.x) {
+					x += m_hgap;
+				}
+
+				w->setBounds(x, y, d.w, bounds.h);
+
+				x += d.w;
 			} else {
-				w->setBounds(b.x, b.y, bounds.w, b.h);
+				if (y > bounds.y) {
+					y += m_vgap;
+				}
+
+				w->setBounds(x, y, bounds.w, d.h);
+
+				y += d.h;
+			}
+		}
+	}
+}
+*/
+
+int
+getFlex(const UWidget * widget)  {
+	if (UInteger * flex = dynamic_cast<UInteger*>(widget->get("layout"))) {
+		return *flex;
+	}
+	return 0;
+}
+
+void
+UBoxLayout::layoutContainer(const UWidget * container) {
+	UDimension prefSize;
+	if (container->getParent()) {
+		prefSize = getPreferredLayoutSize(container, container->getParent()->getSize());
+	} else if (!container->getSize().isEmpty()) {
+		prefSize = container->getSize();
+	} else {
+		prefSize = UDimension::maxDimension;
+	}
+	UDimension size = container->getSize();
+	int totalFlex = getTotalFlex(container);
+	float ratio = (totalFlex != 0) ? 1.0f / totalFlex : 0.0f;
+
+	int orientation = (m_orientation != NoOrientation) ?
+		m_orientation : container->getOrientation();
+	if (orientation == NoOrientation) {
+		orientation = Horizontal;
+	}
+
+	int addSize = 0;
+	if (orientation == Vertical) {
+		addSize = size.h - prefSize.h;
+	} else {
+		addSize = size.w - prefSize.w;
+	}
+	// FIXME: should we shrink?
+	if (addSize < 0) {
+		addSize = 0;
+	}
+
+	URectangle bounds = container->getInnerBounds();
+
+	int x = bounds.x;
+	int y = bounds.y;
+	int width = 0;
+	int height = 0;
+	int alignx = container->getHorizontalAlignment();
+	int aligny = container->getVerticalAlignment();
+
+	if (totalFlex == 0 && alignx == AlignRight) {
+		x += addSize;
+	} else if (totalFlex == 0 && alignx == AlignCenter) {
+		x += int(addSize / 2.0f);
+	}
+
+	for (unsigned int i = 0 ; i < container->getWidgetCount() ; i++) {
+		UWidget * w = container->getWidget(i);
+
+		if (w->isVisible()) {
+			const UDimension & d = w->getPreferredSize();
+
+			if (orientation == Horizontal) {
+				width = d.w;
+				if (addSize) {
+					width += int(getFlex(w) * ratio * addSize);
+				}
+
+				if (aligny == AlignStretch) {
+					height = bounds.h;
+					y = bounds.y;
+				} else if (aligny == AlignTop) {
+					height = d.h;
+					y = bounds.y;
+				} else if (aligny == AlignBottom) {
+					height = d.h;
+					y = bounds.h - d.h;
+				} else { //if (aligny == AlignCenter) {
+					height = d.h;
+					y = int(bounds.h / 2.0f - d.h / 2.0f);
+				}
+				w->setBounds(x, y, width, height);
+
+				x += width;
+				x += m_hgap;
+			} else { // orientation == Vertical
+				height = d.h;
+				if (addSize) {
+					height += int(getFlex(w) * ratio * addSize);
+				}
+				if (alignx == AlignStretch) {
+					width = bounds.w;
+					x = bounds.x;
+				} else if (alignx == AlignTop) {
+					width = d.w;
+					x = bounds.x;
+				} else if (alignx == AlignBottom) {
+					width = d.w;
+					x = bounds.w - d.w;
+				} else { //if (alignx == AlignCenter) {
+					width = d.w;
+					x = int(bounds.w / 2.0f - d.w / 2.0f);
+				}
+				w->setBounds(x, y, width, height);
+
+				y += height;
+				y += m_vgap;
 			}
 		}
 	}
@@ -281,5 +248,9 @@ UBoxLayout::layoutContainer(const UWidget * container) {
 
 int
 UBoxLayout::getTotalFlex(const UWidget * container) {
-	return 0;
+	int ret = 0;
+	for (unsigned int i = 0; i  < container->getWidgetCount(); ++i) {
+		ret += getFlex(container->getWidget(i));
+	}
+	return ret;
 }
