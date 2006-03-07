32c2ae29f341c56a999f1f6cad5814b87a47ef20
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2006-03-07 16:21:07 +0000

    - adding BoGameLogSaver which handles Boson::saveGameLogs()   -> this allows to splut saveGameLogs() up into multiple methods without      bloating the Boson class - adding saveBoDebugLog()   this saves a *.bodebuglog file that includes all debug/warning/error messages   that are still in the debuglog in memory.   if backtraces are available, those are saved into the log, too.

--- trunk/code/boson/gameengine/boson.h
+++ trunk/code/boson/gameengine/boson.h
@@ -1,7 +1,7 @@
 /*
     This file is part of the Boson game
     Copyright (C) 1999-2000 Thomas Capricelli (capricel@email.enst.fr)
-    Copyright (C) 2001-2005 Andreas Beckermann (b_mann@gmx.de)
+    Copyright (C) 2001-2006 Andreas Beckermann (b_mann@gmx.de)
     Copyright (C) 2001-2005 Rivo Laks (rivolaks@hot.ee)
 
     This program is free software; you can redistribute it and/or modify
@@ -558,6 +558,7 @@ protected slots:
 
 private:
 	friend class BoAdvance;
+	friend class BoGameLogSaver;
 
 private:
 	class BosonPrivate;
