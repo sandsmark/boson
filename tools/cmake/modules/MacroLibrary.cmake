e12eedac7982fe853c7d35e70422166a1389c028
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2006-03-29 16:53:11 +0000

    removing our copy of cmake files

--- tools/cmake/modules/MacroLibrary.cmake
+++
@@ -1,8 +0,0 @@
-# - include MacroLibrary offers a collection of macros which extend the built-in cmake commands
-# OPTIONAL_FIND_PACKAGE( <name> [QUIT] )
-
-INCLUDE (MacroOptionalFindPackage)
-INCLUDE (MacroAdditionalCleanFiles)
-INCLUDE (MacroAddFileDependencies)
-INCLUDE (MacroGetenvWinPath)
-
