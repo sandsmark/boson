788c4fc5bd22941dbcb9898b78c027929577148a
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2006-03-29 16:53:11 +0000

    removing our copy of cmake files

--- trunk/tools/cmake/modules/FindCarbon.cmake
+++
@@ -1,8 +0,0 @@
-INCLUDE(CMakeFindFrameworks)
-
-CMAKE_FIND_FRAMEWORKS(Carbon)
-
-if(Carbon_FRAMEWORKS)
-	set (CARBON_LIBRARY "-framework Carbon" CACHE FILEPATH "Carbon framework" FORCE)
-	set (CARBON_FOUND 1)
-endif(Carbon_FRAMEWORKS)
