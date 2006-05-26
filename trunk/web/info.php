d58489843208882942dd3a18c027243991314027
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2006-05-26 20:44:11 +0000

    - Getting ready for 0.12   This updates most of homepage, todo are announcement and screenshots

--- trunk/web/info.php
+++ trunk/web/info.php
@@ -53,27 +53,25 @@ draw_bigbox_text("These requirements apply to the latest stable release
 
     <b>Minimum hardware requirements:</b><br>
     Note: If you have an older graphics card like a TNT2 you will need a fast CPU.<br>
-    * 500 MHz ix86 Processor (don't know anything about other machines). AMD 64 is also known to work<br>
-    * 256 MB RAM<br>
-    * 3D accelerator card (this is the most important part!!)<br>
+    * 1 GHz x86 Processor (don't know anything about other machines). AMD 64 is also known to work<br>
+    * 256 MB RAM (512 MB is recommended)<br>
+    * 3D accelerator card (modern card with DirectX 9 support is recommended)<br>
     * Optional: Sound card<br>
     <br>
     <b>Minimum software requirements:</b><br>
-    * XFree 4.x server (with OpenGL support) - see <a href=\"http://www.xfree86.org/\" target=\"_blank\">http://www.xfree86.org/</a><br>
+    * XFree 4.x or X.org server (with OpenGL support) - see <a href=\"http://www.xfree86.org/\" target=\"_blank\">http://www.xfree86.org/</a><br>
     * OpenGL 1.2 - The OpenGL library (should be included in your XFree Server or graphics card driver)<br>
     * Qt 3.1 or better - see <a href=\"http://www.trolltech.com/\" target=\"_blank\">http://www.trolltech.com/</a><br>
     * kdelibs 3.1 or better (3.0 is untested) - see <a href=\"http://www.kde.org/\" target=\"_blank\">http://www.kde.org/</a><br>
     * Python 2.0 or better (2.3 is recommended) - see <a href=\"http://www.python.org/\" target=\"_blank\">http://www.python.org/</a><br>
     * libvorbis (for sound and music) - see <a href=\"http://www.vorbis.com/\" target=\"_blank\">http://www.vorbis.com/</a><br>
-    * lib3ds (Units and building rendering) - see <a href=\"http://lib3ds.sf.net/\" target=\"_blank\">http://lib3ds.sf.net/</a><br>
     * OpenAL (audio library) - see <a href=\"http://www.openal.org/\"target=\"_blank\">http://www.openal.org/</a><br>
-    * WML and Perl  - For the handbook - see <a href=\"http://www.thewml.org\" target=\"_blank\">http://www.thewml.org</a><br>
     <br>");
+//    * WML and Perl  - For the handbook - see <a href=\"http://www.thewml.org\" target=\"_blank\">http://www.thewml.org</a><br>
 
 draw_bigbox_subheader("<a name=\"compiler\"></a>Compiler requirements");
 draw_bigbox_text("To compile Boson (you don't need a compiler if you're downloading a binary version),
-    you need <a href=\"http://gcc.gnu.org/\" target=\"_blank\">Gcc</a> version 2.95.3 or 3.1.x, 3.2, 3.3 or 3.4.
-    <b>Gcc 3.0.x</b> is untested and <b>may not work</b>. <b>Gcc 2.96 won't work</b> either.");
+    you need <a href=\"http://gcc.gnu.org/\" target=\"_blank\">Gcc</a> version 3.3 or better.");
 
 draw_bigbox_end();
 
