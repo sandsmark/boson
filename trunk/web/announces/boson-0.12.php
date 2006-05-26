58480d490bd427245199a0d4cde1d1f953f667ec
rivol <rivol@14664f6f-540d-0410-967e-c81f6ba7cd39>
2006-05-26 22:18:56 +0000

    - Fix newlines

--- trunk/web/announces/boson-0.12.php
+++ trunk/web/announces/boson-0.12.php
@@ -63,17 +63,20 @@ A lot has changed since the last release - so much that we can't even remember m
 * Power Plants. The power plant unit has been in Boson for a very long time - now they actually generate power! If a player has insufficient power, several tasks (such as building new units) take more time.<br>
 Note that many core features are still in development and thus Boson is still mostly unplayable.<br>
 <br>
-For a longer, though still very incomplete list see <a href=\"http://boson.eu.org/announces/changelog-0.12.php\">http://boson.eu.org/announces/changelog-0.12.php</a>
+For a longer, though still very incomplete list see<br>
+<a href=\"http://boson.eu.org/announces/changelog-0.12.php\">http://boson.eu.org/announces/changelog-0.12.php</a>
 </p>
 
 <p class=\"announcement\">
 <b>Requirements</b><br>
-You can find a list of hardware and software requirements at <a href=\"http://boson.eu.org/info.php\">http://boson.eu.org/info.php</a> Note that proper 3d acceleration is required.
+You can find a list of hardware and software requirements at <a href=\"http://boson.eu.org/info.php\">http://boson.eu.org/info.php</a><br>
+Note that proper 3d acceleration is required.
 </p>
 
 <p class=\"announcement\">
 <b>Downloading and installing Boson</b><br>
-Source packages can be found at <a href=\"http://sourceforge.net/project/showfiles.php?group_id=15087&release_id=419522 \">http://sourceforge.net/project/showfiles.php?group_id=15087&release_id=419522</a><br>
+Source packages can be found at<br>
+<a href=\"http://sourceforge.net/project/showfiles.php?group_id=15087&release_id=419522 \">http://sourceforge.net/project/showfiles.php?group_id=15087&release_id=419522</a><br>
 Due to the new cmake based build system the installation instructions completely differ from previous releases. Also note that because of the cmake transition we do not provide static package for this release.<br>
 <br>
 First of all you need cmake 2.4.2, see <a href=\"http://www.cmake.org/files/v2.4/\">http://www.cmake.org/files/v2.4/</a><br>
@@ -88,7 +91,8 @@ $ su<br>
 # make install<br>
 <br>
 If you want to install at a different prefix, use 'cmake -DCMAKE_INSTALL_PREFIX=/install/boson/to ..' instead of the 'cmake ..' line.<br>
-Installation instructions can also be found at <a href=\"http://boson.eu.org/install.php\">http://boson.eu.org/install.php</a>
+Installation instructions can also be found at<br>
+<a href=\"http://boson.eu.org/install.php\">http://boson.eu.org/install.php</a>
 </p>
 
 <p class=\"announcement\">
