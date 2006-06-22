6fe0fbfdf838b1daa8f9d20cd3d698c260abec1c
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2006-06-22 22:22:47 +0000

    more macros that help at building semi-static binaries.
    in particular adding BOSON_TARGET_LINK_LIBRARIES: it behaves just like the
    normal TARGET_LINK_LIBRARIES, but it adds -Wl,-Bstatic to the beginning and
    -Wl,-Bdynamic to the end of the libraries list, if a semi-static binary is being
    built.
    this macro should be used instead of TARGET_LINK_LIBRARIES in boson.

--- tools/cmake/modules/boson/BosonMacros.cmake
+++ tools/cmake/modules/boson/BosonMacros.cmake
@@ -1,6 +1,8 @@
 # This file contains the following macros:
 # BOSON_ADD_BOUI_FILES
 # BOSON_READ_STATIC_DEPENDENCIES_FROM_LA
+# BOSON_USE_STATIC_QT_AND_KDE
+# BOSON_TARGET_LINK_LIBRARIES
 #
 # This file defines the following variables:
 # QT_MT_REQUIRED (to TRUE)
@@ -64,40 +66,85 @@ ENDMACRO (BOSON_ADD_BOUI_FILES)
 
 # reads the .la _la_file of a library and places the libraries that it depends on in the dependencies variable
 MACRO(BOSON_READ_STATIC_DEPENDENCIES_FROM_LA _la_file dependencies)
-	set(_dependencies "")
-	if (EXISTS "${_la_file}")
-		file(READ ${_la_file} _la_data)
-		string(REGEX MATCH "dependency_libs='.*" _dependencies "${_la_data}")
-		string(REGEX REPLACE "' *\n.*" "" _dependencies "${_dependencies}")
-		string(REGEX REPLACE "dependency_libs='" "" _dependencies "${_dependencies}")
+   set(_dependencies "")
+   if (EXISTS "${_la_file}")
+      file(READ ${_la_file} _la_data)
+      string(REGEX MATCH "dependency_libs='.*" _dependencies "${_la_data}")
+      string(REGEX REPLACE "' *\n.*" "" _dependencies "${_dependencies}")
+      string(REGEX REPLACE "dependency_libs='" "" _dependencies "${_dependencies}")
 
-		# turn into cmake list
-		string(REGEX REPLACE " " ";" _dependencies ${_dependencies})
+      # turn into cmake list
+      string(REGEX REPLACE " " ";" _dependencies ${_dependencies})
 
-		set(_dependencies_tmp "${_dependencies}")
-		set(_dependencies "")
-		foreach (element ${_dependencies_tmp})
-			# we don't use -R at all in boson.
-			# certainly not with static libs.
-			if (${element} MATCHES "^ *-R")
-				set(element "")
-			endif (${element} MATCHES "^ *-R")
+      set(_dependencies_tmp "${_dependencies}")
+      set(_dependencies "")
+      foreach (element ${_dependencies_tmp})
+         # we don't use -R at all in boson.
+         # certainly not with static libs.
+         if (${element} MATCHES "^ *-R")
+            set(element "")
+         endif (${element} MATCHES "^ *-R")
 
-			# "libfoo.la" -> "libfoo"
-			string(REGEX REPLACE "\\.la" "" element "${element}")
+         # "libfoo.la" -> "libfoo"
+         string(REGEX REPLACE "\\.la" "" element "${element}")
 
-			# "/path/to/libs/libfoo" -> "-lfoo"
-			string(REGEX REPLACE "^ */.*/lib" "-l" element "${element}")
+         # "/path/to/libs/libfoo" -> "-lfoo"
+         string(REGEX REPLACE "^ */.*/lib" "-l" element "${element}")
 
-			if (element)
-				set(_dependencies "${_dependencies};${element}")
-			endif (element)
-		endforeach(element)
-	else (EXISTS "${_la_file}")
-		message(STATUS "${_la_file} does not exist. Need a .la file to find dependencies of static library. Linking will probably fail")
-	endif (EXISTS "${_la_file}")
-	set(${dependencies} "${_dependencies}")
+         if (element)
+            set(_dependencies "${_dependencies};${element}")
+         endif (element)
+      endforeach(element)
+   else (EXISTS "${_la_file}")
+      message(STATUS "${_la_file} does not exist. Need a .la file to find dependencies of static library. Linking will probably fail")
+   endif (EXISTS "${_la_file}")
+   set(${dependencies} "${_dependencies}")
 ENDMACRO(BOSON_READ_STATIC_DEPENDENCIES_FROM_LA)
+
+MACRO(BOSON_USE_STATIC_QT_AND_KDE)
+   MESSAGE(STATUS "TODO: use proper path to *.la files")
+   # TODO: use KDEDIR/QTDIR or so (i.e. use path to real libfoo.a and replace
+   #       ".a" by ".la")
+   SET(_qt_mt_la "/home/andi/kde/boson/static/qt-copy/lib/libqt-mt.la")
+   SET(_kdecore_la "/home/andi/kde/boson/static/kdedir/lib/libkdecore.la")
+
+   BOSON_READ_STATIC_DEPENDENCIES_FROM_LA(${_qt_mt_la} _static_qt_dependencies)
+   BOSON_READ_STATIC_DEPENDENCIES_FROM_LA(${_kdecore_la} _static_kdecore_dependencies)
+
+   STRING(REGEX REPLACE " " ";" QT_LIBRARIES "${QT_LIBRARIES}")
+   STRING(REGEX REPLACE " " ";" QT_AND_KDECORE_LIBS "${QT_AND_KDECORE_LIBS}")
+   SET(QT_LIBRARIES "${QT_LIBRARIES};${_static_qt_dependencies}")
+   SET(QT_AND_KDECORE_LIBS "${QT_AND_KDECORE_LIBS};${_static_kdecore_dependencies};${_static_qt_dependencies}")
+
+
+   # we don't want these libs linked statically atm.
+   SET(link_libs_dynamic "dl;pthread")
+
+   # NEVER EVER link libGL statically
+   SET(link_libs_dynamic "${link_libs_dynamic};GL")
+   FOREACH (lib ${link_libs_dynamic})
+      IF ("${QT_AND_KDECORE_LIBS}" MATCHES "-l${lib}")
+         STRING(REGEX REPLACE "-l${lib}" "" QT_AND_KDECORE_LIBS "${QT_AND_KDECORE_LIBS}")
+         SET(QT_AND_KDECORE_LIBS "${QT_AND_KDECORE_LIBS};-Wl,-Bdynamic;-l${lib};-Wl,-Bstatic")
+      ENDIF ("${QT_AND_KDECORE_LIBS}" MATCHES "-l${lib}")
+
+      IF ("${QT_LIBRARIES}" MATCHES "-l${lib}")
+         STRING(REGEX REPLACE "-l${lib}" "" QT_LIBRARIES "${QT_LIBRARIES}")
+         SET(QT_LIBRARIES "${QT_LIBRARIES};-Wl,-Bdynamic;-l${lib};-Wl,-Bstatic")
+      ENDIF ("${QT_LIBRARIES}" MATCHES "-l${lib}")
+   ENDFOREACH (lib)
+ENDMACRO(BOSON_USE_STATIC_QT_AND_KDE)
+
+
+# behaves like TARGET_LINK_LIBRARIES, but in addition provides semi-static builds
+# if BOSON_LINK_STATIC is set
+MACRO(BOSON_TARGET_LINK_LIBRARIES target)
+   set(args "${ARGN}")
+   IF (BOSON_LINK_STATIC)
+      SET(args "-Wl,-Bstatic;${args};-Wl,-Bdynamic")
+   ENDIF (BOSON_LINK_STATIC)
+   TARGET_LINK_LIBRARIES(${target} ${args})
+ENDMACRO(BOSON_TARGET_LINK_LIBRARIES)
 
 
 # vim: et sw=3 textwidth=0
