e8b71ac6c2b66c534c9e8d7e0ff722c6a64bf8e3
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2006-06-22 19:48:34 +0000

    no need to check for the "uic" binary in PATH anymore: we provide a fixed version of kde3uic.cmake

--- tools/cmake/modules/boson/CheckQt3Executables.cmake
+++ tools/cmake/modules/boson/CheckQt3Executables.cmake
@@ -42,12 +42,14 @@ MACRO ( CHECK_QT3_EXECUTABLES )
   # AB: this sucks - we can test for cmake 2.4 and 2.5, but not for 2.4.2
   # -> therefore we need to use a STATUS message here instead of a FATAL_ERROR:
   #    the user might be using 2.4.x where kde3uic.cmake is fixed.
-  IF (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.5)
-    STRING(REGEX MATCH "for Qt\\ version\\ 3\\." _uic_version_2 ${_uic_version})
-    IF (NOT _uic_version_2)
-      MESSAGE( STATUS "WARNING: QT_UIC_EXECUTABLE is set correctly, however cmake version <= 2.4.2 also requires that the \"uic\" executable in path is a Qt 3.x uic. This seems not to be the case on your system. Version string: ${_uic_version}")
-    ENDIF (NOT _uic_version_2)
-  ENDIF (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.5)
+  # AB: UPDATE: we don't need this test anymore: we provide a fixed version of
+  #             kde3uic.cmake
+#  IF (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.5)
+#    STRING(REGEX MATCH "for Qt\\ version\\ 3\\." _uic_version_2 ${_uic_version})
+#    IF (NOT _uic_version_2)
+#      MESSAGE( STATUS "WARNING: QT_UIC_EXECUTABLE is set correctly, however cmake version <= 2.4.2 also requires that the \"uic\" executable in path is a Qt 3.x uic. This seems not to be the case on your system. Version string: ${_uic_version}")
+#    ENDIF (NOT _uic_version_2)
+#  ENDIF (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.5)
 
 ENDMACRO ( CHECK_QT3_EXECUTABLES )
 
