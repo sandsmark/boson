21734b8572843bb30881384d40522e0dd2a87a42
abmann <abmann@14664f6f-540d-0410-967e-c81f6ba7cd39>
2006-06-24 01:05:23 +0000

    - static versions of QT_AND_KDECORE_KDEUI_LIBS and QT_AND_KDECORE_KDEUI_KIO_LIBS - rewrite BOSON_TARGET_LINK_LIBRARIES:   before we used "-Wl,-Bstatic ${all_libraries} -Wl,-Bdynamic" - however this   doesn't work with cmake under certain circumstances, which we run into a lot:   when we do the above for targets A and B, and A depends on B, then the second   -Wl,-Bdynamic will get stripped, because cmake considers that "library" a   duplicate, as it was added before already.   by some weird reason -Wl,-Bstatic is NOT stripped and thus we end up doing a   full-static build, which we do not intend.
      so instead we do for every library "-Wl,-Bstatic ${lib} -Wl,-Bdynamic", and
      deliver that whole "line" to cmake as if it was a single library. thus cmake
      will either strip all three commands (which is fine for us) or none at all
      (which is fine for us, too).
    
      of course the generated linker lines are terribly ugly (very long), but this
      is only for building semi-static binaries and therefore is perfectly fine with
      me :-)
    
    with this patch i am now able to do a full semi-static build of boson. some
    binaries (in particular boconditioneditor) don't work yet, since the
    dependencies are not resolved correctly (cmake seems to remove duplications at
    the wrong points). however both, the boson and the bobmfconverter binaries work
    and that's all that counts for me.
    
    and the best part: it actually is maintainable now :-)

--- trunk/tools/cmake/modules/boson/BosonMacros.cmake
+++ trunk/tools/cmake/modules/boson/BosonMacros.cmake
@@ -101,18 +101,46 @@ MACRO(BOSON_READ_STATIC_DEPENDENCIES_FROM_LA _la_file dependencies)
    set(${dependencies} "${_dependencies}")
 ENDMACRO(BOSON_READ_STATIC_DEPENDENCIES_FROM_LA)
 
+
+MACRO(_REMOVE_LIBS_FROM_LIST list have_removed remove_libs)
+   SET(_newlist "")
+   SET(${have_removed} "")
+   FOREACH (current_lib ${${list}})
+      SET(_match FALSE)
+      FOREACH (remove_lib ${remove_libs})
+         IF (${current_lib} MATCHES "^ *(-l)?${remove_lib}")
+            SET(_match TRUE)
+         ENDIF (${current_lib} MATCHES "^ *(-l)?${remove_lib}")
+      ENDFOREACH (remove_lib)
+      IF (_match)
+         SET(${have_removed} "${${have_removed}};${current_lib}")
+      ELSE (_match)
+         SET(_newlist "${_newlist};${current_lib}")
+      ENDIF (_match)
+   ENDFOREACH (current_lib)
+   SET(${list} ${_newlist})
+ENDMACRO(_REMOVE_LIBS_FROM_LIST)
+
 MACRO(BOSON_USE_STATIC_QT_AND_KDE)
    GET_FILENAME_COMPONENT(_qt_lib_dir ${QT_QT_LIBRARY} PATH)
    SET(_qt_mt_la "${_qt_lib_dir}/libqt-mt.la")
    SET(_kdecore_la "${KDE3_LIB_DIR}/libkdecore.la")
+   SET(_kdeui_la "${KDE3_LIB_DIR}/libkdeui.la")
+   SET(_kio_la "${KDE3_LIB_DIR}/libkio.la")
 
    BOSON_READ_STATIC_DEPENDENCIES_FROM_LA(${_qt_mt_la} _static_qt_dependencies)
    BOSON_READ_STATIC_DEPENDENCIES_FROM_LA(${_kdecore_la} _static_kdecore_dependencies)
+   BOSON_READ_STATIC_DEPENDENCIES_FROM_LA(${_kdeui_la} _static_kdeui_dependencies)
+   BOSON_READ_STATIC_DEPENDENCIES_FROM_LA(${_kio_la} _static_kio_dependencies)
 
    STRING(REGEX REPLACE " " ";" QT_LIBRARIES "${QT_LIBRARIES}")
    STRING(REGEX REPLACE " " ";" QT_AND_KDECORE_LIBS "${QT_AND_KDECORE_LIBS}")
+   STRING(REGEX REPLACE " " ";" QT_AND_KDECORE_KDEUI_LIBS "${QT_AND_KDECORE_KDEUI_LIBS}")
+   STRING(REGEX REPLACE " " ";" QT_AND_KDECORE_KDEUI_KIO_LIBS "${QT_AND_KDECORE_KDEUI_KIO_LIBS}")
    SET(QT_LIBRARIES "${QT_LIBRARIES};${_static_qt_dependencies}")
    SET(QT_AND_KDECORE_LIBS "${QT_AND_KDECORE_LIBS};${_static_kdecore_dependencies};${_static_qt_dependencies}")
+   SET(QT_AND_KDECORE_KDEUI_LIBS "kdeui;${_static_kdeui_dependencies};${QT_AND_KDECORE_LIBS}")
+   SET(QT_AND_KDECORE_KDEUI_KIO_LIBS "kio;${_static_kio_dependencies};${QT_AND_KDECORE_KDEUI_LIBS}")
 
 
    # we don't want these libs linked statically atm.
@@ -120,29 +148,116 @@ MACRO(BOSON_USE_STATIC_QT_AND_KDE)
 
    # NEVER EVER link libGL statically
    SET(link_libs_dynamic "${link_libs_dynamic};GL")
-   FOREACH (lib ${link_libs_dynamic})
-      IF ("${QT_AND_KDECORE_LIBS}" MATCHES "-l${lib}")
-         STRING(REGEX REPLACE "-l${lib}" "" QT_AND_KDECORE_LIBS "${QT_AND_KDECORE_LIBS}")
-         SET(QT_AND_KDECORE_LIBS "${QT_AND_KDECORE_LIBS};-Wl,-Bdynamic;-l${lib};-Wl,-Bstatic")
-      ENDIF ("${QT_AND_KDECORE_LIBS}" MATCHES "-l${lib}")
 
-      IF ("${QT_LIBRARIES}" MATCHES "-l${lib}")
-         STRING(REGEX REPLACE "-l${lib}" "" QT_LIBRARIES "${QT_LIBRARIES}")
-         SET(QT_LIBRARIES "${QT_LIBRARIES};-Wl,-Bdynamic;-l${lib};-Wl,-Bstatic")
-      ENDIF ("${QT_LIBRARIES}" MATCHES "-l${lib}")
-   ENDFOREACH (lib)
+   _REMOVE_LIBS_FROM_LIST(QT_AND_KDECORE_LIBS libs_removed "${link_libs_dynamic}")
+   set(QT_AND_KDECORE_LIBS "${QT_AND_KDECORE_LIBS};-Wl,-Bdynamic;${libs_removed}")
+   _REMOVE_LIBS_FROM_LIST(QT_LIBRARIES libs_removed "${link_libs_dynamic}")
+   set(QT_LIBRARIES "${QT_LIBRARIES};-Wl,-Bdynamic;${libs_removed}")
+   _REMOVE_LIBS_FROM_LIST(QT_AND_KDECORE_KDEUI_LIBS libs_removed "${link_libs_dynamic}")
+   set(QT_AND_KDECORE_KDEUI_LIBS "${QT_AND_KDECORE_KDEUI_LIBS};-Wl,-Bdynamic;${libs_removed}")
+   _REMOVE_LIBS_FROM_LIST(QT_AND_KDECORE_KDEUI_KIO_LIBS libs_removed "${link_libs_dynamic}")
+   set(QT_AND_KDECORE_KDEUI_KIO_LIBS "${QT_AND_KDECORE_KDEUI_KIO_LIBS};-Wl,-Bdynamic;${libs_removed}")
+
 ENDMACRO(BOSON_USE_STATIC_QT_AND_KDE)
+
+
+# lib_variable: the _name_ of a variable that holds the libname.
+#               the libname in that variable will be set to the transformed
+#               libname, i.e.:
+#                 * start with -l if possible
+#                 * don't be an absolute path
+#                 * have a -L with it, if possible
+#               -> the variable may therefore actually be a ; separated list
+#                  afterwards.
+MACRO(_BOSON_TARGET_LINK_LIBRARIES_TRANSFORM_LIBNAME lib_variable)
+   # AB: I don't know if whitespaces are possible here, but we definitely
+   #     don't want them
+   STRING(REGEX REPLACE "^ +" "" ${lib_variable} "${${lib_variable}}")
+   STRING(REGEX REPLACE " +$" "" ${lib_variable} "${${lib_variable}}")
+
+   # AB: we should be able to use -lkdecore instead of kdecore (same for the
+   #     others), because we use LINK_DIRECTORIES for Qt/KDE libs anyway, so
+   #     we don't need the -L line that cmake adds
+   SET(_qt_kde_libs "kdecore;kdeui;kio;qt-mt;qassistantclient")
+   FOREACH (current_lib ${_qt_kde_libs})
+     IF ("${${lib_variable}}" MATCHES "^${current_lib}$")
+        SET(${lib_variable} "-l${current_lib}")
+     ENDIF ("${${lib_variable}}" MATCHES "^${current_lib}$")
+   ENDFOREACH (current_lib)
+
+   IF (${${lib_variable}} MATCHES "^/")
+      GET_FILENAME_COMPONENT(_path "${${lib_variable}}" PATH)
+      IF (${${lib_variable}} MATCHES "python")
+         message(STATUS "${${lib_variable}}")
+      ENDIF (${${lib_variable}} MATCHES "python")
+
+      # AB: NAME_WE does not work properly: it turns "python2.4.so" into
+      #     "python2" - we want only the ".so" part removed.
+#      GET_FILENAME_COMPONENT(_file "${${lib_variable}}" NAME_WE)
+      GET_FILENAME_COMPONENT(_file "${${lib_variable}}" NAME)
+      STRING(REGEX MATCH "^.*\\." _file "${_file}")
+      STRING(REGEX REPLACE "\\.$" "" _file "${_file}")
+      IF (${_file} MATCHES "^lib")
+         STRING(REGEX REPLACE "^lib" "" _file "${_file}")
+         SET(${lib_variable} "-L${_path};-l${_file}")
+      ENDIF (${_file} MATCHES "^lib")
+   ENDIF (${${lib_variable}} MATCHES "^/")
+ENDMACRO(_BOSON_TARGET_LINK_LIBRARIES_TRANSFORM_LIBNAME)
 
 
 # behaves like TARGET_LINK_LIBRARIES, but in addition provides semi-static builds
 # if BOSON_LINK_STATIC is set
 MACRO(BOSON_TARGET_LINK_LIBRARIES target)
-   set(args "${ARGN}")
+
+   # we don't want these libs linked statically atm.
+   SET(link_libs_dynamic "dl;pthread")
+
+   # NEVER EVER link libGL statically
+   SET(link_libs_dynamic "${link_libs_dynamic};GL")
+
+
+   SET(args "${ARGN}")
    IF (BOSON_LINK_STATIC)
-      SET(args "-Wl,-Bstatic;${args};-Wl,-Bdynamic")
+      # first transform the libs, i.e. if possible turn things like "kdecore"
+      # to "-lkdecore" and "/path/to/libfoo.a" to "-L/path/to;-lfoo.a"
+      SET(tmp_args "${args}")
+      SET(args "")
+      FOREACH (lib ${tmp_args})
+         _BOSON_TARGET_LINK_LIBRARIES_TRANSFORM_LIBNAME(lib)
+         SET(args "${args};${lib}")
+      ENDFOREACH (lib)
+
+      # replace "-lfoo" by "-Wl,-Bstatic -lfoo -Wl,-Bdynamic", if static
+      # linking is desired
+      SET(tmp_args "${args}")
+      SET(args "")
+      FOREACH (lib ${tmp_args})
+         SET(want_static FALSE)
+         IF (${lib} MATCHES "^ *-l")
+            SET(want_static TRUE)
+            FOREACH (l ${link_libs_dynamic})
+               IF (${lib} MATCHES "^ *(-l)?${l}")
+                  SET(want_static FALSE)
+               ENDIF (${lib} MATCHES "^ *(-l)?${l}")
+            ENDFOREACH (l)
+         ENDIF (${lib} MATCHES "^ *-l")
+
+         IF (want_static)
+            # note: the whole thing after the ';' is considered by cmake as a
+            #_single_ lib, as it is only space separated (cmake lists are ;
+            # separated).
+            # this is the whole point of this macro, as this way the
+            # -Wl,-Bdynamic won't get removed - unless the whole lib will get
+            # removed (cmake does that if it notices that it is duplicated and
+            # therefore not required.. however it is wrong about the
+            # -Wl,-Bdynamic one)
+            SET(args "${args};-Wl,-Bstatic ${lib} -Wl,-Bdynamic")
+         ELSE (want_static)
+            SET(args "${args};${lib}")
+         ENDIF (want_static)
+      ENDFOREACH (lib)
    ENDIF (BOSON_LINK_STATIC)
    TARGET_LINK_LIBRARIES(${target} ${args})
 ENDMACRO(BOSON_TARGET_LINK_LIBRARIES)
-
 
 # vim: et sw=3 textwidth=0
